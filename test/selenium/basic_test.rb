dcf7815626cf034ac26c59fb295830b1e916b8d1
R. Tyler Croy <tyler@monkeypox.org>
2011-09-30 00:02:57 -0700

    Verify that we're creating a new job from the fresh install properly

--- test/selenium/basic_test.rb
+++ test/selenium/basic_test.rb
@@ -6,4 +6,34 @@ class BasicSanityTests < JenkinsSeleniumTest
     @driver.navigate.to @base_url
     assert_not_nil @driver.title.match("\[Jenkins\]")
   end
+
+  def test_create_new_job
+    job_name = "Selenium_Test_Job"
+
+    @driver.navigate.to @base_url
+
+    link = @driver.find_element(:link_text, "create new jobs")
+    link.click
+
+    name_field = nil
+    @waiter.until do
+      name_field = @driver.find_element(:id, "name")
+    end
+
+    assert_not_nil name_field, "Couldn't find the Job Name field?!"
+
+    name_field.send_keys job_name
+
+    job_type = @driver.find_element(:xpath, "//input[@value='hudson.model.FreeStyleProject']")
+    job_type.click
+
+    name_field.submit
+
+    created = nil
+    @waiter.until do
+      created = @driver.title.match("#{job_name} Config")
+    end
+
+    assert_not_nil created, "It doesn't appear that we created a new job properly"
+  end
 end
