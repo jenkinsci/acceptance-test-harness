76557bd9c4b41fb9525443176e3f1771b5b8f315
R. Tyler Croy <tyler@monkeypox.org>
2011-10-08 13:37:24 -0700

    Test the addition and running of a shell build step

--- test/selenium/core/freestyle_test.rb
+++ test/selenium/core/freestyle_test.rb
@@ -25,4 +25,21 @@ class FreestyleJobTests < JenkinsSeleniumTest
     assert_not_nil enable_button, "Couldn't find the [Enable] button, guess we failed to disable the job!"
   end
 
+  def test_exec_shell_build_step
+    @job.configure do
+      @job.add_build_step "ls"
+    end
+
+    @job.build
+
+    # This is kind of silly, but I can't think of a better way to wait for the
+    # biuld to complete
+    sleep 5
+
+    console = @job.console_for_build(1)
+
+    assert_not_nil console.match("\\+ ls"), "Could not verify that the script properly ran in the following console: #{console}"
+    assert_not_nil console.match("Finished: SUCCESS"), "Could not verify that the job ran properly"
+  end
+
 end
