c4c08a2bc70201ceffe2a4785060ea61fafdc3db
Vojtech Juranek <vjuranek@redhat.com>
2011-10-31 18:15:35 +0100

    Added test for tying a job to a label

--- test/selenium/core/freestyle_test.rb
+++ test/selenium/core/freestyle_test.rb
@@ -1,7 +1,8 @@
 require File.dirname(__FILE__) + "/../lib/base"
 require File.dirname(__FILE__) + "/../pageobjects/newjob"
 require File.dirname(__FILE__) + "/../pageobjects/job"
-
+require File.dirname(__FILE__) + "/../pageobjects/newslave"
+require File.dirname(__FILE__) + "/../pageobjects/slave"
 
 class FreestyleJobTests < JenkinsSeleniumTest
   def setup
@@ -54,6 +55,38 @@ class FreestyleJobTests < JenkinsSeleniumTest
     assert build.succeeded, "The build did not succeed!"
     #assert build.console.include? @PARAM_VALUE, "Test parameter value not found!" # requires ruby 1.9
     assert_not_nil build.console.index(@PARAM_VALUE), "Test parameter value not found!"
+  end
+
+  def test_tie_to_label
+    @slave_name = "test_slave"
+    @test_label = "test_label"
+
+    # create new slave
+    NewSlave.create_dumb(@driver, @base_url, @slave_name)
+    @slave = Slave.new(@driver, @base_url, @slave_name)
+    @slave.set_num_executors(1)
+    @slave.set_remote_fs(@slave_tempdir)
+    @slave.set_labels(@test_label)
+    @slave.set_command_on_master("java -jar #{JENKINS_LIB_DIR}/slave.jar")
+    @slave.save
+    #wait for slave to be online
+    start = Time.now
+    while (@slave.is_offline && ((Time.now - start) < TIMEOUT))
+      sleep 1
+    end
+    #TODO throw some exception if the slave is not start in time
+
+    # tie job to the label
+    @job.configure do
+      @job.tie_to(@test_label)
+    end
+
+    # run the job and check where it run
+    @job.queue_build
+    build = @job.build(1)
+    check_string = "Building remotely on #{@slave_name}"
+    assert_not_nil build.console.index(check_string), "Test wasn't run on #{@slave_name}"
+
   end
 
 end
