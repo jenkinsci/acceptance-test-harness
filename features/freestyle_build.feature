15b128f5eee65b5c815aafb35ef460bc12e460bc
R. Tyler Croy <tyler@monkeypox.org>
2011-11-12 11:52:30 -0800

    Flesh out more build steps and commit the current progress as an html report
    Generated with: `cucumber -f html -o report.html`

--- features/freestyle_build.feature
+++ features/freestyle_build.feature
@@ -7,21 +7,25 @@ Feature: Build freestyle jobs
   Scenario: Create a simple job
     Given a bare Jenkins instance
     When I create a job named "test"
+    And I configure the job
     And I add a script build step to run "ls"
+    And I save the job
     And I run the job
     Then I should see console output matching "+ ls"
 
   Scenario: Disable a job
     Given a job
     When I configure the job
-    And I click the "Disable Build" checkbox
+    And I click the "disable" checkbox
+    And I save the job
     Then the job should be disabled
 
   Scenario: Enable concurrent builds
     Given a job
     When I configure the job
-    And I click the "Execute Concurrent Builds" checkbox
+    And I click the "_.concurrentBuild" checkbox
     And I add a script build step to run "sleep 20"
+    And I save the job
     Then I should be able to build two jobs
     And the jobs should run concurrently
 
--- features/step_definitions/freestyle_steps.rb
+++ features/step_definitions/freestyle_steps.rb
@@ -2,7 +2,21 @@
 # vim: tabstop=2 expandtab shiftwidth=2
 
 
-Given /^a Jenkins instance$/ do
+Given /^a bare Jenkins instance$/ do
+end
+
+Given /^a job$/ do
+  suffix = (rand() * 1_000_000).to_s[0 .. 20]
+  name = "test_job_#{suffix}"
+  @job = Job.create_freestyle(@base_url, name)
+end
+
+
+############################################################################
+
+
+When /^I configure the job$/ do
+  @job.configure
 end
 
 When /^I create a job named "([^"]*)"$/ do |name|
@@ -10,17 +24,30 @@ When /^I create a job named "([^"]*)"$/ do |name|
 end
 
 When /^I add a script build step to run "([^"]*)"$/ do |script|
-  @job.configure do
-    @job.add_script_step(script)
-  end
+  @job.add_script_step(script)
 end
 
 When /^I run the job$/ do
   @job.queue_build
 end
 
+When /^I click the "([^"]*)" checkbox$/ do |name|
+  find(:xpath, "//input[@name='#{name}']").set(true)
+end
+
+When /^I save the job$/ do
+  @job.save
+end
+
+
+############################################################################
+
+
 Then /^I should see console output matching "([^"]*)"$/ do |script|
   @job.last_build.console.should match /#{Regexp.escape(script)}/
 end
 
-
+Then /^the job should be disabled$/ do
+  @job.open
+  page.should have_content('This project is currently disabled')
+end
