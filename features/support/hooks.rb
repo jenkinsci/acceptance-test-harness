bb13d98aa408cc64c823e3dd2522077a9a616e5b
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-05-08 16:34:01 -0700

    updated to use the controller

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,15 +1,30 @@
 #!/usr/bin/env ruby
 # vim: tabstop=2 expandtab shiftwidth=2
 
-require File.dirname(__FILE__) + "/../../lib/runner.rb"
+$LOAD_PATH.push File.dirname(__FILE__) + "/../.."
+require "lib/jenkins_controller.rb"
+require "lib/local_controller.rb"
+require "lib/sysv_init_controller.rb"
 
 Before do
-  @runner = Jenkins::Runner.new
-  @runner.setup
-  @base_url = @runner.base_url
+
+  # default is to run locally, but allow the parameters to be given as env vars
+  # so that rake can be invoked like "rake test type=remote_sysv"
+  if ENV['type']
+    controller_args = {}
+    ENV.each { |k,v| controller_args[k.to_sym]=v }
+  else
+    controller_args = { :type => :local }
+  end
+
+  @runner = JenkinsController.create(controller_args)
+  @runner.start
+  at_exit do
+    @runner.stop
+  end
+  @base_url = @runner.url
   Capybara.app_host = @base_url
 end
 
 After do |scenario|
-  @runner.teardown scenario.failed?
 end
