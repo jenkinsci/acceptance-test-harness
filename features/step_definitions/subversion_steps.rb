908217276b4c326b9c3380f775218cea2b2674f0
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-11-19 19:54:17 -0800

    made more robust

--- features/step_definitions/subversion_steps.rb
+++ features/step_definitions/subversion_steps.rb
@@ -1,4 +1,5 @@
 When /^I check out code from Subversion repository "([^"]*)"$/ do |url|
-  @job.find(:xpath,"//input[@type='radio'][following-sibling::label[1]/text()='Subversion']").click
+  # older version put <label> next to radio, recent version put label as the parent of radio
+  @job.find(:xpath,"//input[@type='radio'][(following-sibling::label[1]/text()='Subversion') or (normalize-space(../text())='Subversion')]").click
   @job.find(:xpath,"//input[@id='svn.remote.loc']").set(url)
 end
