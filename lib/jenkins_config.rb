3e94e7cf2cabfea187b4d8905ab703bb5df4b20c
Vojtech Juranek <vjuranek@redhat.com>
2013-01-16 16:43:33 +0100

    Add global Jenkins configuration object

---
+++ lib/jenkins_config.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+require File.dirname(__FILE__) + "/pageobject.rb"
+
+module Jenkins
+  class JenkinsConfig < PageObject
+    
+    def initialize(*args)
+      super(*args)
+    end
+    
+    def configure_url
+      @base_url + "/configure"
+    end
+
+    def open
+      visit(configure_url)
+    end
+    
+    def configure(&block)
+      visit configure_url
+      unless block.nil?
+        yield
+        save
+      end
+    end
+
+
+    def add_tool(name)
+      click_button(name)
+    end
+
+    def self.get(base_url, name)
+      self.new(base_url, name)
+    end
+
+  end
+end
