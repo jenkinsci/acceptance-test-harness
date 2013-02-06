5c5b7a17f4cf6bb7e3aee217afbe1804651768f1
Oliver Gondža <ogondza@redhat.com>
2013-02-06 23:56:27 +0100

    Wait until axis appear before filling in

--- features/step_definitions/matrix-reloader_steps.rb
+++ features/step_definitions/matrix-reloader_steps.rb
@@ -4,35 +4,34 @@ When /^I configure user axis "([^"]*)" with values "([^"]*)"$/ do |name, value|
   end  
   find(:xpath, "//button[text()='Add axis']").click
   find(:xpath, "//li/a[text()='User-defined Axis']").click
-  find(:xpath, "(//div[@name='axis' and @descriptorid='hudson.matrix.TextAxis']//td/input[@name='_.name'])[last()]").set(name)
-  find(:xpath, "(//div[@name='axis' and @descriptorid='hudson.matrix.TextAxis']//td/input[@name='_.valueString'])[last()]").set(value)
-  #find(:xpath, "//button[text()='Save']").click
+  sleep 0.1 # wait until axis appear
+  input = "//div[@name='axis' and @descriptorid='hudson.matrix.TextAxis']//td/input";
+  find(:xpath, "(#{input}[@name='_.name'])[last()]").set(name)
+  find(:xpath, "(#{input}[@name='_.valueString'])[last()]").set(value)
 end
 
 Then /^I should see matrix configuration "([^"]*)"$/ do |configuration|
-   page.should have_xpath("//input[@name='MRP::#{configuration}']")
+  page.should have_xpath("//input[@name='MRP::#{configuration}']")
 end
 
 When /^I select matrix configuration "([^"]*)"$/ do |configuration|
-   find(:xpath, "//input[@name='MRP::#{configuration}']").set(true)
+  find(:xpath, "//input[@name='MRP::#{configuration}']").set(true)
 end
 
 When /^I rebuild matrix job$/ do
-   find(:xpath, "//form[@action='configSubmit']//button[text()='Rebuild Matrix']").click
+  find(:xpath, "//form[@action='configSubmit']//button[text()='Rebuild Matrix']").click
 end
 
-Then /^combination "([^"]*)" should be built$/ do |configuration|
-      visit @job.last_build.build_url
-   build_title = find(:xpath,"//h1[contains(text(),'Build')]").text()
-   build_name = build_title.split(" ")[2] 
-   visit @job.job_url + "/#{configuration}/lastBuild"
-   page.should have_xpath("//h1[contains(text(),'Build #{build_name}')]")
-end
+Then /^combination "([^"]*)" (should|should not) be built$/ do |configuration, operator|
+  visit @job.last_build.build_url
+  build_title = find(:xpath,"//h1[contains(text(),'Build')]").text()
+  build_name = build_title.split(" ")[2]
+  visit @job.job_url + "/#{configuration}/lastBuild"
 
-Then /^combination "([^"]*)" should not be built$/ do |configuration|
-   visit @job.last_build.build_url
-   build_title = find(:xpath,"//h1[contains(text(),'Build')]").text()
-   build_name = build_title.split(" ")[2] 
-   visit @job.job_url + "/#{configuration}/lastBuild"
-   page.should_not have_xpath("//h1[contains(text(),'Build #{build_name}')]")
+  haveBuiltConfig = have_xpath("//h1[contains(text(),'Build #{build_name}')]");
+  if operator == "should"
+    page.should haveBuiltConfig
+  else
+    page.should_not haveBuiltConfig
+  end
 end
