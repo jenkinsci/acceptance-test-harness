229e7540b86cec3c8ca1707baf0eb79a2d195789
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-02-14 14:45:12 -0800

    Not using the DE version

--- veewee/definitions/jenkins-test-opensuse-11.4/definition.rb
+++ veewee/definitions/jenkins-test-opensuse-11.4/definition.rb
@@ -11,15 +11,11 @@ Veewee::Session.declare({
     'linux netdevice=eth0 netsetup=dhcp',
     ' install=http://download.opensuse.org/distribution/11.4/repo/oss/ insecure=1',
     ' lang=en_US autoyast=http://%IP%:%PORT%/autoinst_en.xml',
-    # disable prev line and enable next line to install with german settings
-    #' lang=de_DE autoyast=http://%IP%:%PORT%/autoinst_de.xml',
     ' textmode=1',
     '<Enter>'
    ],
   :kickstart_port => "7122", :kickstart_timeout => "10000",
   :kickstart_file => ["autoinst_en.xml", "autoinst_en.xml"],
-  # disable prev line and enable next line to install with german settings
-  #:kickstart_file => ["autoinst_de.xml", "autoinst_de.xml"],
   :ssh_login_timeout => "10000", :ssh_user => "vagrant", :ssh_password => "vagrant", :ssh_key => "",
   :ssh_host_port => "7222", :ssh_guest_port => "22",
   :sudo_cmd => "echo '%p'|sudo -S sh '%f'",
