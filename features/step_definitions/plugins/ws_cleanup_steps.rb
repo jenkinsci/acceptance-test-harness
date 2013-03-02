0aa702cc550b5c271076195c4ee63cc1348cbcad
Oliver Gondža <ogondza@redhat.com>
2013-03-02 23:26:12 +0100

    Convert last (should|should not) expression

--- features/step_definitions/plugins/ws_cleanup_steps.rb
+++ features/step_definitions/plugins/ws_cleanup_steps.rb
@@ -3,8 +3,6 @@ Then /^I should be able to configure workspace cleanup$/ do
   page.should have_xpath("//input[@name='hudson-plugins-ws_cleanup-PreBuildCleanup']");
 end
 
-Then /^there (should|should not) be "([^"]*)" in the workspace$/ do |operator, filename|
-  present = @job.workspace.contains(filename)
-  expected = operator == 'should'
-  present.should eql(expected)
+Then /^there (should|should not) be "([^"]*)" in the workspace$/ do |should_or_not, filename|
+  @job.workspace.contains(filename).send should_or_not, eql(true)
 end
