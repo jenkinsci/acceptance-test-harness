4c2bcf8eca7b627a7f73a63eae5c71c9dcca9fce
Oliver Gondža <ogondza@redhat.com>
2013-04-16 22:34:46 +0200

    typo

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 curl -s -o use-ruby https://repository-cloudbees.forge.cloudbees.com/distributions/ci-addons/ruby/use-ruby
 RUBY_VERSION=1.9.3-p194 source ./use-ruby
 
-gem instal --conservative bundle
+gem install --conservative bundle
 bundle check || bundle install
 bundle list
 
