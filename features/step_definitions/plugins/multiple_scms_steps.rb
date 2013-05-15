73c829dcf2c7621f7bbab0f6b10577b151ef1339
Oliver Gondža <ogondza@redhat.com>
2013-05-15 20:56:56 +0200

    Use Jenkins::Scm for Multiple SCMs feature

--- features/step_definitions/plugins/multiple_scms_steps.rb
+++ features/step_definitions/plugins/multiple_scms_steps.rb
@@ -1,34 +1,15 @@
 When /^I choose Multiple SCMs SCM$/ do
-  @job.find(:xpath,"//input[@type='radio'][(following-sibling::label[1]/text()='Multiple SCMs') or (normalize-space(../text())='Multiple SCMs')]").click
-end	  
-
-When /^I add a new SCM$/ do
-  find(:xpath, "//button[text()='Add SCM']").click()
+  @multiscm = @job.add_scm 'Multiple SCMs'
 end
 
-When /^I choose new "([^"]*)" SCM$/ do |scm|
-  find(:xpath,"//a[text()='#{scm}']").click
+When /^I add Git scm url "([^"]*)" and directory name "([^"]*)"$/ do |url, dirname|
+  @gitscm = @multiscm.add 'Git'
+  @gitscm.url url
+  @gitscm.local_dir dirname
 end
 
-When /^I add Git scm url "([^"]*)" and directory name "([^"]*)"$/ do |url, name|
-  find(:xpath, "//button[text()='Add SCM']").click()
-  find(:xpath,"//a[text()='Git']").click
-  sleep 1
-  find(:xpath,"//div[@descriptorid='hudson.plugins.git.GitSCM']//input[@name='_.url']").set(url)
-  find(:xpath, "(//div[@descriptorid='hudson.plugins.git.GitSCM']//button[text()='Advanced...'])[last()]").click()
-  sleep 1
-  find(:xpath, "//div[@descriptorid='hudson.plugins.git.GitSCM']//input[@name='_.relativeTargetDir']").set(name)
+When /^I add Subversion scm url "([^"]*)" and directory name "([^"]*)"$/ do |url, dirname|
+  @svnscm = @multiscm.add "Subversion"
+  @svnscm.url url
+  @svnscm.local_dir dirname
 end
-
-When /^I add Subversion scm url "([^"]*)" and directory name "([^"]*)"$/ do |url, name|
-  find(:xpath, "//button[text()='Add SCM']").click()
-  find(:xpath,"//a[text()='Subversion']").click()
-  sleep 1
-  find(:xpath,"//input[@id='svn.remote.loc']").set(url)
-  find(:xpath,"//input[@name='_.local']").set(name)
-end
-
-Then /^I should see "([^"]*)" SCM option$/ do |scm|
-  page.should have_xpath("//a[text()='#{scm}']")
-end
-
--- lib/plugins/subversion.rb
+++ lib/plugins/subversion.rb
@@ -1,10 +1,14 @@
 module Jenkins
   class SubversionScm < Scm
 
+    register 'Subversion'
+
     def url(url)
-      find(:path, path("locations/remote")).set(url)
+      find(:path, path('locations/remote')).set(url)
+    end
+
+    def local_dir(dir)
+      find(:path, path('locations/local')).set(dir)
     end
   end
 end
-
-Jenkins::Scm.register('Subversion', Jenkins::SubversionScm)
