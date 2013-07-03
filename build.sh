24eba66068f0a9b37f5cd2aebe9f265c1ba716d9
Oliver Gondža <ogondza@redhat.com>
2013-07-03 12:28:57 +0200

    Use upstream Jenkins instead of pull request builds

--- build.sh
+++ build.sh
@@ -7,4 +7,6 @@ RUBY_VERSION=1.9.3-p194 source ./use-ruby
 gem install --conservative bundle
 bundle update
 
+curl -s -o jenkins.war https://ci.jenkins-ci.org/job/jenkins_main_trunk/lastSuccessfulBuild/artifact/war/target/jenkins.war
+
 JENKINS_WAR=jenkins.war bundle exec rake
