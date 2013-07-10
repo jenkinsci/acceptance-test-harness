90909d012127d846af4bc40e0e7541cb463a1fa9
Oliver Gondža <ogondza@redhat.com>
2013-07-10 12:16:53 +0200

    Fix WS checking step

--- features/step_definitions/plugins/ws_cleanup_steps.rb
+++ features/step_definitions/plugins/ws_cleanup_steps.rb
@@ -1,3 +1,4 @@
 Then /^there (should|should not) be "([^"]*)" in the workspace$/ do |should_or_not, filename|
-  @job.workspace.contains(filename).send should_or_not, eql(true)
+  @job.last_build.wait_until_finished
+  @job.workspace.contains(filename).send should_or_not, eql(true), "Artifact not present"
 end
