b8f56cab7abfe525f5b832d1235cfa547044aaa2
Oliver Gondža <ogondza@redhat.com>
2013-08-20 13:40:30 +0200

    Simple audit trail scenarios

---
+++ lib/plugins/audit_trail.rb
@@ -0,0 +1,40 @@
+require File.dirname(__FILE__) + "/../pageobject.rb"
+
+module Plugins
+  class AuditTrail < Jenkins::PageObject
+
+    def initialize(base_url)
+      super(base_url, 'Audit Trail Log')
+
+      begin
+        sleep 1
+        open
+      end until page.has_xpath? "//h1[text()='Audit Trail']"
+    end
+
+    def url
+      base_url + '/log/Audit Trail/'
+    end
+
+    def open
+      visit url
+    end
+
+    def events
+      open
+      events = []
+      all(:css, '#main-panel pre').each do |row|
+        matcher = /((?:\/\w+)+.*?) by /.match(row.text)
+        next if matcher.nil? # Earlier versions used one element per log entry newer use two
+        events << matcher[1]
+      end
+
+      return events
+    end
+
+    def empty?
+      open
+      first(:css, '#main-panel pre').nil?
+    end
+  end
+end
