9da714259e19737da631222ea4759257d565fe9f
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-11-21 10:31:42 -0800

    refactored docker code for better reuse.

--- features/step_definitions/docker.rb
+++ features/step_definitions/docker.rb
@@ -1,25 +1,21 @@
 Given /^a docker fixture "([^"]*)"$/ do |name|
   fixtures = File.dirname(__FILE__)+"/../../fixtures/#{name}"
-  if !system("sudo docker build -t jenkins/#{name} .", :chdir=>fixtures)
-    raise "Failed to build container"
-  end
-  spawn()
-  IO.popen("sudo docker run -d -p 2222:22 jenkins/#{name}") do |p|
-    @docker = p.gets
-  end
-  sleep 3
+  img = Jenkins::Docker.build(name)
+  @docker = img.start([22])
 end
 
 # this is for illustration only and we'll remove this
 Then /^I login via ssh$/ do
   key = File.dirname(__FILE__)+"/../../fixtures/sshd/unsafe"
-  if !system("ssh -p 2222 -i #{key} test@localhost uname -a")
+  File.chmod(0600,key)
+
+  if !system("ssh -p #{@docker.port(22)} -o StrictHostKeyChecking=no -i #{key} test@localhost uname -a")
     raise "ssh failed!"
   end
 end
 
 Before('@docker') do |scenario|
-  if !system('which docker', :out => '/dev/null')
+  if !Jenkins::Docker.available?
     puts 'Skipping: docker not available'
     scenario.skip!
   end
@@ -28,11 +24,6 @@ end
 After('@docker') do |scenario|
   if @docker
     puts "Shutting down: #{@docker}"
-    if !system("sudo docker kill #{@docker}", :out => '/dev/null')
-      raise "Failed to kill #{@docker}"
-    end
-    if !system("sudo docker rm #{@docker}", :out => '/dev/null')
-      raise "Failed to rm #{@docker}"
-    end
+    @docker.clean
   end
 end
