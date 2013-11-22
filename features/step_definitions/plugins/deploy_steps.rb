f895b515ea4243f528e3e5162bb8d2326bb36ee9
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-11-21 19:24:04 -0800

    Added a test for deploy plugin

---
+++ features/step_definitions/plugins/deploy_steps.rb
@@ -0,0 +1,17 @@
+When(/^I deploy "([^"]*)" to docker tomcat7 fixture at context path "([^"]*)"$/) do |war,path|
+  section = find(:xpath, "//div[@descriptorId='hudson.plugins.deploy.DeployPublisher']")
+  section.find(:path, '/publisher/war').set(war)
+  section.find(:path, '/publisher/contextPath').set(path)
+
+  section.find(:xpath,'.//option[@value="hudson.plugins.deploy.tomcat.Tomcat7xAdapter"]').click()
+
+  # these parameters are hard-coded to tomcat7 docker fixture
+  section.find(:path, '/publisher/adapter/userName').set("admin")
+  section.find(:path, '/publisher/adapter/password').set("tomcat")
+  section.find(:path, '/publisher/adapter/url').set("http://localhost:#{@docker['tomcat7'].port(8080)}/")
+
+end
+When(/^docker tomcat7 fixture should show "([^"]*)" at "([^"]*)"$/) do |pattern, url|
+  http = RestClient.get "http://localhost:#{@docker['tomcat7'].port(8080)}#{url}"
+  http.to_str.should match /#{pattern}/
+end
