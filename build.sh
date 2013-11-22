44301f795d579c9f10341cbe2867a188313b92f2
Oliver Gondža <ogondza@redhat.com>
2013-11-22 19:30:13 +0100

    Bump ruby version used at cloudbees to 2.0

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # Build step to run tests
 
 curl -s -o use-ruby https://repository-cloudbees.forge.cloudbees.com/distributions/ci-addons/ruby/use-ruby
-RUBY_VERSION=1.9.3-p194 source ./use-ruby
+RUBY_VERSION=2.0.0-dev source ./use-ruby
 
 gem install --conservative bundle
 bundle update
