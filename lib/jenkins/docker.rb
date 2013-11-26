2660b68cd1f4678e21f5c474e707e1dbd051170b
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-11-25 17:32:31 -0800

    create intermediate objects as needed

--- lib/jenkins/docker.rb
+++ lib/jenkins/docker.rb
@@ -2,97 +2,96 @@
 # Wrapper around docker
 #
 module Jenkins
-end
+  module Docker
 
-module Jenkins::Docker
+    # command to invoke docker
+    DOCKER = "sudo docker "
 
-  # command to invoke docker
-  DOCKER = "sudo docker "
-
-  # returns true-ish if docker is available in the system
-  def self.available?
-    system('which docker', :out => '/dev/null')
-  end
-
-  # build a fixture
-  #
-  # @param tag [String]     name to give to the created image
-  # @param dir [String]     a directory that houses Dockerfile
-  # @return [Image]         a prepared docker image
-  def self.build(tag,dir)
-    if !system("#{DOCKER} build -t #{tag} .", :chdir=>dir)
-      raise "Failed to build container"
+    # returns true-ish if docker is available in the system
+    def self.available?
+      system('which docker', :out => '/dev/null')
     end
 
-    Image.new(tag)
-  end
+    # build a fixture
+    #
+    # @param tag [String]     name to give to the created image
+    # @param dir [String]     a directory that houses Dockerfile
+    # @return [Image]         a prepared docker image
+    def self.build(tag,dir)
+      if !system("#{DOCKER} build -t #{tag} .", :chdir=>dir)
+        raise "Failed to build container"
+      end
 
-  # Container image, a template to launch virtual machines from.
-  class Image
-    # @param name [String]   name of the image
-    def initialize(name)
-      @name = name.chomp
+      Image.new(tag)
     end
 
-    # @param ports [Array<Integer>]   ports to expose
-    # @return [Container]
-    def start(ports)
-      opts = ports.map {|y| " -p 127.0.0.1::#{y}"}.join
-      IO.popen("#{DOCKER} run -d #{opts} #{@name}") do |p|
-        sleep 3   # TODO: find out how to properly wait for the service to start. maybe just wait for port to start listening?
+    # Container image, a template to launch virtual machines from.
+    class Image
+      # @param name [String]   name of the image
+      def initialize(name)
+        @name = name.chomp
+      end
 
-        Container.new(p.gets)
+      # @param ports [Array<Integer>]   ports to expose
+      # @return [Container]
+      def start(ports)
+        opts = ports.map {|y| " -p 127.0.0.1::#{y}"}.join
+        IO.popen("#{DOCKER} run -d #{opts} #{@name}") do |p|
+          sleep 3   # TODO: find out how to properly wait for the service to start. maybe just wait for port to start listening?
+
+          Container.new(p.gets)
+        end
+      end
+
+      def to_s
+        "Docker image #{@name}"
       end
     end
 
-    def to_s
-      "Docker image #{@name}"
-    end
-  end
-
-  # Running container, a virtual machine
-  class Container
-    def initialize(cid)
-      @cid = cid.chomp
-    end
-
-    # Container ID
-    # @return [String]
-    def cid
-      @cid
-    end
-
-    # find the ephemeral port that the given container port is mapped to
-    # @return [Integer]
-    def port(n)
-      IO.popen("#{DOCKER} port #{@cid} #{n}") do |io|   # this returns single line like "0.0.0.0:55326"
-        io.gets.split(":")[1].to_i
+    # Running container, a virtual machine
+    class Container
+      def initialize(cid)
+        @cid = cid.chomp
       end
-    end
 
-    # stop and remove any trace of the container
-    def clean
-      if !system("#{DOCKER} kill #{@cid}", :out => '/dev/null')
-        raise "Failed to kill #{@cid}"
+      # Container ID
+      # @return [String]
+      def cid
+        @cid
       end
-      if !system("#{DOCKER} rm #{@cid}", :out => '/dev/null')
-        raise "Failed to rm #{@cid}"
+
+      # find the ephemeral port that the given container port is mapped to
+      # @return [Integer]
+      def port(n)
+        IO.popen("#{DOCKER} port #{@cid} #{n}") do |io|   # this returns single line like "0.0.0.0:55326"
+          io.gets.split(":")[1].to_i
+        end
       end
-    end
 
-    # copy a files/folders from inside the container to outside
-    def cp(from,to)
-      if !system("#{DOCKER} cp #{@cid}:#{from} #{to}")
-        raise "Failed to copy #{from} to #{to}"
+      # stop and remove any trace of the container
+      def clean
+        if !system("#{DOCKER} kill #{@cid}", :out => '/dev/null')
+          raise "Failed to kill #{@cid}"
+        end
+        if !system("#{DOCKER} rm #{@cid}", :out => '/dev/null')
+          raise "Failed to rm #{@cid}"
+        end
       end
-    end
 
-    def to_s
-      "Docker container #{@cid}"
-    end
+      # copy a files/folders from inside the container to outside
+      def cp(from,to)
+        if !system("#{DOCKER} cp #{@cid}:#{from} #{to}")
+          raise "Failed to copy #{from} to #{to}"
+        end
+      end
 
-    def self.of(test,name)
-      test.instance_variable_get('@docker')[name]
+      def to_s
+        "Docker container #{@cid}"
+      end
+
+      def self.of(test,name)
+        test.instance_variable_get('@docker')[name]
+      end
     end
   end
 end
