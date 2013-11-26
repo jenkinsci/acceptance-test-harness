fb589fa10e0793cea8269f95ffb47a892cf8df57
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-11-26 15:19:50 -0800

    ADD prevents caching, which makes it difficult for other fixtures to enhance this.
    See https://github.com/dotcloud/docker/issues/880

--- fixtures/sshd/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -ex
-useradd test -d /home/test
-mkdir -p /home/test/.ssh
-chown test /home/test
-echo "test:test" | chpasswd
-cat > /home/test/.ssh/authorized_keys << EOF
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDzpxmTW9mH87DMkMSqBrSecoSHVCkKbW5IOO+4unak8M8cyn+b0iX07xkBn4hUJRfKA7ezUG8EX9ru5VinteqMOJOPknCuzmUS2Xj/WJdcq3BukBxuyiIRoUOXsCZzilR/DOyNqpjjI3iNb4los5//4aoKPCmLInFnQ3Y42VaimH1298ckEr4tRxsoipsEAANPXZ3p48gGwOf1hp56bTFImvATNwxMViPpqyKcyVaA7tXCBnEk/GEwb6MiroyHbS0VvBz9cZOpJv+8yQnyLndGdibk+hPbGp5iVAIsm28FEF+4FvlYlpBwq9OYuhOCREJvH9CxDMhbOXgwKPno9GyN kohsuke@atlas
-EOF
