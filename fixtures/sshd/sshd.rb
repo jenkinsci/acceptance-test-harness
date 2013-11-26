d0ee3e0468b72b4300d505f73b71f20c3b668cb8
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-11-26 15:05:32 -0800

    added more convenience methods

--- fixtures/sshd/sshd.rb
+++ fixtures/sshd/sshd.rb
@@ -11,11 +11,20 @@ module Jenkins
         key
       end
 
+      # ssh command line
+      def ssh
+        "ssh -p #{port(22)} -o StrictHostKeyChecking=no -i #{private_key} test@localhost"
+      end
+
       # login with SSH public key and make sure you can connect
       def ssh_with_publickey(cmd)
-        if !system("ssh -p #{port(22)} -o StrictHostKeyChecking=no -i #{private_key} test@localhost #{cmd}")
+        if !system("#{ssh} #{cmd}")
           raise "ssh failed!"
         end
+      end
+
+      def ssh_popen(cmd,&block)
+        IO.popen("#{ssh} #{cmd}",&block)
       end
 
       register "sshd", [22]
