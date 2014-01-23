4f7795a3afef732e178815e81459828b7fe3dc5d
Oliver Gondža <ogondza@redhat.com>
2014-01-23 22:59:12 +0100

    JUnit scenario for parameterized tests

--- features/step_definitions/junit_publisher_steps.rb
+++ features/step_definitions/junit_publisher_steps.rb
@@ -4,3 +4,19 @@ When /^I set Junit archiver path "([^"]*)"$/ do |path|
   find(:xpath, "//a[text()='Publish JUnit test result report']").click
   find(:xpath, "//input[@name='_.testResults']").set(path)
 end
+
+Then /^"(.*?)" error summary should match "(.*?)"$/ do |test, message|
+  toggle test
+  page.text.should include message
+  toggle test
+end
+
+def toggle(test)
+  xpath = "//a[text()='#{test}']/../a[starts-with(@href, 'javascript')]"
+  elements = all(:xpath, xpath)
+  elements.length.should be >= 1
+  # JUnit report produced by TestNG contains colliding test names
+  for e in elements do
+    e.click
+  end
+end
