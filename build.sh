2f7e9615c1fbb66739b78cef6ffb22db9ecc51f6
Oliver Gondža <ogondza@gmail.com>
2014-02-28 11:54:07 +0100

    PRELAUNCH=true does not seem to have the desired effect on cloudbees

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ bundle update
 
 curl -s -o jenkins.war $WAR_URL
 
-STARTUP_TIMEOUT=240 PRELAUNCH=true JENKINS_WAR=jenkins.war bundle exec rake
+STARTUP_TIMEOUT=240 JENKINS_WAR=jenkins.war bundle exec rake
