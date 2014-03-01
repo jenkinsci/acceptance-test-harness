03e45c9c8a48c6c2b4ccc15c3eab8c88dc2aebdc
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-03-01 13:32:19 -0800

    allow WebDriver selection via an environment variable

--- src/main/java/org/jenkinsci/test/acceptance/FallbackConfig.java
+++ src/main/java/org/jenkinsci/test/acceptance/FallbackConfig.java
@@ -4,7 +4,13 @@ import com.google.inject.AbstractModule;
 import org.jenkinsci.test.acceptance.controller.JenkinsController;
 import org.jenkinsci.test.acceptance.controller.WinstoneController;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.util.Locale;
 
 /**
  * The default configuration for running tests.
@@ -18,10 +24,30 @@ public class FallbackConfig extends AbstractModule {
     @Override
     protected void configure() {
         try {
-            // for now, bind WebDriver to Firefox
-            bind(WebDriver.class).toInstance(new FirefoxDriver());
+            String browser = System.getenv("BROWSER");
+            if (browser==null)  browser="firefox";
+            browser = browser.toLowerCase(Locale.ENGLISH);
 
-            // bind from scaffolding
+            switch (browser) {
+            case "firefox":
+                bind(WebDriver.class).toInstance(new FirefoxDriver());
+                break;
+            case "ie":
+            case "iexplore":
+            case "iexplorer":
+                bind(WebDriver.class).toInstance(new InternetExplorerDriver());
+                break;
+            case "chrome":
+                bind(WebDriver.class).toInstance(new ChromeDriver());
+                break;
+            case "safari":
+                bind(WebDriver.class).toInstance(new SafariDriver());
+                break;
+            case "htmlunit":
+                bind(WebDriver.class).toInstance(new HtmlUnitDriver());
+                break;
+            }
+
             bind(JenkinsController.class).toInstance(new WinstoneController());
         } catch (Exception e) {
             throw new Error("Failed to configure Jenkins acceptance test harness",e);
