cd3952797322adb20050f1b3ed5234486047c2dd
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-03-01 09:53:54 -0800

    this is now ported over to AntPluginTest class

--- spec/spec_helper.rb
+++
@@ -1,3 +0,0 @@
-# meat of the rspec glue code is defined in lib/jenkins/rspec.rb for better reuse in other gems
-$:.unshift File.expand_path(File.dirname(__FILE__) + '/../lib')
-require 'jenkins/rspec'
