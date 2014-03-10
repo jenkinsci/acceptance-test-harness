c34567cf4d4edd44d59ba46c28064262ff97b351
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-03-09 21:27:06 -0700

    Ported this test case

--- features/build_history.feature
+++
@@ -1,9 +0,0 @@
-Feature: Display build history
-  As a Jenkins user or administrator
-  I should be able to view the build history both globally or per-job
-  So that I can identify build trends, times, etc.
-
-  Scenario: Viewing global build history
-    Given a simple job
-    When I build the job
-    Then the global build history should show the build
---
+++ src/test/java/plugins/BuildHistoryTest.java
@@ -0,0 +1,40 @@
+package plugins;
+
+import org.jenkinsci.test.acceptance.Matchers;
+import org.jenkinsci.test.acceptance.junit.AbstractJUnitTest;
+import org.jenkinsci.test.acceptance.po.FreeStyleJob;
+import org.jenkinsci.test.acceptance.po.Jenkins;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+import javax.inject.Inject;
+
+/**
+ * Feature: Display build history
+   As a Jenkins user or administrator
+   I should be able to view the build history both globally or per-job
+   So that I can identify build trends, times, etc.
+
+ */
+public class BuildHistoryTest extends AbstractJUnitTest {
+    @Inject
+    Jenkins j;
+
+    @Inject
+    WebDriver driver;
+
+    /**
+       Scenario: Viewing global build history
+         Given a simple job
+         When I build the job
+         Then the global build history should show the build
+     */
+    @Test
+    public void view_global_build_history() {
+        FreeStyleJob job = j.createJob();
+        job.queueBuild().waitUntilFinished();
+
+        j.visit("view/All/builds");
+        assertThat(driver, Matchers.hasContent(String.format("%s #1", job.name)));
+    }
+}
