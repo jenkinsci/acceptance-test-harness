49792f9c6ed0c2bd54d0218fd6024e1fba2e6252
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-03-13 18:14:09 -0700

    doc improvement

--- src/main/java/org/jenkinsci/test/acceptance/guice/SubWorld.java
+++ src/main/java/org/jenkinsci/test/acceptance/guice/SubWorld.java
@@ -2,16 +2,25 @@ package org.jenkinsci.test.acceptance.guice;
 
 import com.google.inject.Injector;
 import com.google.inject.Provider;
+import org.jenkinsci.test.acceptance.controller.MachineProvider;
 
 /**
+ * Represents a parallel Guice {@link Injector} inside {@link World}
+ * so that components can be selectively bound to {@link World}.
+ *
+ * <p>
+ * For example, this mechanism allows sophisticated test launcher configurations to
+ * use different {@link MachineProvider} implementations for master and slaves, etc.
+ *
+ * <p>
+ * See WIRING.md
+ *
  * @author Kohsuke Kawaguchi
  */
 public class SubWorld {
-    public final String name;
     public final Injector injector;
 
-    public SubWorld(String name, Injector injector) {
-        this.name = name;
+    /*package*/ SubWorld(Injector injector) {
         this.injector = injector;
     }
 
@@ -24,7 +33,7 @@ public class SubWorld {
      *     bind Foo to ...
      * }
      *
-     * bind Foo toProvider masters[Foo]  // export Foo from master to its parent
+     * bind Foo toProvider masters[Foo]  // export Foo from the "masters" subworld to the parent
      * </pre>
      */
     public <T> Provider<T> getAt(Class<T> t) {
