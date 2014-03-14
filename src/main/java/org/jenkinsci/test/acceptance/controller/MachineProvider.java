b4d4f13a17ea7a7a62d5f4fa8c7c29afc38aca9f
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-03-13 18:32:15 -0700

    doc improvement

--- src/main/java/org/jenkinsci/test/acceptance/controller/MachineProvider.java
+++ src/main/java/org/jenkinsci/test/acceptance/controller/MachineProvider.java
@@ -1,20 +1,26 @@
 package org.jenkinsci.test.acceptance.controller;
 
 import com.google.inject.Provider;
-import org.jenkinsci.test.acceptance.resolver.JenkinsResolver;
+import org.jenkinsci.test.acceptance.slave.SlaveProvider;
 
 /**
- * Provider for {@link org.jenkinsci.test.acceptance.controller.Machine}
+ * Provider for {@link Machine}.
+ *
+ * {@link JenkinsProvider} and {@link SlaveProvider}s rely on {@link MachineProvider}
+ * to acquire computers/VMs/containers to run Jenkins-under-test and slaves in.
  *
  * @author Vivek Pandey
  */
-public interface MachineProvider extends Provider<Machine>{
+public interface MachineProvider extends Provider<Machine> {
 
+    /**
+     * TCP/IP ports on this machine  that can be used by the client of {@link MachineProvider}.
+     */
     public int[] getAvailableInboundPorts();
 
     /**
      * A MachineProvider should encapsulates how authentication is done via
-     * {@link org.jenkinsci.test.acceptance.controller.Authenticator} contract.
+     * {@link Authenticator} contract.
      */
     public Authenticator authenticator();
 }
