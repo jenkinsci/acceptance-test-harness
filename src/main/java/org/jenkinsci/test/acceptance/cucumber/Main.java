409a50fcdfeb54a8cca0d35ce754494088e32151
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-03-14 15:40:38 -0700

    Renamed

--- src/main/java/org/jenkinsci/test/acceptance/cucumber/Main.java
+++ src/main/java/org/jenkinsci/test/acceptance/cucumber/Main.java
@@ -1,4 +1,4 @@
-package org.jenkinsci.test.acceptance;
+package org.jenkinsci.test.acceptance.cucumber;
 
 import cucumber.runtime.Env;
 import cucumber.runtime.ObjectFactoryImpl;
@@ -13,14 +13,14 @@ import java.util.Collections;
 /**
  * @author Kohsuke Kawaguchi
  */
-public class Driver {
+public class Main {
     public static void main(String[] args) throws Exception {
         if (args.length==0)
             args = new String[]{"features"};
 
         RuntimeOptions runtimeOptions = new RuntimeOptions(new Env("cucumber-jvm"), args);
 
-        ClassLoader classLoader = Driver.class.getClassLoader();
+        ClassLoader classLoader = Main.class.getClassLoader();
         ResourceLoader resourceLoader = new MultiLoader(classLoader);
 
         cucumber.runtime.Runtime runtime = new Runtime(resourceLoader, classLoader,
