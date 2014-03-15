183a70d46f8c79d9819bcb5628c7d67355f3172a
Oliver Gondža <ogondza@redhat.com>
2014-03-15 22:32:33 +0100

    Wait for audit trail exestence

--- src/test/java/plugins/AuditTrailPluginTest.java
+++ src/test/java/plugins/AuditTrailPluginTest.java
@@ -24,6 +24,7 @@ public class AuditTrailPluginTest extends AbstractJUnitTest {
     @Before
     public void setUp() {
         auditTrail = jenkins.getLogger("Audit Trail");
+        auditTrail.waitForExistence();
     }
 
 
