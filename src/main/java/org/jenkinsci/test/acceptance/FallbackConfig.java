87a312367b74ed7b619e59f54625f54edbdda1ad
Ulli Hafner <ullrich.hafner@gmail.com>
2014-03-17 13:54:52 +0100

    Set locale to "en" when starting the web driver.

--- src/main/java/org/jenkinsci/test/acceptance/FallbackConfig.java
+++ src/main/java/org/jenkinsci/test/acceptance/FallbackConfig.java
@@ -1,8 +1,11 @@
 package org.jenkinsci.test.acceptance;
 
-import com.cloudbees.sdk.extensibility.ExtensionList;
-import com.google.inject.AbstractModule;
-import com.google.inject.Provides;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 import org.jenkinsci.test.acceptance.controller.JenkinsController;
 import org.jenkinsci.test.acceptance.controller.JenkinsControllerFactory;
 import org.jenkinsci.test.acceptance.guice.TestCleaner;
@@ -10,19 +13,20 @@ import org.jenkinsci.test.acceptance.guice.TestScope;
 import org.jenkinsci.test.acceptance.server.JenkinsControllerPoolProcess;
 import org.jenkinsci.test.acceptance.server.PooledJenkinsController;
 import org.jenkinsci.test.acceptance.slave.LocalSlaveProvider;
-import org.jenkinsci.test.acceptance.slave.SlaveController;
 import org.jenkinsci.test.acceptance.slave.SlaveProvider;
 import org.junit.runners.model.Statement;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
-import java.io.IOException;
-import java.util.Locale;
-import java.util.concurrent.TimeUnit;
+import com.cloudbees.sdk.extensibility.ExtensionList;
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
 
 /**
  * The default configuration for running tests.
@@ -32,6 +36,9 @@ import java.util.concurrent.TimeUnit;
  * @author Kohsuke Kawaguchi
  */
 public class FallbackConfig extends AbstractModule {
+    /** Browser property to set the default locale. */
+    private static final String LANGUAGE_SELECTOR = "intl.accept_languages";
+
     @Override
     protected void configure() {
         // default in case nothing is specified
@@ -43,15 +50,24 @@ public class FallbackConfig extends AbstractModule {
         if (browser==null)  browser="firefox";
         browser = browser.toLowerCase(Locale.ENGLISH);
 
+
         switch (browser) {
         case "firefox":
-            return new FirefoxDriver();
+            FirefoxProfile profile = new FirefoxProfile();
+            profile.setPreference(LANGUAGE_SELECTOR, "en");
+
+            return new FirefoxDriver(profile);
         case "ie":
         case "iexplore":
         case "iexplorer":
             return new InternetExplorerDriver();
         case "chrome":
-            return new ChromeDriver();
+            Map<String, String> prefs = new HashMap<String, String>();
+            prefs.put(LANGUAGE_SELECTOR, "en");
+            ChromeOptions options = new ChromeOptions();
+            options.setExperimentalOption("prefs", prefs);
+
+            return new ChromeDriver(options);
         case "safari":
             return new SafariDriver();
         case "htmlunit":
