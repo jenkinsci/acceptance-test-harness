74d28fdb52a16c1000558aed3356f389586be34d
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-03-18 16:14:21 -0700

    added code that computes fingerprint

--- src/main/java/org/jenkinsci/test/acceptance/controller/SshKeyPair.java
+++ src/main/java/org/jenkinsci/test/acceptance/controller/SshKeyPair.java
@@ -1,12 +1,21 @@
 package org.jenkinsci.test.acceptance.controller;
 
 import com.google.inject.ProvidedBy;
-import org.codehaus.plexus.util.FileUtils;
+import org.apache.commons.io.FileUtils;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.PEMReader;
+import org.jvnet.hudson.crypto.RSAPublicKeyUtil;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.security.GeneralSecurityException;
+import java.security.KeyPair;
+import java.security.Security;
 
 /**
  * Configuration injected from outside that holds the SSH key pair that can be used for tests.
@@ -33,10 +42,22 @@ public class SshKeyPair {
     }
 
     public String readPublicKey() throws IOException {
-        return FileUtils.fileRead(publicKey,"UTF-8");
+        return FileUtils.readFileToString(publicKey);
     }
 
     public String readPrivateKey() throws IOException {
-        return FileUtils.fileRead(privateKey,"UTF-8");
+        return FileUtils.readFileToString(privateKey);
     }
-}
+
+    /**
+     * Computes EC2 fingerprint of this key.
+     */
+    public String getFingerprint() throws IOException, GeneralSecurityException {
+        Security.addProvider(new BouncyCastleProvider());
+         Reader r = new BufferedReader(new StringReader(FileUtils.readFileToString(privateKey)));
+         PEMReader pem = new PEMReader(r);
+
+         KeyPair pair = (KeyPair) pem.readObject();
+        return RSAPublicKeyUtil.getEC2FingerPrint(pair.getPublic());
+     }
+ }
