62a726f94fd7075194d445069ee625c8e7b791d0
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-03-18 18:14:18 -0700

    Ported WinstoneDockerController

---
+++ src/main/java/org/jenkinsci/test/acceptance/controller/WinstoneDockerController.java
@@ -0,0 +1,98 @@
+package org.jenkinsci.test.acceptance.controller;
+
+import com.cloudbees.sdk.extensibility.Extension;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import org.jenkinsci.test.acceptance.docker.Docker;
+import org.jenkinsci.test.acceptance.docker.fixtures.WinstoneContainer;
+import org.jenkinsci.utils.process.CommandBuilder;
+import org.jenkinsci.utils.process.ProcessInputStream;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.net.URL;
+
+/**
+ * Runs jenkins.war inside docker so that it gets a different IP address even though it's run on the same host.
+ *
+ * For efficiency, the docker container gets the entire host file system bind-mounted on it,
+ * and we ssh into that box and start jenkins.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class WinstoneDockerController extends LocalController {
+    @Inject
+    Docker docker;
+
+    private Class<? extends WinstoneContainer> fixtureType = WinstoneContainer.class;
+    private WinstoneContainer container;
+
+    public WinstoneDockerController(File war) {
+        super(war);
+    }
+
+    public void setFixture(Class<? extends WinstoneContainer> fixtureType) {
+        this.fixtureType = fixtureType;
+    }
+
+    @Override
+    public ProcessInputStream startProcess() throws IOException {
+        try {
+            CommandBuilder opts = new CommandBuilder();
+            opts.add("-v", getJenkinsHome()+":/work");
+            opts.add("-v", war.getParent()+":/war");
+
+            container = docker.start(fixtureType, opts, null);
+
+            CommandBuilder cmds = new CommandBuilder();
+            cmds.add("java","-jar","/war/"+war.getName());
+            cmds.add("--ajp13Port=-1","--controlPort=8081","--httpPort=8080");
+            return container.popen(cmds);
+        } catch (InterruptedException e) {
+            throw (IOException)new InterruptedIOException("Failed to launch winstone").initCause(e);
+        }
+    }
+
+    @Override
+    public void stopNow() throws IOException {
+        try {
+            super.stopNow();
+        } finally {
+            if (container!=null)
+                container.close();
+        }
+    }
+
+    public WinstoneContainer getContainer() {
+        return container;
+    }
+
+    @Override
+    public URL getUrl() {
+        try {
+            return new URL("http://"+container.getIpAddress()+":8080/");
+        } catch (IOException e) {
+            throw new AssertionError(String.format("%s failed to report its IP address",container),e);
+        }
+    }
+
+    @Extension
+    public static class FactoryImpl extends LocalFactoryImpl {
+        @Inject
+        Injector injector;
+
+        @Override
+        public String getId() {
+            return "winstone_docker";
+        }
+
+        @Override
+        public WinstoneDockerController create() {
+            WinstoneDockerController c = new WinstoneDockerController(getWarFile());
+            injector.injectMembers(c);
+            return c;
+        }
+    }
+}
+
