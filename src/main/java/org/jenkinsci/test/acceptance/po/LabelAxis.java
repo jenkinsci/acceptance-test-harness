60e870cf6135c5de6e0ebb2994e624be036bf7e3
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-03-22 21:29:10 -0700

    another axis impl

---
+++ src/main/java/org/jenkinsci/test/acceptance/po/LabelAxis.java
@@ -0,0 +1,24 @@
+package org.jenkinsci.test.acceptance.po;
+
+import org.openqa.selenium.WebElement;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+@AxisPageObjecct("Slaves")
+public class LabelAxis extends Axis {
+    public LabelAxis(PageObject context, String path) {
+        super(context, path);
+    }
+
+    public void select(String name) {
+        WebElement checkBox = find(by.path(path)).findElement(by.xpath(".//input[@name='values' and @json='%s']", name));
+        if (!checkBox.isDisplayed()) {
+            // unfold the labels and slaves sub-nodes
+            find(by.xpath("//div[@class='yahooTree labelAxis-tree']//table[@id='ygtvtableel1']//a")).click();
+            find(by.xpath("//div[@class='yahooTree labelAxis-tree']//table[@id='ygtvtableel2']//a")).click();
+        }
+        checkBox.click();
+    }
+}
+
