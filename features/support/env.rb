4a43739f5896da5a032ec76d67b9b014209d1ad9
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-03-23 08:25:32 -0700

    unused code

--- features/support/env.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-# vim: tabstop=2 expandtab shiftwidth=2
-
-$LOAD_PATH.push File.dirname(__FILE__) + "/../../lib"
-
-require 'capybara/cucumber'
-require 'sauce/cucumber'
-require 'jenkins/cucumber'
-require 'jenkins/env'
