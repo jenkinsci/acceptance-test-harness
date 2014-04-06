3e4012a0039f15d3defca6e2402c270b16c781ac
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-04-06 12:01:06 -0700

    added a test case

--- src/test/java/plugins/WarningsPluginTest.java
+++ src/test/java/plugins/WarningsPluginTest.java
@@ -1,10 +1,18 @@
 package plugins;
 
+import org.hamcrest.Matcher;
+import org.jenkinsci.test.acceptance.Matchers;
 import org.jenkinsci.test.acceptance.junit.AbstractJUnitTest;
 import org.jenkinsci.test.acceptance.junit.WithPlugins;
+import org.jenkinsci.test.acceptance.plugins.warnings.WarningsPublisher;
+import org.jenkinsci.test.acceptance.po.Build;
 import org.jenkinsci.test.acceptance.po.FreeStyleJob;
+import org.jenkinsci.test.acceptance.po.PageObject;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.jenkinsci.test.acceptance.Matchers.*;
 
 /**
  Feature: Adds Warnings collection support
@@ -39,6 +47,27 @@ public class WarningsPluginTest extends AbstractJUnitTest {
     public void detect_no_errors_in_console_log_and_workspace_when_there_are_none() {
         job.configure();
         WarningsPublisher pub = job.addPublisher(WarningsPublisher.class);
+        pub.addConsoleScanner("Maven");
+        pub.addWorkspaceFileScanner("Java Compiler (javac)", "**/*");
+        job.save();
 
+        Build b = job.queueBuild().shouldSucceed();
+
+        assertThatBuildHasWarnings(b, 0,"Java");
+        assertThatBuildHasWarnings(b, 0, "Maven");
+    }
+
+    private void assertThatBuildHasWarnings(Build b, int i, String kind) {
+
+//        job.open();
+//        find(by.xpath("div[@class='test-trend-caption'][text()='%s Warnings Trend']",kind));
+
+        Matcher<PageObject> m = hasAction(String.format("%s Warnings", kind));
+        if (i==0)   m = not(m);
+
+        assertThat(job, m);
+        assertThat(b, m);
+        b.open();
+        assertThat(driver, hasContent(String.format("%s Warnings: %d",kind,i)));
     }
 }
