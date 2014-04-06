bc38deed67b020dbdd454d2d8107f8d44db018e7
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-04-05 17:23:02 -0700

    Ported the other plot test case

--- src/test/java/plugins/PlotPluginTest.java
+++ src/test/java/plugins/PlotPluginTest.java
@@ -1,11 +1,11 @@
 package plugins;
 
 import org.jenkinsci.test.acceptance.junit.AbstractJUnitTest;
+import org.jenkinsci.test.acceptance.junit.Bug;
 import org.jenkinsci.test.acceptance.junit.WithPlugins;
 import org.jenkinsci.test.acceptance.plugins.plot.PlotPublisher;
-import org.jenkinsci.test.acceptance.po.Build;
 import org.jenkinsci.test.acceptance.po.FreeStyleJob;
-import org.jenkinsci.test.acceptance.po.Job;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -16,6 +16,14 @@ import org.junit.Test;
  */
 @WithPlugins("plot")
 public class PlotPluginTest extends AbstractJUnitTest {
+
+    private FreeStyleJob job;
+
+    @Before
+    public void setUp() {
+        job = jenkins.jobs.create();
+    }
+
     /**
      Scenario: Generate simple plot
        Given I have installed the "plot" plugin
@@ -31,20 +39,49 @@ public class PlotPluginTest extends AbstractJUnitTest {
      */
     @Test
     public void generate_simple_plot() {
-        FreeStyleJob job = jenkins.jobs.create();
         job.configure();
         job.copyResource(resource("/plot_plugin/plot.csv"));
         PlotPublisher pub = job.addPublisher(PlotPublisher.class);
         pub.group.set("My group");
         pub.title.set("My plot");
-        pub.source("csv","plot.csv");
+        pub.source("csv", "plot.csv");
         job.save();
 
-        Build b = job.queueBuild().shouldSucceed();
-        assertThatBuildHasPlot(job,"My plot","My group");
+        job.queueBuild().shouldSucceed();
+        assertThatBuildHasPlot("My plot","My group");
     }
 
-    private void assertThatBuildHasPlot(Job job, String title, String group) {
+    /**
+     Scenario: Post-build rendering should work
+       Given I have installed the "plot" plugin
+       And a job
+       When I configure the job
+       And I add plot "Some plot" in group "Plots"
+       And I save the job
+       And I configure the job
+       And I configure csv data source "plot.csv"
+       And I save the job
+       And I build the job
+       Then the build should succeed
+       And there should be a plot called "Some plot" in group "Plots"
+     */
+    @Test @Bug({"JENKINS-18585","JENKINS-18674"})
+    public void postbuild_rendering_should_work() {
+        job.configure();
+        PlotPublisher pub = job.addPublisher(PlotPublisher.class);
+        pub.group.set("Plots");
+        pub.title.set("Some plot");
+        job.save();
+
+        job.configure();
+        pub.source("csv", "plot.csv");
+        job.save();
+
+        job.queueBuild().shouldSucceed();
+        assertThatBuildHasPlot("Some plot","Plots");
+    }
+
+    private void assertThatBuildHasPlot(String title, String group) {
         job.visit("plot");
         find(by.xpath("//h1[contains(text(), '%s')]", group));
         find(by.xpath("//select[@name='choice']/option[contains(text(), '%s')]",title));
