ef22f252ca32f4820a25629198a5744305aa38bd
Oliver Gondža <ogondza@redhat.com>
2014-04-06 23:19:55 +0200

    Ported Multiple SCMs plugin

---
+++ src/main/java/org/jenkinsci/test/acceptance/plugins/multiple_scms/MutlipleScms.java
@@ -0,0 +1,53 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2014 Red Hat, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jenkinsci.test.acceptance.plugins.multiple_scms;
+
+import org.jenkinsci.test.acceptance.po.Control;
+import org.jenkinsci.test.acceptance.po.Job;
+import org.jenkinsci.test.acceptance.po.Scm;
+import org.jenkinsci.test.acceptance.po.ScmPageObject;
+
+@ScmPageObject("Multiple SCMs")
+public class MutlipleScms extends Scm {
+    private final Control addButton = control("hetero-list-add[scmList]");
+
+    private final Job job;
+
+    public MutlipleScms(Job job, String path) {
+        super(job, path);
+        this.job = job;
+    }
+
+    public <T extends Scm> T addScm(Class<T> type) {
+
+        String caption = type.getAnnotation(ScmPageObject.class).value();
+        addButton.click();
+        clickLink(caption);
+
+        sleep(100);
+        String path = last(by.button("Delete SCM")).getAttribute("path");
+
+        return newInstance(type, job, path.substring(0, path.length() - 18));
+    }
+}
