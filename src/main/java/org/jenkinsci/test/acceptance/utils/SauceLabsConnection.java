a312bf1a48fa5ce1b4f08c2e53dc5efa39200bad
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-04-07 14:39:49 -0700

    renaming them to align with http://selenium-client-factory.infradna.com/driver-sauceOnDemand.html

--- src/main/java/org/jenkinsci/test/acceptance/utils/SauceLabsConnection.java
+++ src/main/java/org/jenkinsci/test/acceptance/utils/SauceLabsConnection.java
@@ -24,7 +24,7 @@ public class SauceLabsConnection {
     private String password;
 
     public SauceLabsConnection() throws IOException {
-        File conf = new File(System.getProperty("user.home"),".saucelabs");
+        File conf = new File(System.getProperty("user.home"),".sauce-ondemand");
         if (!conf.exists())
             throw new IOException("SauceLabs connection file is missing: "+conf);
 
@@ -33,10 +33,10 @@ public class SauceLabsConnection {
             props.load(is);
         }
         username = props.getProperty("username");
-        password = props.getProperty("password");
+        password = props.getProperty("key");
 
         if (username==null || password==null)
-            throw new IOException("Missing username/password entries in "+conf);
+            throw new IOException("Missing username/key entries in "+conf);
     }
 
     public WebDriver createWebDriver(DesiredCapabilities caps) throws IOException {
