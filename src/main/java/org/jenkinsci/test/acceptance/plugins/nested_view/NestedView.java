06def7a8ead7cef98e3ba0bc2d8099e1c36095e8
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-04-07 22:00:54 -0700

    copy&paste error

--- src/main/java/org/jenkinsci/test/acceptance/plugins/nested_view/NestedView.java
+++ src/main/java/org/jenkinsci/test/acceptance/plugins/nested_view/NestedView.java
@@ -1,7 +1,6 @@
 package org.jenkinsci.test.acceptance.plugins.nested_view;
 
 import com.google.inject.Injector;
-import org.jenkinsci.test.acceptance.po.Control;
 import org.jenkinsci.test.acceptance.po.Describable;
 import org.jenkinsci.test.acceptance.po.View;
 import org.jenkinsci.test.acceptance.po.ViewsMixIn;
@@ -30,6 +29,6 @@ public class NestedView extends View {
     }
 
     public void assertInactiveView(String name) {
-        find(by.xpath("//table[@id='viewList']//td[@class='inactive' and text()='%s']",name));
+        find(by.xpath("//table[@id='viewList']//td[contains(@class,'inactive')]/a[text()='%s']",name));
     }
 }
