b9f43b82d4c4f5308ebc9cea63d12bc16a29f993
Stephen Connolly <stephen.alan.connolly@gmail.com>
2014-04-16 09:38:27 +0100

    make the jut-server.sh script work from any directory

--- jut-server.sh
+++ jut-server.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-if [ ! -d target ]
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ ! -d "$DIR/target" ]
 then
-  mvn package -DskipTests
+  mvn package -DskipTests -f "$DIR/pom.xml"
 fi
-sh target/appassembler/bin/jut-server "$@"
+sh "$DIR/target/appassembler/bin/jut-server" "$@"
