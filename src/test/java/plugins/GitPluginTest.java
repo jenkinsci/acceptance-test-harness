4e65ab6b86d13e2e7e65c7ba14d5562835258913
Thomas Kieffer <thomaskieffer@web.de>
2014-04-18 17:19:40 +0200

    Running gitplugin tests against docker container

--- src/test/java/plugins/GitPluginTest.java
+++ src/test/java/plugins/GitPluginTest.java
@@ -23,24 +23,53 @@
  */
 package plugins;
 
+import javax.inject.Inject;
+
+import java.net.URL;
+
+import org.jenkinsci.test.acceptance.docker.DockerContainerHolder;
+import org.jenkinsci.test.acceptance.docker.fixtures.GitContainer;
 import org.jenkinsci.test.acceptance.junit.AbstractJUnitTest;
+import org.jenkinsci.test.acceptance.junit.Native;
 import org.jenkinsci.test.acceptance.junit.WithPlugins;
+import org.jenkinsci.test.acceptance.plugins.credentials.ManagedCredentials;
 import org.jenkinsci.test.acceptance.plugins.git.GitScm;
+import org.jenkinsci.test.acceptance.plugins.ssh_credentials.SshPrivateKeyCredential;
 import org.jenkinsci.test.acceptance.po.Job;
+import org.junit.Before;
 import org.junit.Test;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+
+@Native("docker")
 @WithPlugins("git")
 public class GitPluginTest extends AbstractJUnitTest {
 
-    private static final String REPO_URL = "git://github.com/jenkinsci/git-plugin.git";
+    private static final String USERNAME = "gitplugin";
+
+    @Inject
+    DockerContainerHolder<GitContainer> gitServer;
 
     private Job job;
 
-    @Test
-    public void simple_checkout() {
+    private GitContainer container;
+    private String repoUrl;
+
+    @Before
+    public void init() {
+        container = gitServer.get();
+        repoUrl = container.getRepoUrl();
+        addCredentials();
         job = jenkins.jobs.create();
         job.configure();
-        job.useScm(GitScm.class).url(REPO_URL);
+    }
+
+    @Test
+    public void simple_checkout() {
+        job.useScm(GitScm.class)
+                .url(repoUrl)
+                .credentials(USERNAME);
         job.addShellStep("test -f pom.xml");
         job.save();
 
@@ -49,9 +78,10 @@ public class GitPluginTest extends AbstractJUnitTest {
 
     @Test
     public void checkout_branch() {
-        job = jenkins.jobs.create();
-        job.configure();
-        job.useScm(GitScm.class).url(REPO_URL).branch.set("svn");
+        job.useScm(GitScm.class)
+                .url(repoUrl)
+                .credentials(USERNAME)
+                .branch.set("svn");
         job.addShellStep("test `git rev-parse origin/svn` = `git rev-parse HEAD`");
         job.save();
 
@@ -60,20 +90,22 @@ public class GitPluginTest extends AbstractJUnitTest {
 
     @Test
     public void name_remote_repo() {
-        job = jenkins.jobs.create();
-        job.configure();
-        job.useScm(GitScm.class).url(REPO_URL).remoteName("custom_origin");
+        job.useScm(GitScm.class)
+                .url(repoUrl)
+                .credentials(USERNAME)
+                .remoteName("custom_origin");
         job.addShellStep("test -f pom.xml && git remote -v");
         job.save();
 
-        job.startBuild().shouldSucceed().shouldContainsConsoleOutput("custom_origin\\s+" + REPO_URL);
+        job.startBuild().shouldSucceed().shouldContainsConsoleOutput("custom_origin\\s+" + repoUrl);
     }
 
     @Test
     public void checkout_local_branch() {
-        job = jenkins.jobs.create();
-        job.configure();
-        job.useScm(GitScm.class).url(REPO_URL).localBranch("selenium_test_branch");
+        job.useScm(GitScm.class)
+                .url(repoUrl)
+                .credentials(USERNAME)
+                .localBranch("selenium_test_branch");
         job.addShellStep("test `git rev-parse selenium_test_branch` = `git rev-parse HEAD`");
         job.save();
 
@@ -82,9 +114,10 @@ public class GitPluginTest extends AbstractJUnitTest {
 
     @Test
     public void checkout_to_local_dir() {
-        job = jenkins.jobs.create();
-        job.configure();
-        job.useScm(GitScm.class).url(REPO_URL).localDir("local_dir");
+        job.useScm(GitScm.class)
+                .url(repoUrl)
+                .credentials(USERNAME)
+                .localDir("local_dir");
         job.addShellStep("cd local_dir && test -f pom.xml");
         job.save();
 
@@ -93,9 +126,9 @@ public class GitPluginTest extends AbstractJUnitTest {
 
     @Test
     public void poll_for_changes() {
-        job = jenkins.jobs.create();
-        job.configure();
-        job.useScm(GitScm.class).url(REPO_URL);
+        job.useScm(GitScm.class) //
+                .url(container.getRepoUrl())
+                .credentials(USERNAME);
         job.pollScm().schedule("* * * * *");
         job.addShellStep("test -f pom.xml");
         job.save();
@@ -104,5 +137,14 @@ public class GitPluginTest extends AbstractJUnitTest {
 
         // We should have some build after 70 seconds
         job.getLastBuild().shouldSucceed().shouldExist();
+    }
+
+    private void addCredentials() {
+        ManagedCredentials c = new ManagedCredentials(jenkins);
+        c.open();
+        SshPrivateKeyCredential sc = c.add(SshPrivateKeyCredential.class);
+        sc.username.set(USERNAME);
+        sc.selectEnterDirectly().privateKey.set(resource("/ssh_keys/unsafe").asText());
+        c.save();
     }
 }
