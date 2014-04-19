7c7e3406cca7d694fdbc35f60a4f5920e7ef2741
Ulli Hafner <ullrich.hafner@gmail.com>
2014-04-19 19:00:00 +0200

    Mark old form-element-plugin resolving method as deprecated.
    Code still is used by ec2.groovy.

--- ec2.groovy
+++ ec2.groovy
@@ -45,7 +45,7 @@ masters = subworld {
 }
 
 joc = subworld {
-    bind JenkinsController toInstance new WinstoneController(localWar, null)
+    bind JenkinsController toInstance new WinstoneController(localWar, JenkinsController.downloadPathElement())
 }
 
 bind SlaveProvider toProvider slaves[SshSlaveProvider]
