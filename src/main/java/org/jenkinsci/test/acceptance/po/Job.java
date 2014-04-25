4520e3d71e28b86e76b7499b41aa7097d35d4185
Oliver Gondža <ogondza@redhat.com>
2014-04-25 10:52:20 +0200

    Document Describable annotation

--- src/main/java/org/jenkinsci/test/acceptance/po/Job.java
+++ src/main/java/org/jenkinsci/test/acceptance/po/Job.java
@@ -27,6 +27,10 @@ import java.util.zip.GZIPOutputStream;
 import static org.jenkinsci.test.acceptance.Matchers.*;
 
 /**
+ * Job Page object superclass.
+ *
+ * Use {@link Describable} annotation to register an implementation.
+ *
  * @author Kohsuke Kawaguchi
  */
 public class Job extends ContainerPageObject {
