eaef1e7eefcb4a8bb9952d904b3cc083f0c2e2a0
Fabian Trampusch <fabian.trampusch@freenet.de>
2014-04-27 20:19:13 +0200

    testing the checkstyle-tabs

--- src/main/java/org/jenkinsci/test/acceptance/plugins/checkstyle/CheckstyleAction.java
+++ src/main/java/org/jenkinsci/test/acceptance/plugins/checkstyle/CheckstyleAction.java
@@ -4,6 +4,7 @@ import org.jenkinsci.test.acceptance.po.ContainerPageObject;
 import org.openqa.selenium.WebElement;
 
 import java.net.URL;
+import java.util.*;
 
 /**
  * Page object for Checkstyle action.
@@ -52,5 +53,61 @@ public class CheckstyleAction extends ContainerPageObject {
 
     private int asInt(WebElement e) {
         return Integer.parseInt(e.getText().trim());
+    }
+
+    public SortedMap<String, Integer> getFileNamesAndNumberOfWarnings() {
+        open();
+        final List<WebElement> rows = all(by.xpath("//table[@id='files']/tbody/tr"));
+
+        // Remove footer and header
+        rows.remove(0);
+        rows.remove(rows.size() - 1);
+
+        // map rows to "file => number warnings"
+        return mapTableCellsKeyValue(rows);
+    }
+
+    public SortedMap<String, Integer> getCategoriesAndNumberOfWarnings() {
+        open();
+        find(by.xpath(".//A[@href]/em[text() = 'Categories']")).click();
+
+        final List<WebElement> rows = all(by.xpath("(//table[@id='modules'])[1]/tbody/tr")); // a real unique identifier would help here
+
+        // Remove footer and header
+        rows.remove(0);
+        rows.remove(rows.size() - 1);
+
+        // map rows to "Category => number warnings"
+        return mapTableCellsKeyValue(rows);
+    }
+
+    public SortedMap<String, Integer> getTypesAndNumberOfWarnings() {
+        open();
+        find(by.xpath(".//A[@href]/em[text() = 'Categories']")).click(); // intentional, the xpath-selector is not robust without this
+        find(by.xpath(".//A[@href]/em[text() = 'Types']")).click();
+
+        final List<WebElement> rows = all(by.xpath("(//table[@id='modules'])[2]/tbody/tr")); // a real unique identifier would help here too
+
+        // Remove footer and header
+        rows.remove(0);
+        rows.remove(rows.size() - 1);
+
+        // map rows to "Type => number warnings"
+        return mapTableCellsKeyValue(rows);
+    }
+
+    private SortedMap<String, Integer> mapTableCellsKeyValue(final List<WebElement> rows) {
+        final SortedMap<String, Integer> result = new TreeMap<String, Integer>();
+        for(WebElement elem : rows) {
+            final List<WebElement> cells = elem.findElements(by.xpath("./td"));
+            final String key = asTrimmedString(cells.get(0));
+            final Integer value = asInt(cells.get(1));
+            result.put(key, value);
+        }
+        return result;
+    }
+
+    private String asTrimmedString(final WebElement webElement) {
+        return webElement.getText().trim();
     }
 }
--- src/test/java/plugins/CheckstylePluginTest.java
+++ src/test/java/plugins/CheckstylePluginTest.java
@@ -3,6 +3,8 @@ package plugins;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import org.apache.xerces.jaxp.DocumentBuilderFactoryImpl;
 import org.custommonkey.xmlunit.XMLAssert;
@@ -70,16 +72,81 @@ public class CheckstylePluginTest extends AbstractJUnitTest {
      */
     @Test
     public void view_checkstyle_report() {
-        FreeStyleJob job = setupJob();
-        Build b = job.queueBuild().waitUntilFinished().shouldSucceed();
+        final FreeStyleJob job = setupJob();
+        final Build b = job.queueBuild().waitUntilFinished().shouldSucceed();
+        final CheckstyleAction ca = new CheckstyleAction(job);
 
-        CheckstyleAction ca = new CheckstyleAction(job);
+        assertWarningsTrendAndSummary(ca);
+        assertFileTab(ca);
+        assertCategoryTab(ca);
+        assertTypeTab(ca);
+    }
+
+    private void assertWarningsTrendAndSummary(final CheckstyleAction ca) {
         assertThat(ca.getWarningNumber(), is(776));
         assertThat(ca.getNewWarningNumber(), is(776));
         assertThat(ca.getFixedWarningNumber(), is(0));
         assertThat(ca.getHighWarningNumber(), is(776));
         assertThat(ca.getNormalWarningNumber(), is(0));
         assertThat(ca.getLowWarningNumber(), is(0));
+    }
+
+    private void assertTypeTab(final CheckstyleAction ca) {
+        final SortedMap<String, Integer> expectedTypes = new TreeMap<String, Integer>();
+        expectedTypes.put("AvoidInlineConditionalsCheck", 9);
+        expectedTypes.put("AvoidStarImportCheck", 1);
+        expectedTypes.put("ConstantNameCheck", 1);
+        expectedTypes.put("DesignForExtensionCheck", 35);
+        expectedTypes.put("EmptyBlockCheck", 1);
+        expectedTypes.put("FileTabCharacterCheck", 47);
+        expectedTypes.put("FinalParametersCheck", 120);
+        expectedTypes.put("HiddenFieldCheck", 44);
+        expectedTypes.put("JavadocMethodCheck", 88);
+        expectedTypes.put("JavadocPackageCheck", 1);
+        expectedTypes.put("JavadocStyleCheck", 9);
+        expectedTypes.put("JavadocTypeCheck", 3);
+        expectedTypes.put("JavadocVariableCheck", 3);
+        expectedTypes.put("LineLengthCheck", 160);
+        expectedTypes.put("MagicNumberCheck", 8);
+        expectedTypes.put("MethodNameCheck", 1);
+        expectedTypes.put("NeedBracesCheck", 26);
+        expectedTypes.put("ParameterNameCheck", 2);
+        expectedTypes.put("ParameterNumberCheck", 4);
+        expectedTypes.put("RegexpSinglelineCheck", 23);
+        expectedTypes.put("RightCurlyCheck", 1);
+        expectedTypes.put("TodoCommentCheck", 3);
+        expectedTypes.put("UnusedImportsCheck", 2);
+        expectedTypes.put("VisibilityModifierCheck", 12);
+        expectedTypes.put("WhitespaceAfterCheck", 66);
+        expectedTypes.put("WhitespaceAroundCheck", 106);
+        assertThat(ca.getTypesAndNumberOfWarnings(), is(expectedTypes));
+    }
+
+    private void assertCategoryTab(final CheckstyleAction ca) {
+        final SortedMap<String, Integer> expectedCategories = new TreeMap<String, Integer>();
+        expectedCategories.put("Blocks", 28);
+        expectedCategories.put("Checks", 123);
+        expectedCategories.put("Coding", 61);
+        expectedCategories.put("Design", 47);
+        expectedCategories.put("Imports", 3);
+        expectedCategories.put("Javadoc", 104);
+        expectedCategories.put("Naming", 4);
+        expectedCategories.put("Regexp", 23);
+        expectedCategories.put("Sizes", 164);
+        expectedCategories.put("Whitespace", 219);
+        assertThat(ca.getCategoriesAndNumberOfWarnings(), is(expectedCategories));
+    }
+
+    private void assertFileTab(final CheckstyleAction ca) {
+        final SortedMap<String, Integer> expectedFileDetails = new TreeMap<String, Integer>();
+        expectedFileDetails.put("JavaProvider.java", 18);
+        expectedFileDetails.put("PluginImpl.java", 8);
+        expectedFileDetails.put("RemoteLauncher.java", 63);
+        expectedFileDetails.put("SFTPClient.java", 76);
+        expectedFileDetails.put("SFTPFileSystem.java", 34);
+        expectedFileDetails.put("SSHConnector.java", 96);
+        expectedFileDetails.put("SSHLauncher.java", 481);
+        assertThat(ca.getFileNamesAndNumberOfWarnings(), is(expectedFileDetails));
     }
 
     @Test
