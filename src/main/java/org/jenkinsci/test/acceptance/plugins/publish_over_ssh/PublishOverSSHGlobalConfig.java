6f93e646739b0152ce9ab5a607daf3db93765b36
Stoffl <stoffl@fs.cs.hm.edu>
2014-04-30 00:53:11 +0200

    First SSH Plugin Test

---
+++ src/main/java/org/jenkinsci/test/acceptance/plugins/publish_over_ssh/PublishOverSSHGlobalConfig.java
@@ -0,0 +1,85 @@
+package org.jenkinsci.test.acceptance.plugins.publish_over_ssh;
+
+import org.jenkinsci.test.acceptance.controller.JenkinsController;
+import org.jenkinsci.test.acceptance.po.Control;
+import org.jenkinsci.test.acceptance.po.Jenkins;
+import org.jenkinsci.test.acceptance.po.PageArea;
+import org.jenkinsci.test.acceptance.po.PageObject;
+
+import javax.inject.Inject;
+
+/**
+ *
+ */
+public class PublishOverSSHGlobalConfig extends PageArea {
+    @Inject
+    JenkinsController controller;
+
+    public final Control add = control("repeatable-add");
+
+    @Inject
+    public PublishOverSSHGlobalConfig(Jenkins jenkins) {
+        super(jenkins, "/jenkins-plugins-publish_over_ssh-BapSshPublisherPlugin");
+    }
+
+    public CommonConfig setCommonConfig() {
+        String p = last(by.xpath(".//tr[@name='commonConfig'][starts-with(@path,'/jenkins-plugins-publish_over_ssh-BapSshPublisherPlugin/')]")).getAttribute("path");
+        return new CommonConfig(page,p);
+    }
+
+    public static class CommonConfig extends PageArea {
+        public CommonConfig(PageObject parent, String path) {
+            super(parent, path);
+        }
+
+        // global controls for all sites
+        public final Control encryptedPassphrase = control("encryptedPassphrase");
+        public final Control keyPath = control("keyPath");
+        public final Control key = control("key");
+
+        public final Control disableAllExecGlobal = control("disableAllExec");
+
+        // Dropdown list for Publisher config class="setting-input dropdownList"
+    }
+
+    public InstanceSite addInstanceSite() {
+        add.click();
+        String p = last(by.xpath(".//div[@name='instance'][starts-with(@path,'/jenkins-plugins-publish_over_ssh-BapSshPublisherPlugin/')]")).getAttribute("path");
+        return new InstanceSite(page,p);
+    }
+
+    public static class InstanceSite extends PageArea {
+        public InstanceSite(PageObject parent, String path) {
+            super(parent, path);
+        }
+
+        // instance controls
+        public final Control name = control("name");
+        public final Control hostname = control("hostname");
+        public final Control username = control("username");
+        public final Control remoteRootDir = control("remoteRootDir");
+
+        // advanced config button
+        public final Control addAdvancedConfig = control("advanced-button");
+
+        public AdvancedConfig addAdvancedConfig(){
+            addAdvancedConfig.click();
+            String p = last(by.xpath(".//div[@name='instance'][starts-with(@path,'/jenkins-plugins-publish_over_ssh-BapSshPublisherPlugin/')]")).getAttribute("path");
+            return new AdvancedConfig(page,p);
+        }
+
+        public final Control validate = control("validate-button");
+        public final Control delete = control("repeatable-delete");
+    }
+
+    public static class AdvancedConfig extends PageArea {
+        public AdvancedConfig(PageObject parent, String path) {
+            super(parent, path);
+        }
+
+        public final Control port = control("port");
+        public final Control timeout = control("timeout");
+        public final Control disableAllExecInstance = control("disableAllExec");
+    }
+
+}
---
+++ src/test/java/plugins/PublishOverSSHPluginTest.java
@@ -0,0 +1,138 @@
+package plugins;
+
+import com.google.common.io.LineReader;
+import com.google.inject.Inject;
+import org.apache.commons.io.FileUtils;
+import org.hamcrest.CoreMatchers;
+import org.jenkinsci.test.acceptance.docker.Docker;
+import org.jenkinsci.test.acceptance.docker.fixtures.SshdContainer;
+import org.jenkinsci.test.acceptance.junit.AbstractJUnitTest;
+import org.jenkinsci.test.acceptance.junit.Native;
+import org.jenkinsci.test.acceptance.junit.Resource;
+import org.jenkinsci.test.acceptance.junit.WithPlugins;
+import org.jenkinsci.test.acceptance.plugins.publish_over_ssh.PublishOverSSHGlobalConfig.AdvancedConfig;
+import org.jenkinsci.test.acceptance.plugins.publish_over_ssh.PublishOverSSHGlobalConfig.CommonConfig;
+import org.jenkinsci.test.acceptance.plugins.publish_over_ssh.PublishOverSSHGlobalConfig.InstanceSite;
+import org.jenkinsci.test.acceptance.plugins.publish_over_ssh.PublishOverSSHPublisher;
+import org.jenkinsci.test.acceptance.plugins.publish_over_ssh.PublishOverSSHGlobalConfig;
+import org.jenkinsci.test.acceptance.plugins.publish_over_ssh.PublishOverSSHPublisher;
+import org.jenkinsci.test.acceptance.plugins.publish_over_ssh.PublishOverSSHPublisher.Publishers;
+import org.jenkinsci.test.acceptance.plugins.publish_over_ssh.PublishOverSSHPublisher.TransferSet;
+import org.jenkinsci.test.acceptance.po.FreeStyleJob;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ Feature: Tests for SSH plugin
+ */
+@WithPlugins("scp")
+public class PublishOverSSHPluginTest extends AbstractJUnitTest {
+    @Inject
+    Docker docker;
+    @Native("docker")
+    /**
+     @native(docker)
+     Scenario: Configure a job with SCP publishing
+     Given I have installed the "scp" plugin
+     And a docker fixture "sshd"
+     And a job
+     When I configure docker fixture as SCP site
+     And I configure the job
+     And I copy resource "pmd_plugin/pmd.xml" into workspace
+     And I publish "pmd.xml" with SCP plugin
+     And I save the job
+     And I build the job
+     Then the build should succeed
+     And SCP plugin should have published "pmd.xml" on docker fixture
+     */
+    @Test
+
+    public void configure_job_with_ssh_key_file_and_key_password_publishing() throws IOException, InterruptedException {
+        SshdContainer sshd = docker.start(SshdContainer.class);
+        Resource cp_file = resource("/scp_plugin/lorem-ipsum-scp.txt");
+        Resource ssh_priv_key = resource("/ssh_keys/unsafe");
+        File sshFile = ssh_priv_key.asFile();
+        BufferedReader sshReader = new BufferedReader(new FileReader(sshFile));
+
+        FreeStyleJob j = jenkins.jobs.create();
+
+        jenkins.configure();
+        CommonConfig cc = new PublishOverSSHGlobalConfig(jenkins).setCommonConfig(); {
+            cc.encryptedPassphrase.set("jenkins-ci");
+            cc.keyPath.set(resource("/ssh_keys/unsafe").asFile().getAbsolutePath());
+
+            // for private key copied in form
+//            String ssh_priv_key_string="";
+//
+//            while(sshReader.ready()){
+//                ssh_priv_key_string= ssh_priv_key_string + sshReader.readLine();
+//            }
+//            cc.key.set(ssh_priv_key_string);
+
+
+            //or like this:
+            // String ssh_priv_key_string = FileUtils.readFileToString(sshFile);
+
+            // for disabling exec global
+            //cc.disableAllExecGlobal.check();
+
+        }
+
+        InstanceSite is = new PublishOverSSHGlobalConfig(jenkins).addInstanceSite(); {
+            is.name.set("testSSHserver");
+            is.hostname.set(sshd.ipBound(22));
+            is.username.set("test");
+            is.remoteRootDir.set("/tmp");
+
+        }
+
+        AdvancedConfig ac = is.addAdvancedConfig(); {
+            ac.port.set(sshd.port(22));
+            ac.timeout.set("300000");
+
+            // for disabling exec per instance
+            //ac.disableAllExecInstance.check();
+        }
+
+        // delete button
+        //is.delete.click();
+
+        // validate input
+        //is.validate.click();
+        jenkins.save();
+
+        j.configure(); {
+            j.copyResource(cp_file);
+            PublishOverSSHPublisher popsp = j.addPublisher(PublishOverSSHPublisher.class);
+            // set default set
+            Publishers publishers = popsp.setPublishers();
+            // add new set
+            //Publishers newSps = popsp.addPublishers();
+            // set default set
+            TransferSet ts = publishers.setTransferSet();
+            // add new set
+            //TransferSet newTs = publishers.addTransferSet();
+            ts.sourceFiles.set("lorem-ipsum-scp.txt");
+            //ts.remoteDirectory.set("testfolder");
+            //ts.removePrefix.set("/tmp/");
+            //ts.execCommand.set("echo 'i was here' >> /tmp/testecho");
+        }
+        j.save();
+
+        j.startBuild().shouldSucceed();
+
+        sshd.cp("/tmp/lorem-ipsum-scp.txt", new File("/tmp"));
+        assertThat(FileUtils.readFileToString(new File("/tmp/lorem-ipsum-scp.txt")), CoreMatchers.is(cp_file.asText()));
+    }
+
+
+    //public void configure_job_with_ssh_key_path_and_key_password_publishing() throws IOException, InterruptedException {
+    // public void configure_job_with_ssh_key_text_and_key_password_publishing() throws IOException, InterruptedException {
+    // public void configure_job_with_ssh_key_text_and_no_password_publishing() throws IOException, InterruptedException {
+    // public void configure_job_with_ssh_key_path_and_no_password_publishing() throws IOException, InterruptedException {
+    // public void configure_job_with_ssh_key_file_and_no_password_publishing() throws IOException, InterruptedException {
+}
