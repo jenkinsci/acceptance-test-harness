4659a69fa0064ab5f5e9e2eeb01fc6a4b70dd0a7
Martin Ende <ende@hm.edu>
2014-05-03 06:34:11 +0200

    NodeParameter: added remaining members to access all radio buttons and node eligibility options of node parameter panel

--- src/main/java/org/jenkinsci/test/acceptance/plugins/nodelabelparameter/NodeParameter.java
+++ src/main/java/org/jenkinsci/test/acceptance/plugins/nodelabelparameter/NodeParameter.java
@@ -11,10 +11,16 @@ import org.openqa.selenium.WebElement;
  */
 @Describable("Node")
 public class NodeParameter extends Parameter {
+    public final Control runIfSuccess = control("triggerIfResult[success]");
+    public final Control runIfUnstable = control("triggerIfResult[unstable]");
+    public final Control runAllCases = control("triggerIfResult[allCases]");
+
     public final Control allowMultiple = control("triggerIfResult[allowMultiSelectionForConcurrentBuilds]");
+    public final Control disallowMultiple = control("triggerIfResult[multiSelectionDisallowed]");
 
     public final WebElement allNodes = find(by.xpath("//option[text()[normalize-space(.)='All Nodes']]"));
     public final WebElement ignoreOffline = find(by.xpath("//option[text()[normalize-space(.)='Ignore Offline Nodes']]"));
+    public final WebElement ignoreTempffline = find(by.xpath("//option[text()[normalize-space(.)='Ignore Temp Offline Nodes']]"));
 
     public NodeParameter(Job job, String path) {
         super(job, path);
