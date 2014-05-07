ce2c45ed60a2d4415ba75161caeb1c897c5034ca
Marco Miller <marco.miller@ericsson.com>
2014-05-07 09:38:26 -0400

    [FIXED JENKINS-22788] Optional env. proxy support; slight usage change.

--- src/main/java/org/jenkinsci/test/acceptance/plugins/gerrit_trigger/GerritTriggerEnv.java
+++ src/main/java/org/jenkinsci/test/acceptance/plugins/gerrit_trigger/GerritTriggerEnv.java
@@ -37,12 +37,16 @@ public class GerritTriggerEnv {
     private String hostName;
     private String project;
     private String userHome;
+    //
+    private boolean noProxy;
 
     private GerritTriggerEnv() {
         gerritUser = getEnv("gtGerrituser");
         hostName = getEnv("gtHostname");
         project = getEnv("gtProject");
         userHome = getEnv("gtUserhome");
+        //then,
+        noProxy = getEnvNP("gtNoProxyForHost");
     }
 
     public static GerritTriggerEnv getInstance() {
@@ -60,6 +64,10 @@ public class GerritTriggerEnv {
         return hostName;
     }
 
+    public boolean getNoProxy() {
+        return noProxy;
+    }
+
     public String getProject() {
         return project;
     }
@@ -72,5 +80,9 @@ public class GerritTriggerEnv {
         String env = System.getenv(name);
         assumeNotNull(env);
         return env;
+    }
+
+    private boolean getEnvNP(String name) {
+        return System.getenv(name) != null;
     }
 }
