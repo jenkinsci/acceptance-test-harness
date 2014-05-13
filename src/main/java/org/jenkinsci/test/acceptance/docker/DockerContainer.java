9f9412e76dd170ed98f00e2e7297e89157dd4dfb
Tobias Meyer <tobiuhg@gmail.com>
2014-05-13 11:28:28 +0200

    Returned to package

--- src/main/java/org/jenkinsci/test/acceptance/docker/DockerContainer.java
+++ src/main/java/org/jenkinsci/test/acceptance/docker/DockerContainer.java
@@ -23,7 +23,7 @@ public class DockerContainer implements Closeable {
     private File logfile;
     private Thread shutdownHook;
 
-    protected void init(String cid, Process p, File logfile) {
+    /* package */ void init(String cid, Process p, File logfile) {
         this.cid = cid;
         this.p = p;
         this.logfile = logfile;
