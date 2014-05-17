fd63192f5c6df02a5172b00b2e29a61a7e1a936b
Martin Ende <ende@hm.edu>
2014-05-17 19:58:41 +0200

    TextFinderPublisher: added Describable annotation TextFinderPublisher: added public final members to access the text fields and checkboxes

--- src/main/java/org/jenkinsci/test/acceptance/plugins/textfinder/TextFinderPublisher.java
+++ src/main/java/org/jenkinsci/test/acceptance/plugins/textfinder/TextFinderPublisher.java
@@ -1,14 +1,24 @@
 package org.jenkinsci.test.acceptance.plugins.textfinder;
 
+import org.jenkinsci.test.acceptance.po.Control;
+import org.jenkinsci.test.acceptance.po.Describable;
 import org.jenkinsci.test.acceptance.po.Job;
 import org.jenkinsci.test.acceptance.po.PostBuildStep;
+import org.openqa.selenium.WebElement;
 
 /**
  * Created by Martin Ende on 5/17/14.
  */
 
-
+@Describable("Jenkins Text Finder")
 public class TextFinderPublisher extends PostBuildStep {
+
+    public final WebElement filePath = find(by.xpath("//input[@name='_.fileSet']"));
+    public final WebElement regEx =find(by.xpath("//input[@name='_.regexp']"));
+    public final WebElement succeedIfFound = find(by.xpath("//input[@name='_.succeedIfFound']"));
+    public final WebElement unstableIfFound = find(by.xpath("//input[@name='_.unstableIfFound']"));
+
+
 
     public TextFinderPublisher(Job parent, String path) {
         super(parent, path);
