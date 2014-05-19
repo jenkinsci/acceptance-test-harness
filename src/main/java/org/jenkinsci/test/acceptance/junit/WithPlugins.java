1aedbbcbec207d98a86848648aa45d50abe9f8f3
Ulli Hafner <ullrich.hafner@gmail.com>
2014-05-19 23:46:46 +0200

    [FIXED JENKINS-23093][JENKINS-23087] Fixed WithPlugins annotation. Now existing plugin will be overwritten with newer version if env var NEVER_REPLACE_EXISTING_PLUGINS is not set.

--- src/main/java/org/jenkinsci/test/acceptance/junit/WithPlugins.java
+++ src/main/java/org/jenkinsci/test/acceptance/junit/WithPlugins.java
@@ -1,6 +1,9 @@
 package org.jenkinsci.test.acceptance.junit;
 
-import com.google.inject.Inject;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
 
 import org.jenkinsci.test.acceptance.controller.JenkinsController;
 import org.jenkinsci.test.acceptance.po.Jenkins;
@@ -11,10 +14,7 @@ import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.Inherited;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
+import com.google.inject.Inject;
 
 import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.*;
@@ -24,15 +24,20 @@ import static java.lang.annotation.RetentionPolicy.*;
  * <p/>
  * Example: @WithPlugin("subversion")
  * <p/>
- * One can specify a specific version after the plugin name with a suffixed '@'
+ * One can specify a specific minimum version after the plugin name with a suffixed '@'.
+ * <p/>
  * Example: @WithPlugin("subversion@1.54")
  * <p/>
- * The annotation guarantees that the plugin is installed in required or later version.
- * If required version is not available in update center, the test will fail. If the
- * plugin is already installed but not in correct version the test will be skipped.
+ * The annotation guarantees that the plugin is installed in required or later version. If required version is not
+ * available in update center, the test will fail. If the plugin is already installed but not in correct version then
+ * the environment variable NEVER_REPLACE_EXISTING_PLUGINS is evaluated:
+ * <ul>
+ *     <li>if the environment variable is set then the test will be skipped.</li>
+ *     <li>if the environment variable is undefined then the installed version of the plugin is overwritten with
+ *     the latest version of the plugin.</li>
+*  </ul>
  * <p/>
- * When running tests, this annotation triggers {@link JenkinsAcceptanceTestRule}
- * to install all the plugins.
+ * When running tests, this annotation triggers {@link JenkinsAcceptanceTestRule} to install all the plugins.
  * <p/>
  * We also want to use this to filter tests, especially for non-destructive tests.
  *
@@ -75,17 +80,21 @@ public @interface WithPlugins {
                         if (!pm.isInstalled(name)) {
                             pm.installPlugins(name);
                         }
-
-                        String requiredVersion = candidate.getVersion();
-                        if (requiredVersion != null) {
-                            if (!jenkins.getPlugin(name).isNewerThan(requiredVersion)) {
-                                throw new AssumptionViolatedException(String.format(
-                                        "Test requires %s plugin in version %s", name, requiredVersion
-                                ));
+                        else {
+                            String requiredVersion = candidate.getVersion();
+                            if (requiredVersion != null) {
+                                if (!jenkins.getPlugin(name).isNewerThan(requiredVersion)) {
+                                    if (System.getenv("NEVER_REPLACE_EXISTING_PLUGINS") != null) {
+                                        throw new AssumptionViolatedException(String.format(
+                                                "Test requires %s plugin in version %s", name, requiredVersion));
+                                    }
+                                    else {
+                                        pm.installPlugins(name);
+                                    }
+                                }
                             }
                         }
                     }
-
                     return true;
                 }
             };
