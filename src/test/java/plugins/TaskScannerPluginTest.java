102533f8d9f8769065e2f71aebbed84ccd93c1e7
Martin Ende <ende@hm.edu>
2014-05-27 22:13:20 +0200

    ADDED: a subclass of AbstractCodeStylePluginPostBuildStep to add a Task Scanner Publisher to the job ADDED: frame of a new test class for testing the Task Scanner plugin

---
+++ src/test/java/plugins/TaskScannerPluginTest.java
@@ -0,0 +1,14 @@
+package plugins;
+
+
+/**
+ Feature: Scan for open tasks
+ In order to be able to collect and analyse open tasks.
+ As a Jenkins user
+ I want to install and configure Task Scanner plugin
+
+  @author Martin Ende
+ */
+public class TaskScannerPluginTest extends AbstractCodeStylePluginHelper{
+
+}
