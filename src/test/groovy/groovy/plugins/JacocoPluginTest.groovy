35fa803672336a0fa490fae4135132fa12997d6a
Christian Fritz <mail@chr-fritz.de>
2014-06-03 12:35:33 +0200

    Extend the jacoco plugin test to check teh summery coverage

--- src/test/groovy/groovy/plugins/JacocoPluginTest.groovy
+++ src/test/groovy/groovy/plugins/JacocoPluginTest.groovy
@@ -3,6 +3,7 @@ package groovy.plugins
 import org.jenkinsci.test.acceptance.geb.GebSpec
 import org.jenkinsci.test.acceptance.junit.WithPlugins
 import org.jenkinsci.test.acceptance.plugins.jacoco.JacocoPublisher
+import org.jenkinsci.test.acceptance.plugins.jacoco.JacocoResultPage
 import org.jenkinsci.test.acceptance.po.Job
 
 /**
@@ -14,7 +15,7 @@ import org.jenkinsci.test.acceptance.po.Job
 class JacocoPluginTest extends GebSpec {
     private Job job;
 
-    def "Check successfully execution"() {
+    def "Check successfully execution and summary"() {
         given: "A job with jacoco results"
         job = jenkins.jobs.create();
         job.copyDir(resource("/jacoco/test"));
@@ -23,9 +24,11 @@ class JacocoPluginTest extends GebSpec {
         job.save();
 
         when: "the build started and succeed"
-        job.startBuild().waitUntilFinished().shouldSucceed();
+        def build = job.startBuild().waitUntilFinished().shouldSucceed();
 
         then: "jacoco appears within the navigation area."
-        assert job.navigationLinks.containsValue("Coverage Trend")
+        assert build.navigationLinks.containsValue("Coverage Report")
+        to JacocoResultPage, build
+        assert summaryCoverage.branch == 50.0
     }
 }
