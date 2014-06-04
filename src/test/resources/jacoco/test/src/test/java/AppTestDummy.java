4f240d6dfe46b7f960c01a4147728c51924e6270
Christian Fritz <mail@chr-fritz.de>
2014-06-04 14:24:13 +0200

    Renamed AppTest into AppTestDummy 

--- src/test/resources/jacoco/test/src/test/java/AppTestDummy.java
+++ src/test/resources/jacoco/test/src/test/java/AppTestDummy.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 /**
  * Unit test for simple App.
  */
-public class AppTest {
+public class AppTestDummy {
     @Test
     public void testMain() throws Exception {
         assertEquals(App.main(), -1);
