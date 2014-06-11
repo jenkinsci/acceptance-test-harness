856652237d00b8fa42d0de0db7f0f9b39af9bf81
Michael Prankl <eidottermihi@gmail.com>
2014-06-11 19:51:23 +0200

    First simple test for analysis-collector plugin.

--- src/test/java/plugins/AnalysisCollectorPluginTest.java
+++ src/test/java/plugins/AnalysisCollectorPluginTest.java
@@ -4,6 +4,7 @@ import org.jenkinsci.test.acceptance.junit.AbstractJUnitTest;
 import org.jenkinsci.test.acceptance.junit.Resource;
 import org.jenkinsci.test.acceptance.junit.WithPlugins;
 import org.jenkinsci.test.acceptance.plugins.AbstractCodeStylePluginPostBuildStep;
+import org.jenkinsci.test.acceptance.plugins.analysis_collector.AnalysisCollectorAction;
 import org.jenkinsci.test.acceptance.plugins.analysis_collector.AnalysisCollectorPublisher;
 import org.jenkinsci.test.acceptance.plugins.checkstyle.CheckstylePublisher;
 import org.jenkinsci.test.acceptance.plugins.findbugs.FindbugsPublisher;
@@ -16,6 +17,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.jenkinsci.test.acceptance.Matchers.hasAction;
 import static org.junit.Assert.assertThat;
 
@@ -27,41 +29,46 @@ import static org.junit.Assert.assertThat;
 @WithPlugins({"analysis-collector", "checkstyle", "pmd", "findbugs", "tasks"})
 public class AnalysisCollectorPluginTest extends AbstractJUnitTest {
 
+    /**
+     * Scenario: First build with new warnings
+     * Given I have job with artifacts of static analysis tools
+     * And this artifacts are published by their corresponding plugins
+     * When I add a post-build step to publish a combined static analyis result
+     * Then the job and build will have a action "Static Analysis Warning"
+     * And this action will show the combined static analysis result
+     */
     @Test
-    public void test() {
-        FreeStyleJob job = setupJob("/analysis_collector_plugin", publishers());
-        Build lastBuild = job.startBuild();
+    public void first_build_new_warnings() {
+        FreeStyleJob job = setupJob("/analysis_collector_plugin");
+        Build lastBuild = job.startBuild().waitUntilFinished();
         assertThat(job, hasAction("Static Analysis Warnings"));
         assertThat(lastBuild, hasAction("Static Analysis Warnings"));
-        // TODO Resources for job with FindBugs, Checkstyle, PMD, Tasks, Dry
-        // TODO PO for Static Analysis Results
-    }
-
-    private <T extends AbstractCodeStylePluginPostBuildStep> List<Class<T>> publishers() {
-        final List<Class<T>> publishers = new ArrayList<>();
-        publishers.add((Class<T>) CheckstylePublisher.class);
-        publishers.add((Class<T>) PmdPublisher.class);
-        publishers.add((Class<T>) FindbugsPublisher.class);
-        publishers.add((Class<T>) TaskScannerPublisher.class);
-        publishers.add((Class<T>) AnalysisCollectorPublisher.class);
-        return publishers;
+        AnalysisCollectorAction result = new AnalysisCollectorAction(job);
+        assertThat(result.getWarningNumber(), is(795));
+        assertThat(result.getHighWarningNumber(), is(779));
+        assertThat(result.getNormalWarningNumber(), is(9));
+        assertThat(result.getLowWarningNumber(), is(7));
+        assertThat(result.getNewWarningNumber(), is(795));
     }
 
     /**
-     * Setup a job with given resources and publishers.
+     * Setup a job with given resources and needed publishers.
      *
-     * @param resourceToCopy   Resource to copy to build (Directory or File path)
-     * @param publisherClasses Publishers to add
-     * @param <T>              Type of the publisher
+     * @param resourceToCopy Resource to copy to build (Directory or File path)
      * @return the made job
      */
-    public <T extends AbstractCodeStylePluginPostBuildStep> FreeStyleJob setupJob(String resourceToCopy, List<Class<T>> publisherClasses) {
+    public FreeStyleJob setupJob(String resourceToCopy) {
         final FreeStyleJob job = jenkins.jobs.create();
         job.configure();
         copyResources(resourceToCopy, job);
-        for (Class<T> publisher : publisherClasses) {
-            job.addPublisher(publisher);
-        }
+        job.addPublisher(CheckstylePublisher.class);
+        job.addPublisher(PmdPublisher.class);
+        job.addPublisher(FindbugsPublisher.class);
+        TaskScannerPublisher taskScannerPublisher = job.addPublisher(TaskScannerPublisher.class);
+        taskScannerPublisher.highPriorityTags.sendKeys("PRIO1");
+        taskScannerPublisher.normalPriorityTags.sendKeys("PRIO2,TODO");
+        taskScannerPublisher.lowPriorityTags.sendKeys("PRIO3");
+        job.addPublisher(AnalysisCollectorPublisher.class);
         job.save();
         return job;
     }
