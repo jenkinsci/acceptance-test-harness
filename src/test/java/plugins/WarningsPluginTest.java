335ce098422da408cd8c11da346baf7b0cafaede
Ulli Hafner <ullrich.hafner@gmail.com>
2014-08-14 19:19:06 +0200

    Improved test cases to use analysis base classes.

--- src/test/java/plugins/WarningsPluginTest.java
+++ src/test/java/plugins/WarningsPluginTest.java
@@ -5,11 +5,9 @@ import org.jenkinsci.test.acceptance.junit.WithPlugins;
 import org.jenkinsci.test.acceptance.plugins.analysis_core.AbstractCodeStylePluginBuildConfigurator;
 import org.jenkinsci.test.acceptance.plugins.warnings.WarningsAction;
 import org.jenkinsci.test.acceptance.plugins.warnings.WarningsBuildSettings;
-import org.jenkinsci.test.acceptance.plugins.warnings.WarningsPublisher;
 import org.jenkinsci.test.acceptance.po.Build;
 import org.jenkinsci.test.acceptance.po.FreeStyleJob;
 import org.jenkinsci.test.acceptance.po.Job;
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.*;
@@ -17,159 +15,139 @@ import static org.hamcrest.MatcherAssert.*;
 import static org.jenkinsci.test.acceptance.Matchers.*;
 
 /**
- * Feature: Adds Warnings collection support In order to be able to collect and analyze warnings As a Jenkins user I
- * want to install and configure Warnings plugin
+ * Tests various aspects of the warnings plug-in. Most tests copy an existing file with several warnings into the
+ * workspace. This file is then analyzed by console and workspace parsers.
  */
 @WithPlugins("warnings")
 public class WarningsPluginTest extends AbstractCodeStylePluginHelper {
     /** Contains warnings for Javac parser. Warnings have file names preset for include/exclude filter tests. */
     private static final String WARNINGS_FILE_FOR_INCLUDE_EXCLUDE_TESTS = "/warnings_plugin/warningsForRegEx.txt";
+    private static final String SEVERAL_PARSERS_FILE_NAME = "warningsAll.txt";
     /** Contains warnings for several parsers. */
-    private static final String WARNINGS_FILE_SEVERAL_PARSERS = "/warnings_plugin/warningsAll.txt";
-
-    private FreeStyleJob job;
-
-    @Before
-    public void setUp() {
-        job = jenkins.jobs.create();
-    }
+    private static final String SEVERAL_PARSERS_FILE_FULL_PATH = "/warnings_plugin/" + SEVERAL_PARSERS_FILE_NAME;
 
     /**
-     * Scenario: Detect no errors in console log and workspace when there are none Given I have installed the "warnings"
-     * plugin And a job When I configure the job And I add "Scan for compiler warnings" post-build action And I add
-     * console parser for "Maven" And I add workspace parser for "Java Compiler (javac)" applied at "** / *" And I save
-     * the job And I build the job Then build should have 0 "Java" warnings Then build should have 0 "Maven" warnings
+     * Checks that no warnings are reported if the build does nothing.
      */
     @Test
     public void detect_no_errors_in_console_log_and_workspace_when_there_are_none() {
-        job.configure();
-        WarningsPublisher pub = job.addPublisher(WarningsPublisher.class);
-        pub.addConsoleScanner("Maven");
-        pub.addWorkspaceFileScanner("Java Compiler (javac)", "**/*");
-        job.save();
+        AbstractCodeStylePluginBuildConfigurator<WarningsBuildSettings> buildConfigurator = new AbstractCodeStylePluginBuildConfigurator<WarningsBuildSettings>() {
+            @Override
+            public void configure(WarningsBuildSettings settings) {
+                settings.addConsoleScanner("Maven");
+                settings.addWorkspaceFileScanner("Java Compiler (javac)", "**/*");
+            }
+        };
+        FreeStyleJob job = setupJob(null, FreeStyleJob.class, WarningsBuildSettings.class, buildConfigurator);
 
-        Build b = buildJobWithSuccess(job);
+        Build build = buildJobWithSuccess(job);
 
-        assertThat(b, not(hasAction("Java Warnings")));
-        assertThat(b, not(hasAction("Maven Warnings")));
-        b.open();
+        assertThatActionIsMissing(job, build, "Java Warnings");
+        assertThatActionIsMissing(job, build, "Maven Warnings");
+
+        build.open();
         assertThat(driver, hasContent("Java Warnings: 0"));
         assertThat(driver, hasContent("Maven Warnings: 0"));
     }
 
     /**
-     * Scenario: Detect errors in workspace Given I have installed the "warnings" plugin And a job When I configure the
-     * job And I add "Scan for compiler warnings" post-build action And I add workspace parser for "Java Compiler
-     * (javac)" applied at "** /*" And I add a shell build step """ echo '@Deprecated class a {} class b extends a {}' >
-     * a.java javac -Xlint a.java 2> out.log || true """ And I save the job And I build the job Then build should have 1
-     * "Java" warning
-     */
-    @Test
-    public void detect_errors_in_workspace() {
-        job.configure();
-        job.addPublisher(WarningsPublisher.class)
-                .addWorkspaceFileScanner("Java Compiler (javac)", "**/*");
-        job.addShellStep(
-                "echo '@Deprecated class a {} class b extends a {}' > a.java\n" +
-                        "javac -Xlint a.java 2> out.log || true"
-        );
-        job.save();
-
-        Build b = buildJobWithSuccess(job);
-
-        assertThat(b, hasAction("Java Warnings"));
-        b.open();
-        assertThat(driver, hasContent("Java Warnings: 1"));
-    }
-
-    /**
-     * Scenario: Do not detect errors in ignored parts of the workspace Given I have installed the "warnings" plugin And
-     * a job When I configure the job And I add "Scan for compiler warnings" post-build action And I add workspace
-     * parser for "Maven" applied at "no_errors_here.log" And I add a shell build step "mvn clean install > errors.log
-     * || true" And I save the job And I build the job Then build should have 0 "Maven" warning
+     * Checks that no warnings are reported if these are located in a different file.
      */
     @Test
     public void do_not_detect_errors_in_ignored_parts_of_the_workspace() {
+        AbstractCodeStylePluginBuildConfigurator<WarningsBuildSettings> buildConfigurator = new AbstractCodeStylePluginBuildConfigurator<WarningsBuildSettings>() {
+            @Override
+            public void configure(WarningsBuildSettings settings) {
+                settings.addWorkspaceFileScanner("Maven", "no_errors_here.log");
+            }
+        };
+        FreeStyleJob job = setupJob(null, FreeStyleJob.class, WarningsBuildSettings.class, buildConfigurator);
+
         job.configure();
-        job.addPublisher(WarningsPublisher.class)
-                .addWorkspaceFileScanner("Maven", "no_errors_here.log");
         job.addShellStep("mvn clean install > errors.log || true");
         job.save();
 
-        Build b = buildJobWithSuccess(job);
+        Build build = buildJobWithSuccess(job);
 
-        assertThat(b, not(hasAction("Maven Warnings")));
-        b.open();
+        assertThatActionIsMissing(job, build, "Maven Warnings");
+
+        build.open();
         assertThat(driver, hasContent("Maven Warnings: 0"));
     }
 
     /**
-     * Scenario: Detect multiple log results at once in console log Given I have installed the "warnings" plugin And a
-     * job When I configure the job And I add "Scan for compiler warnings" post-build action And I add console parser
-     * for "Java", "JavaDoc" and "MSBuild" And I add a shell build step "cat /warnings_plugin/warningsALL.txt" And I
-     * save the job And I build the job Then build should have 131 Java Warnings, 8 JavaDoc Warnings and 15 MSBuild
-     * warnings
+     * Checks that warning results are correctly created for the parsers
+     * "Java", "JavaDoc" and "MSBuild" if the console log contains multiple warnings of these types.
      */
     @Test
     public void detect_warnings_of_multiple_compilers_in_console() {
+        AbstractCodeStylePluginBuildConfigurator<WarningsBuildSettings> buildConfigurator = new AbstractCodeStylePluginBuildConfigurator<WarningsBuildSettings>() {
+            @Override
+            public void configure(WarningsBuildSettings settings) {
+                settings.addConsoleScanner("Java Compiler (javac)");
+                settings.addConsoleScanner("JavaDoc Tool");
+                settings.addConsoleScanner("MSBuild");
+            }
+        };
+        FreeStyleJob job = setupJob(SEVERAL_PARSERS_FILE_FULL_PATH, FreeStyleJob.class,
+                WarningsBuildSettings.class, buildConfigurator);
+
         job.configure();
-        WarningsPublisher wp = job.addPublisher(WarningsPublisher.class);
-        wp.addConsoleScanner("Java Compiler (javac)");
-        wp.addConsoleScanner("JavaDoc Tool");
-        wp.addConsoleScanner("MSBuild");
-        String warningsPath = this.getClass().getResource(WARNINGS_FILE_SEVERAL_PARSERS).getPath();
-        job.addShellStep("cat " + warningsPath);
+        job.addShellStep("cat " + SEVERAL_PARSERS_FILE_NAME);
         job.save();
-        Build b = buildJobWithSuccess(job);
-        assertThat(b, hasAction("Java Warnings"));
-        assertThat(b, hasAction("JavaDoc Warnings"));
-        assertThat(b, hasAction("MSBuild Warnings"));
-        b.open();
-        assertThat(driver, hasContent("Java Warnings: 131"));
-        assertThat(driver, hasContent("JavaDoc Warnings: 8"));
-        assertThat(driver, hasContent("MSBuild Warnings: 15"));
+
+        verify3ParserResults(job);
     }
 
     /**
-     * Scenario: Detect multiple log results at once in workspace Given I have installed the "warnings" plugin And a job
-     * When I configure the job And I add "Scan for compiler warnings" post-build action And I add workspace parser for
-     * "Java", "JavaDoc" and "MSBuild" And I add a shell build step "cat /warnings_plugin/warningsALL.txt >> errors.log"
-     * And I save the job And I build the job Then build should have 131 Java Warnings, 8 JavaDoc Warnings and 15
-     * MSBuild Warnings
+     * Checks that warning results are correctly created for the workspace parsers
+     * "Java", "JavaDoc" and "MSBuild" if a file with multiple warnings of these types is copied to the workspace.
      */
     @Test
     public void detect_warnings_of_multiple_compilers_in_workspace() {
-        job.configure();
-        WarningsPublisher wp = job.addPublisher(WarningsPublisher.class);
-        wp.addWorkspaceFileScanner("Java Compiler (javac)", "**/*");
-        wp.addWorkspaceFileScanner("JavaDoc Tool", "**/*");
-        wp.addWorkspaceFileScanner("MSBuild", "**/*");
-        String warningsPath = this.getClass().getResource(WARNINGS_FILE_SEVERAL_PARSERS).getPath();
-        job.addShellStep("cat " + warningsPath + " >> errors.log");
-        job.save();
-        Build b = buildJobWithSuccess(job);
-        assertThat(b, hasAction("Java Warnings"));
-        assertThat(b, hasAction("JavaDoc Warnings"));
-        assertThat(b, hasAction("MSBuild Warnings"));
-        b.open();
+        AbstractCodeStylePluginBuildConfigurator<WarningsBuildSettings> buildConfigurator = new AbstractCodeStylePluginBuildConfigurator<WarningsBuildSettings>() {
+            @Override
+            public void configure(WarningsBuildSettings settings) {
+                settings.addWorkspaceFileScanner("Java Compiler (javac)", "**/*");
+                settings.addWorkspaceFileScanner("JavaDoc Tool", "**/*");
+                settings.addWorkspaceFileScanner("MSBuild", "**/*");
+            }
+        };
+        FreeStyleJob job = setupJob(SEVERAL_PARSERS_FILE_FULL_PATH, FreeStyleJob.class,
+                WarningsBuildSettings.class, buildConfigurator);
+
+        verify3ParserResults(job);
+    }
+
+    private void verify3ParserResults(final FreeStyleJob job) {
+        Build build = buildJobWithSuccess(job);
+        assertThatActionExists(job, build, "Java Warnings");
+        assertThatActionExists(job, build, "JavaDoc Warnings");
+        assertThatActionExists(job, build, "MSBuild Warnings");
+
+        build.open();
         assertThat(driver, hasContent("Java Warnings: 131"));
         assertThat(driver, hasContent("JavaDoc Warnings: 8"));
         assertThat(driver, hasContent("MSBuild Warnings: 15"));
     }
 
     /**
-     * Checks that the warnings plugin will not skip build results if "Run always" is checked.
+     * Checks that the warnings plugin will be skipped if "Run always" is not checked.
      */
     @Test
     public void skip_failed_builds() {
         FreeStyleJob job = runBuildWithRunAlwaysOption(false);
         Build build = buildJobAndWait(job).shouldFail();
 
-        assertThat(build, not(hasAction("Java Warnings")));
-        assertThat(job.getLastBuild(), not(hasAction("Java Warnings")));
+        assertThatActionIsMissing(job, build, "Java Warnings");
 
         build.open();
         assertThat(driver, not(hasContent("Java Warnings:")));
+    }
+
+    private void assertThatActionIsMissing(final FreeStyleJob job, final Build build, final String parser) {
+        assertThat(build, not(hasAction(parser)));
+        assertThat(job.getLastBuild(), not(hasAction("Java Warnings")));
     }
 
     /**
@@ -195,7 +173,7 @@ public class WarningsPluginTest extends AbstractCodeStylePluginHelper {
                 settings.setCanRunOnFailed(canRunOnFailed);
             }
         };
-        FreeStyleJob job = setupJob(WARNINGS_FILE_SEVERAL_PARSERS, FreeStyleJob.class,
+        FreeStyleJob job = setupJob(SEVERAL_PARSERS_FILE_FULL_PATH, FreeStyleJob.class,
                 WarningsBuildSettings.class, buildConfigurator);
 
         job.configure();
@@ -239,9 +217,9 @@ public class WarningsPluginTest extends AbstractCodeStylePluginHelper {
         assertThat(action.getLowWarningNumber(), is(0));
     }
 
-    private void assertThatActionExists(final Job job, final Build build, final String type) {
-        assertThat(build, hasAction(type));
-        assertThat(job.getLastBuild(), hasAction(type));
+    private void assertThatActionExists(final Job job, final Build build, final String parser) {
+        assertThat(build, hasAction(parser));
+        assertThat(job.getLastBuild(), hasAction(parser));
     }
 
     /**
