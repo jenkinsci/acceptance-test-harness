5649e766bbe24b654a020382651df6f67d3f98d3
Oliver Gondža <ogondza@gmail.com>
2014-11-01 16:36:24 +0100

    Unused imports

--- src/test/java/plugins/LdapPluginTest.java
+++ src/test/java/plugins/LdapPluginTest.java
@@ -16,15 +16,8 @@ import javax.inject.Inject;
 
 import static org.hamcrest.CoreMatchers.not;
 import static org.jenkinsci.test.acceptance.Matchers.*;
-import javax.annotation.CheckForNull;
-import javax.inject.Inject;
-import java.io.IOException;
-import java.net.ServerSocket;
 
-import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.jenkinsci.test.acceptance.Matchers.hasContent;
-import static org.jenkinsci.test.acceptance.Matchers.hasLoggedInUser;
 
 
 /**
