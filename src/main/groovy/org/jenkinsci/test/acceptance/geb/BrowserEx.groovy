7c801e1f7a9c84c28317e04daf902c8483708760
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-11-19 15:29:36 -0800

    Geb PageObjects should be instantiated via Guice
    ... which lets us bridge Geb page objects into existing Java page objects for logic reuse.

---
+++ src/main/groovy/org/jenkinsci/test/acceptance/geb/BrowserEx.groovy
@@ -0,0 +1,28 @@
+package org.jenkinsci.test.acceptance.geb;
+
+import com.google.inject.Injector;
+import geb.Browser;
+import geb.Configuration;
+import geb.Page;
+
+/**
+ * {@link Browser} subtype that creates pages objects via Guice so that POs can inject other parameters.
+ *
+ * <p>
+ * This is useful to call into Java page objects that implement various logics.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class BrowserEx extends Browser {
+    public final Injector injector;
+
+    BrowserEx(Injector injector, Configuration conf) {
+        super(conf);
+        this.injector = injector;
+    }
+
+    @Override
+    public <T extends Page> T createPage(Class<T> pageType) {
+        return injector.getInstance(pageType).init(this);
+    }
+}
--- src/main/groovy/org/jenkinsci/test/acceptance/geb/GebSpec.groovy
+++ src/main/groovy/org/jenkinsci/test/acceptance/geb/GebSpec.groovy
@@ -1,5 +1,7 @@
 package org.jenkinsci.test.acceptance.geb
 
+import com.google.inject.Injector
+import geb.Browser
 import org.jenkinsci.test.acceptance.junit.JenkinsAcceptanceTestRule
 import org.jenkinsci.test.acceptance.junit.Resource
 import org.jenkinsci.test.acceptance.po.Jenkins
@@ -26,6 +28,9 @@ class GebSpec extends geb.spock.GebSpec {
     @Inject
     public Jenkins jenkins;
 
+    @Inject
+    public Injector injector;
+
     /**
      * Obtains a resource in a wrapper.
      */
@@ -35,5 +40,10 @@ class GebSpec extends geb.spock.GebSpec {
             throw new AssertionError("No such resource " + path + " for " + getClass().getName());
         }
         return new Resource(resource);
+    }
+
+    @Override
+    public Browser createBrowser() {
+        return new BrowserEx(injector,createConf());
     }
 }
