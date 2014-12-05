61f325c687592225bc24698585633380e52798fd
Oliver Gondža <ogondza@gmail.com>
2014-12-05 15:47:22 +0100

    Rename BuildTrigger to BuildOtherProjects to better reflect what it does

--- src/main/java/org/jenkinsci/test/acceptance/po/BuildOtherProjects.java
+++ src/main/java/org/jenkinsci/test/acceptance/po/BuildOtherProjects.java
@@ -6,15 +6,13 @@ package org.jenkinsci.test.acceptance.po;
  * @author Kohsuke Kawaguchi
  */
 @Describable("Build other projects")
-public class BuildTrigger extends AbstractStep implements PostBuildStep {
+public class BuildOtherProjects extends AbstractStep implements PostBuildStep {
     public final Control childProjects = control("childProjects");
 
     public final Control thresholdSuccess = control("threshold[SUCCESS]");
     public final Control thresholdFailure = control("threshold[FAILURE]");
 
-    public BuildTrigger(Job parent, String path) {
+    public BuildOtherProjects(Job parent, String path) {
         super(parent, path);
     }
-
-
 }
