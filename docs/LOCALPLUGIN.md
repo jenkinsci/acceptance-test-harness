51735fc8a04c4c7dfeecd429759e8d8f8bf29ba4
Jesse Glick <jglick@cloudbees.com>
2014-12-19 16:02:15 -0500

    Extending LocalOverrideUpdateCenterMetadataDecoratorImpl with support for LOCAL_SNAPSHOTS=true.

--- docs/LOCALPLUGIN.md
+++ docs/LOCALPLUGIN.md
@@ -17,3 +17,21 @@ You can also do this from [the groovy wiring script](WIRING.md).
     envs['ldap.jpi'] = '/path/to/your.ldap.jpi'
 
 TODO: provide a better binding for this
+
+As a convenience, you can also run with the variable `LOCAL_SNAPSHOTS=true`.
+This will override any plugin in the update center with a locally built `SNAPSHOT` version (if newer than the released one).
+You can leave this mode enabled and thus not need to specify a new environment variable for each plugin you are testing.
+For example, you add to your `~/.m2/settings.xml` a profile like
+
+    <profile>
+        <id>acceptance-tests</id>
+        <activation>
+            <activeByDefault>true</activeByDefault>
+        </activation>
+        <properties>
+            <JENKINS_WAR>/…/jenkinsci/jenkins/war/target/jenkins.war</JENKINS_WAR>
+            <LOCAL_SNAPSHOTS>true</LOCAL_SNAPSHOTS>
+        </properties>
+    </profile>
+
+and then `mvn -DskipTests install` on Jenkins core or any plugin before running acceptance tests.
--- src/main/java/org/jenkinsci/test/acceptance/update_center/LocalOverrideUpdateCenterMetadataDecoratorImpl.java
+++ src/main/java/org/jenkinsci/test/acceptance/update_center/LocalOverrideUpdateCenterMetadataDecoratorImpl.java
@@ -3,9 +3,16 @@ package org.jenkinsci.test.acceptance.update_center;
 import com.cloudbees.sdk.extensibility.Extension;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.jar.Attributes;
 import java.util.jar.JarFile;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.eclipse.aether.util.version.GenericVersionScheme;
+import org.eclipse.aether.version.Version;
+import org.eclipse.aether.version.VersionScheme;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 /**
  * Allow local plugins specified via environment variables to override plugin metadata from update center.
@@ -18,9 +25,38 @@ import java.util.jar.JarFile;
 public class LocalOverrideUpdateCenterMetadataDecoratorImpl implements UpdateCenterMetadataDecorator {
     @Override
     public void decorate(UpdateCenterMetadata ucm) {
+        if ("true".equals(System.getenv("LOCAL_SNAPSHOTS"))) {
+            File userHome = new File(System.getProperty("user.home"));
+            File localRepo = new File(new File(userHome, ".m2"), "repository");
+            VersionScheme versionScheme = new GenericVersionScheme();
+            for (Map.Entry<String,PluginMetadata> entry : ucm.plugins.entrySet()) {
+                // TODO would be more proper to go through Aether APIs to do this
+                String[] gav = entry.getValue().gav.split(":");
+                File artifactDir = new File(new File(localRepo, gav[0].replace('.', File.separatorChar)), gav[1]);
+                File metadata = new File(artifactDir, "maven-metadata-local.xml");
+                if (metadata.isFile()) {
+                    try {
+                        Version ucVersion = versionScheme.parseVersion(gav[2]);
+                        NodeList versions = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(metadata).getElementsByTagName("version");
+                        for (int i = 0; i < versions.getLength(); i++) {
+                            String version = ((Element) versions.item(i)).getTextContent();
+                            if (version.endsWith("-SNAPSHOT") && versionScheme.parseVersion(version).compareTo(ucVersion) > 0) {
+                                File hpi = new File(new File(artifactDir, version), gav[1] + "-" + version + ".hpi");
+                                if (hpi.isFile()) {
+                                    System.err.println("Overriding " + entry.getKey() + " " + ucVersion + " with local build of " + version);
+                                    PluginMetadata m = parseMetadata(hpi);
+                                    ucm.plugins.put(m.name, m);
+                                }
+                            }
+                        }
+                    } catch (Exception x) {
+                        x.printStackTrace();
+                    }
+                }
+            }
+        }
         for (Map.Entry<String,String> e : System.getenv().entrySet()) {
             if (e.getKey().endsWith(".jpi")) {
-                String shortName = e.getKey().substring(e.getKey().length()-4);
                 PluginMetadata m = parseMetadata(new File(e.getValue()));
                 ucm.plugins.put(m.name, m);
             }
@@ -32,6 +68,7 @@ public class LocalOverrideUpdateCenterMetadataDecoratorImpl implements UpdateCen
      */
     private PluginMetadata parseMetadata(File jpi) {
         PluginMetadata m = new PluginMetadata();
+        m.dependencies = new ArrayList<Dependency>();
         try (JarFile j = new JarFile(jpi)) {
             Attributes main = j.getManifest().getMainAttributes();
             m.name = main.getValue("Short-Name");
