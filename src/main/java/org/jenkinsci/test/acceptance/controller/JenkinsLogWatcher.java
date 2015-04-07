30219ff0aec8a4520c02fa426df60f395056584b
Oliver Gondža <ogondza@gmail.com>
2015-04-07 12:01:27 +0200

    Stretch default startup time some more.
    "Inspecting plugin /scratch/jenkins/workspace/core/acceptance-test-harness/jenkins8120127865101372945home/plugins/maven-plugin.jpi" daemon prio=10 tid=0x00007f05b0001000 nid=0x4ab7 runnable [0x00007f0639d7d000]
       java.lang.Thread.State: RUNNABLE
           at java.io.RandomAccessFile.readBytes0(Native Method)
           at java.io.RandomAccessFile.readBytes(RandomAccessFile.java:350)
           at java.io.RandomAccessFile.read(RandomAccessFile.java:385)
           at java.io.RandomAccessFile.readFully(RandomAccessFile.java:444)
           at java.io.RandomAccessFile.readFully(RandomAccessFile.java:424)
           at org.apache.tools.zip.ZipFile.populateFromCentralDirectory(ZipFile.java:388)
           at org.apache.tools.zip.ZipFile.<init>(ZipFile.java:186)
           at org.apache.tools.zip.ZipFile.<init>(ZipFile.java:163)
           at org.apache.tools.ant.types.resources.ZipResource.getInputStream(ZipResource.java:133)
           at org.apache.tools.ant.types.resources.ResourceDecorator.getInputStream(ResourceDecorator.java:125)
           at org.apache.tools.ant.taskdefs.Zip.addResource(Zip.java:1041)
           at org.apache.tools.ant.taskdefs.Zip.addResources(Zip.java:1098)
           at org.apache.tools.ant.taskdefs.Zip.executeMain(Zip.java:665)
           at org.apache.tools.ant.taskdefs.Zip.execute(Zip.java:568)
           at hudson.ClassicPluginStrategy.createClassJarFromWebInfClasses(ClassicPluginStrategy.java:586)
           at hudson.ClassicPluginStrategy.explode(ClassicPluginStrategy.java:527)
           at hudson.ClassicPluginStrategy.createPluginWrapper(ClassicPluginStrategy.java:166)
           at hudson.PluginManager$1$3$1.run(PluginManager.java:253)
           at org.jvnet.hudson.reactor.TaskGraphBuilder$TaskImpl.run(TaskGraphBuilder.java:169)
           at org.jvnet.hudson.reactor.Reactor.runTask(Reactor.java:282)
           at jenkins.model.Jenkins$7.runTask(Jenkins.java:904)
           at org.jvnet.hudson.reactor.Reactor$2.run(Reactor.java:210)
           at org.jvnet.hudson.reactor.Reactor$Node.run(Reactor.java:117)
           at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1145)
           at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:615)
           at java.lang.Thread.run(Thread.java:745)
    
    "pool-6-thread-2" daemon prio=10 tid=0x00007f05dc14d800 nid=0x4ab6 waiting on condition [0x00007f0639e7f000]

--- src/main/java/org/jenkinsci/test/acceptance/controller/JenkinsLogWatcher.java
+++ src/main/java/org/jenkinsci/test/acceptance/controller/JenkinsLogWatcher.java
@@ -79,6 +79,7 @@ public class JenkinsLogWatcher implements LogListenable, Closeable {
         reader.start();
     }
 
+    @Override
     public void close() throws IOException {
         if(pipe != null){
             pipe.close();
@@ -124,7 +125,7 @@ public class JenkinsLogWatcher implements LogListenable, Closeable {
         splitter.removeLogListener(l);
     }
 
-    public static final int DEFAULT_TIMEOUT = 300;//100 sec
+    public static final int DEFAULT_TIMEOUT = 360;
 
     public static final int TIMEOUT = System.getenv("STARTUP_TIME") != null && Integer.parseInt(System.getenv("STARTUP_TIME")) > 0
             ? Integer.parseInt(System.getenv("STARTUP_TIME")) : DEFAULT_TIMEOUT;
