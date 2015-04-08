b9347638bb8d754819b1815c43790ec318f3de85
Oliver Gondža <ogondza@gmail.com>
2015-04-08 15:53:03 +0200

    Simplify docker filter

--- docker-only.groovy
+++ docker-only.groovy
@@ -2,14 +2,11 @@ import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.Statement;
 
 import org.jenkinsci.test.acceptance.junit.FilterRule.Filter;
-import org.jenkinsci.test.acceptance.junit.Native;
+import org.jenkinsci.test.acceptance.junit.WithDocker;
 
 bind Filter toInstance ({ base, method, target ->
-    for (annot in Filter.getAnnotations(method, target, Native.class)) {
-        if ('docker' in annot.value()) {
-            return null;
-        }
-    }
+    def docker = !Filter.getAnnotations(method, target, WithDocker.class).isEmpty();
+    if (docker) return null;
 
     return "Running only docker based tests";
 } as Filter)
