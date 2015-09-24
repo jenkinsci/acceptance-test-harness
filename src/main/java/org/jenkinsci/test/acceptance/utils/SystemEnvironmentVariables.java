c1c715469d21dfaac2ead4076048c512dcebd8c3
Alex Soto <asotobueno@cloudbees.com>
2015-09-24 09:58:36 +0200

    fixes javadoc comment

--- src/main/java/org/jenkinsci/test/acceptance/utils/SystemEnvironmentVariables.java
+++ src/main/java/org/jenkinsci/test/acceptance/utils/SystemEnvironmentVariables.java
@@ -1,9 +1,9 @@
 package org.jenkinsci.test.acceptance.utils;
 
 /**
- * Gets a system environment variable. First it checks if the variable is an environment variable and if it is the case,
+ * Gets a system environment variable. First it checks if the variable is set as Java system property  and if it is the case,
  * it returns the configured value.
- * If not then it checks if it is defined as java system property, and if not it returns the default value.
+ * If not then it checks if it is defined as environment variable, and if not it returns the default value.
  */
 public class SystemEnvironmentVariables {
 
