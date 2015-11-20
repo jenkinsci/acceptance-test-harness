629e778f62c907553237cdea2013a97a1b930ad4
Andres Rodriguez <arodriguez@cloudbees.com>
2015-11-20 14:17:17 +0100

    [JENKINS-31648] Fix license comments, indentation and review javadoc.

--- src/test/java/plugins/FolderPluginTest.java
+++ src/test/java/plugins/FolderPluginTest.java
@@ -1,3 +1,27 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package plugins;
 
 import org.hamcrest.MatcherAssert;
@@ -9,7 +33,6 @@ import org.junit.Test;
 
 /**
  * Acceptance tests for the CloudBees Folder Plugins.
- * @author Andres Rodriguez
  */
 @WithPlugins("cloudbees-folder")
 public class FolderPluginTest extends AbstractJUnitTest {
@@ -17,22 +40,23 @@ public class FolderPluginTest extends AbstractJUnitTest {
     private static final String F01 = "F01";
     
     /**
-     * First simple test scenario: Folder creation (JENKINS-31648)
-     *
-     * - We create a folder named "F01".
-     * - We check the folder exists and we can enter it.
+     * First simple test scenario: Folder creation (JENKINS-31648).
+     * <ol>
+     * <li>We create a folder named "F01".
+     * <li>We check the folder exists and we can enter it.
+     * </ol>
      */
     @Test
-    public void createFolder() throws Exception {
+    public void createFolder() {
         // 1 - Create job
         final FolderJob job = jenkins.jobs.create(FolderJob.class, F01);
         // 2 - Save job
         job.save();
-    	// 3 - Go to main page.
+        // 3 - Go to main page.
         jenkins.open();
-    	// 4 - Go to job page.
+        // 4 - Go to job page.
         job.open();
         // Ensure the folder name is correct.
-    	MatcherAssert.assertThat(driver, Matchers.hasContent(F01));
+        MatcherAssert.assertThat(driver, Matchers.hasContent(F01));
     }
 }
