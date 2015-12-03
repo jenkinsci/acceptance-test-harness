82a6b749651174c84c378d3292f5224796dfebb1
Andres Rodriguez <arodriguez@cloudbees.com>
2015-12-03 18:48:00 +0100

    [JENKINS-31772] Additional javadoc to explain intended use of ClassRule.

--- src/main/java/org/jenkinsci/test/acceptance/junit/AbstractClassBasedJUnitTest.java
+++ src/main/java/org/jenkinsci/test/acceptance/junit/AbstractClassBasedJUnitTest.java
@@ -11,6 +11,15 @@ import java.net.ServerSocket;
 
 /**
  * Convenience base class to derive your plain-old JUnit tests from when using {@link JenkinsAcceptanceTestRule} as a {@link ClassRule}.
+ * <ol>
+ *     <li>Use of the {@link ClassRule} is intended for those cases in which stateful tests are performed:
+ *     e.g., tests targeted at and existing instance. An example would be test suites to perform tasks
+ *     soak test environment: populate data, check status, schedule jobs.</li>
+ *     <li>Use of the {@link ClassRule} is not recommended for the normal ATH use case in which stateless tests are always
+ *     the preferred option. The exception would be tests with a very expensive initialization sequence, but in
+ *     that case this circumstance should be clearly documented, as well as the measures taken to avoid issues for
+ *     being stateful.</li>
+ * </ol>
  *
  * @author Andres Rodriguez
  */
