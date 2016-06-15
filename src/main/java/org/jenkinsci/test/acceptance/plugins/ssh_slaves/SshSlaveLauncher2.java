35fec4b326b186587a6a1a61b9dbcb5a297934b1
raul <raul.arabaolaza@getsetpay.com>
2016-06-15 12:50:32 +0200

    Specialization of SshSlaveLauncher to work with credentials plugin v >2.0.7

---
+++ src/main/java/org/jenkinsci/test/acceptance/plugins/ssh_slaves/SshSlaveLauncher2.java
@@ -0,0 +1,23 @@
+package org.jenkinsci.test.acceptance.plugins.ssh_slaves;
+
+import org.jenkinsci.test.acceptance.plugins.ssh_slaves.SshSlaveLauncher;
+import org.jenkinsci.test.acceptance.po.PageObject;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Created by raul on 15/06/16.
+ */
+public class SshSlaveLauncher2 extends SshSlaveLauncher {
+    public SshSlaveLauncher2(PageObject context, String path) {
+        super(context, path);
+    }
+
+    @Override
+    public org.jenkinsci.test.acceptance.plugins.ssh_credentials.SshSlaveLauncher addCredential() {
+        self().findElement(by.button("Add")).click();
+        elasticSleep(500);
+        control(by.xpath("//*[@id=\"yui-gen2\"]")).click();
+        return new org.jenkinsci.test.acceptance.plugins.ssh_credentials.SshSlaveLauncher(getPage(), "/credentials");
+    }
+
+}
