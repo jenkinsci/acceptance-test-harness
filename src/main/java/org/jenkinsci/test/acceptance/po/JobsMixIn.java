5dd65d2fd8f5aea91acfd147dc866fdbc51a3646
raul <raul.arabaolaza@getsetpay.com>
2016-06-23 14:03:32 +0200

    #JENKINS-36042 Use timeout in waitFor and some style fixes

--- src/main/java/org/jenkinsci/test/acceptance/po/JobsMixIn.java
+++ src/main/java/org/jenkinsci/test/acceptance/po/JobsMixIn.java
@@ -35,11 +35,11 @@ public class JobsMixIn extends MixIn {
             }
         }).click();
 
-        //Sometimes we need to give some time to ok button to become enabled
+        // Sometimes we need to give some time to ok button to become enabled
         waitTillEnabledAndClick("OK", 10, TimeUnit.SECONDS);
 
-        //sometimes job creation is not fast enough, so make sure it's finished before continue
-        waitFor(by.name("config"));
+        // Sometimes job creation is not fast enough, so make sure it's finished before continue
+        waitFor(by.name("config"), 3);
 
         final T j = get(type, name);
 
