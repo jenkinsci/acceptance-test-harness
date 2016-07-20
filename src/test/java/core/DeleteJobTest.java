7e9ef0210e175d39f50f5924dbfd2ab9203b9dd4
Raul <rarabaolaza@cloudbees.com>
2016-07-20 17:17:47 +0200

    [JENKINS-36818] Use Matchers.pageObjectExists() to check existence of project

--- src/test/java/core/DeleteJobTest.java
+++ src/test/java/core/DeleteJobTest.java
@@ -1,14 +1,14 @@
 package core;
 
+import org.jenkinsci.test.acceptance.Matchers;
 import org.jenkinsci.test.acceptance.junit.AbstractJUnitTest;
 import org.jenkinsci.test.acceptance.po.FreeStyleJob;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.jenkinsci.test.acceptance.Matchers.hasContent;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.jenkinsci.test.acceptance.Matchers.pageObjectExists;
+import static org.jenkinsci.test.acceptance.Matchers.pageObjectDoesNotExist;
+
 
 /**
  * Feature: Delete a job
@@ -24,10 +24,10 @@ public class DeleteJobTest extends AbstractJUnitTest {
     @Test
     public void delete_a_simple_job() {
         FreeStyleJob j = jenkins.jobs.create(FreeStyleJob.class, "simple-job");
-        assertTrue(j.open().getTitle().contains(j.name));
+        assertThat(j, pageObjectExists());
 
         j.delete();
 
-        assertFalse(j.open().getTitle().contains(j.name));
+        assertThat(j,pageObjectDoesNotExist());
     }
 }
