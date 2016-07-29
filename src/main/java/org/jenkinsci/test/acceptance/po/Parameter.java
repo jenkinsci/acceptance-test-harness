95be792fa518b97d86a8cb8513e474fcf2d330be
Raul <rarabaolaza@cloudbees.com>
2016-07-29 13:32:28 +0200

    [JENKINS-36928] Changed the semantics of @Describable

--- src/main/java/org/jenkinsci/test/acceptance/po/Parameter.java
+++ src/main/java/org/jenkinsci/test/acceptance/po/Parameter.java
@@ -2,20 +2,30 @@ package org.jenkinsci.test.acceptance.po;
 
 import org.openqa.selenium.By;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Parameter for builds.
  * <p/>
- * Use {@link Describable} annotation to register an implementation.
+ * Use {@link Describable} annotation to register an implementation. The value of {@link Describable} must be the
+ * corresponding caption of the add parameter menu item AND the corresponding {@link hudson.model.ParameterDefinition}
+ * class. For example {"String Parameter", "hudson.model.StringParameterDefinition"}
  *
  * @author Kohsuke Kawaguchi
  */
 public abstract class Parameter extends PageAreaImpl {
+    private static List<Class<? extends Parameter>> subtypes = new LinkedList<>();
+
     public final Job job;
     private String name;
 
     public Parameter(Job job, String path) {
         super(job, path);
         this.job = job;
+        if (!Parameter.subtypes.contains(this.getClass())) {
+            Parameter.subtypes.add(this.getClass());
+        }
     }
 
     public String getName() {
@@ -63,5 +73,9 @@ public abstract class Parameter extends PageAreaImpl {
         String np = driver.findElement(by.xpath("//input[@name='name' and @value='%s']", name)).getAttribute("path");
         String path = np.replaceAll("/name$", "") + "/" + rel;
         return by.path(path);
+    }
+
+    public static List<Class<? extends Parameter>> all() {
+        return Parameter.subtypes;
     }
 }
