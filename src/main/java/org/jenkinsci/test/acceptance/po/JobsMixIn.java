b26cce4360c36228d60393a2d3a4ce8d06497b66
Manuel Franco <mfranco@cloudbees.com>
2016-09-13 12:28:15 +0200

    [JENKINS-38058] explicit check for constructor existence

--- src/main/java/org/jenkinsci/test/acceptance/po/JobsMixIn.java
+++ src/main/java/org/jenkinsci/test/acceptance/po/JobsMixIn.java
@@ -1,5 +1,6 @@
 package org.jenkinsci.test.acceptance.po;
 
+import java.net.URL;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
@@ -58,14 +59,23 @@ public class JobsMixIn extends MixIn {
     }
 
     public <T extends TopLevelItem> T get(Class<T> type, String name) {
-        try {
-            assert getContext() != null;
+        if (contextAvailable() && typeAcceptsContext(type)) {
             return newInstance(type, getContext(), url("job/%s/", name), name);
-        } catch (AssertionError e) {
-            // will get here either if the contructor with 'context' parameter
-            // was not found or if we have no context
-            return newInstance(type, injector, url("job/%s/", name), name);
         }
+        return newInstance(type, injector, url("job/%s/", name), name);
+    }
+
+    private <T extends TopLevelItem> boolean typeAcceptsContext(Class<T> type) {
+        try {
+            type.getConstructor(PageObject.class, URL.class, String.class);
+            return true;
+        } catch (NoSuchMethodException e) {
+            return false;
+        }
+    }
+
+    private boolean contextAvailable() {
+        return getContext() != null;
     }
 
     public FreeStyleJob create() {
