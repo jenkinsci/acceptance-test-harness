01f771455eaac05f0df1affbd1ceea8a37b305da
Raul <rarabaolaza@cloudbees.com>
2016-10-06 15:33:49 +0200

    [JENKINS-38786] Add missing Publishers

---
+++ src/main/java/org/jenkinsci/test/acceptance/po/AggregateDownstreamTestResults.java
@@ -0,0 +1,14 @@
+package org.jenkinsci.test.acceptance.po;
+
+@Describable("Aggregate downstream test results")
+public class AggregateDownstreamTestResults extends AbstractStep implements PostBuildStep {
+
+    public final Control specify = control("specify");
+    public final Control jobs = control("specify/jobs");
+    public final Control includeFailedBuilds = control("specify/includeFailedBuilds");
+
+
+    public AggregateDownstreamTestResults(Job parent, String path) {
+        super(parent, path);
+    }
+}
