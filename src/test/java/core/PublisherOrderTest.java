83128752519bad958f01ef27017ddaa97fbc6651
Raul <rarabaolaza@cloudbees.com>
2016-10-06 18:22:15 +0200

    [JENKINS-37276] Initial implementation based on @Describable

---
+++ src/test/java/core/PublisherOrderTest.java
@@ -0,0 +1,55 @@
+package core;
+
+import org.apache.commons.lang.SystemUtils;
+import org.jenkinsci.test.acceptance.junit.AbstractJUnitTest;
+import org.jenkinsci.test.acceptance.po.AggregateDownstreamTestResults;
+import org.jenkinsci.test.acceptance.po.ArtifactArchiver;
+import org.jenkinsci.test.acceptance.po.BuildTrigger;
+import org.jenkinsci.test.acceptance.po.Fingerprint;
+import org.jenkinsci.test.acceptance.po.FreeStyleJob;
+import org.junit.Test;
+
+public class PublisherOrderTest extends AbstractJUnitTest {
+
+    @Test
+    public void testOrdered() {
+        FreeStyleJob upstream = jenkins.jobs.create(FreeStyleJob.class);
+        upstream.configure();
+        String command = "echo 'hello' > aggregate.txt";
+        if(SystemUtils.IS_OS_UNIX) {
+            upstream.addShellStep(command);
+        } else {
+            upstream.addBatchStep(command);
+        }
+        AggregateDownstreamTestResults aggregate = upstream.addPublisher(AggregateDownstreamTestResults.class);
+        aggregate.specify.check();
+        ArtifactArchiver archiver = upstream.addPublisher(ArtifactArchiver.class);
+        archiver.includes("aggregate.txt");
+        BuildTrigger trigger = upstream.addPublisher(BuildTrigger.class);
+        trigger.childProjects.set("projectName");
+        Fingerprint fingerprint = upstream.addPublisher(Fingerprint.class);
+        fingerprint.targets.set("aggregate.txt");
+        upstream.save();
+    }
+
+    @Test
+    public void testUnordered() {
+        FreeStyleJob upstream = jenkins.jobs.create(FreeStyleJob.class);
+        upstream.configure();
+        String command = "echo 'hello' > aggregate.txt";
+        if(SystemUtils.IS_OS_UNIX) {
+            upstream.addShellStep(command);
+        } else {
+            upstream.addBatchStep(command);
+        }
+        ArtifactArchiver archiver = upstream.addPublisher(ArtifactArchiver.class);
+        archiver.includes("aggregate.txt");
+        BuildTrigger trigger = upstream.addPublisher(BuildTrigger.class);
+        trigger.childProjects.set("projectName");
+        Fingerprint fingerprint = upstream.addPublisher(Fingerprint.class);
+        fingerprint.targets.set("aggregate.txt");
+        AggregateDownstreamTestResults aggregate = upstream.addPublisher(AggregateDownstreamTestResults.class);
+        aggregate.specify.check();
+        upstream.save();
+    }
+}
