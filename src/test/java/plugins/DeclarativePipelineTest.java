8a59aa3d08779a25d7114279672d4649bff5cbba
Andrew Bayer <andrew.bayer@gmail.com>
2016-11-15 14:53:45 -0800

    Added some negative-case Declarative tests

--- src/test/java/plugins/DeclarativePipelineTest.java
+++ src/test/java/plugins/DeclarativePipelineTest.java
@@ -1,7 +1,5 @@
 package plugins;
 
-import org.jenkinsci.test.acceptance.docker.DockerContainerHolder;
-import org.jenkinsci.test.acceptance.docker.fixtures.GitContainer;
 import org.jenkinsci.test.acceptance.junit.AbstractJUnitTest;
 import org.jenkinsci.test.acceptance.junit.Since;
 import org.jenkinsci.test.acceptance.junit.WithPlugins;
@@ -10,15 +8,14 @@ import org.jenkinsci.test.acceptance.po.Build;
 import org.jenkinsci.test.acceptance.po.DumbSlave;
 import org.jenkinsci.test.acceptance.po.WorkflowJob;
 import org.jenkinsci.test.acceptance.slave.SlaveController;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
 
 import javax.inject.Inject;
 
 import java.util.concurrent.Callable;
 import java.util.regex.Pattern;
 
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.jenkinsci.test.acceptance.Matchers.containsRegexp;
 
@@ -26,16 +23,12 @@ import static org.jenkinsci.test.acceptance.Matchers.containsRegexp;
 public class DeclarativePipelineTest extends AbstractJUnitTest {
     @Inject
     private SlaveController slaveController;
-    @Inject
-    DockerContainerHolder<GitContainer> gitServer;
-    @Rule
-    public TemporaryFolder tmp = new TemporaryFolder();
 
     @WithPlugins("pipeline-model-definition")
     @Test
     public void basicDeclarativeTests() throws Exception {
-        WorkflowJob job = jenkins.jobs.create(WorkflowJob.class);
-        job.script.set(
+        WorkflowJob helloWorldJob = jenkins.jobs.create(WorkflowJob.class);
+        helloWorldJob.script.set(
                 "pipeline {\n" +
                 "  agent none\n" +
                 "  stages {\n" +
@@ -48,11 +41,11 @@ public class DeclarativePipelineTest extends AbstractJUnitTest {
                 "}\n"
         );
 
-        job.sandbox.check();
-        job.save();
+        helloWorldJob.sandbox.check();
+        helloWorldJob.save();
 
-        Build b = job.startBuild().shouldSucceed();
-        assertThat(b.getConsole(), containsRegexp("Hello world", Pattern.MULTILINE));
+        Build helloWorldBuild = helloWorldJob.startBuild().shouldSucceed();
+        assertThat(helloWorldBuild.getConsole(), containsRegexp("Hello world", Pattern.MULTILINE));
 
         MavenInstallation.installMaven(jenkins, "M3", "3.1.0");
         final DumbSlave slave = (DumbSlave) slaveController.install(jenkins).get();
@@ -62,8 +55,8 @@ public class DeclarativePipelineTest extends AbstractJUnitTest {
                 return null;
             }
         });
-        WorkflowJob secondJob = jenkins.jobs.create(WorkflowJob.class);
-        secondJob.script.set(
+        WorkflowJob toolsEnvAgentJob = jenkins.jobs.create(WorkflowJob.class);
+        toolsEnvAgentJob.script.set(
                 "pipeline {\n" +
                 "  agent label:'remote'\n" +
                 "  tools {\n" +
@@ -87,14 +80,68 @@ public class DeclarativePipelineTest extends AbstractJUnitTest {
                 "}\n"
         );
 
-        secondJob.sandbox.check();
-        secondJob.save();
+        toolsEnvAgentJob.sandbox.check();
+        toolsEnvAgentJob.save();
 
-        Build secondBuild = secondJob.startBuild().shouldSucceed();
-        String console = secondBuild.getConsole();
-        assertThat(console, containsRegexp("\\(first\\)", Pattern.MULTILINE));
-        assertThat(console, containsRegexp("Apache Maven 3\\.1\\.0", Pattern.MULTILINE));
-        assertThat(console, containsRegexp("\\(second\\)", Pattern.MULTILINE));
-        assertThat(console, containsRegexp("FOO is BAR", Pattern.MULTILINE));
+        Build toolsEnvAgentBuild = toolsEnvAgentJob.startBuild().shouldSucceed();
+        String toolsEnvAgentConsole = toolsEnvAgentBuild.getConsole();
+        assertThat(toolsEnvAgentConsole, containsRegexp("\\(first\\)", Pattern.MULTILINE));
+        assertThat(toolsEnvAgentConsole, containsRegexp("Apache Maven 3\\.1\\.0", Pattern.MULTILINE));
+        assertThat(toolsEnvAgentConsole, containsRegexp("\\(second\\)", Pattern.MULTILINE));
+        assertThat(toolsEnvAgentConsole, containsRegexp("FOO is BAR", Pattern.MULTILINE));
+
+        WorkflowJob missingAgentJob = jenkins.jobs.create(WorkflowJob.class);
+        missingAgentJob.script.set(
+                "pipeline {\n" +
+                "  stages {\n" +
+                "    stage('foo') {\n" +
+                "      steps {\n" +
+                "        echo 'Hello world'\n" +
+                "      }\n" +
+                "    }\n" +
+                "  }\n" +
+                "}\n"
+        );
+
+        missingAgentJob.sandbox.check();
+        missingAgentJob.save();
+
+        Build missingAgentBuild = missingAgentJob.startBuild().shouldFail();
+        String missingAgentConsole = missingAgentBuild.getConsole();
+        assertThat(missingAgentConsole, containsRegexp("Missing required section 'agent'"));
+        assertThat(missingAgentConsole, not(containsRegexp("Hello world")));
+
+        WorkflowJob missingToolVersionJob = jenkins.jobs.create(WorkflowJob.class);
+        missingToolVersionJob.script.set(
+                "pipeline {\n" +
+                "  agent label:'remote'\n" +
+                "  tools {\n" +
+                "    maven 'some-other-version'\n" +
+                "  }\n" +
+                "  environment {\n" +
+                "    FOO = 'BAR'\n" +
+                "  }\n" +
+                "  stages {\n" +
+                "    stage('first') {\n" +
+                "      steps {\n" +
+                "        sh 'mvn --version'\n" +
+                "      }\n" +
+                "    }\n" +
+                "    stage('second') {\n" +
+                "      steps {\n" +
+                "        echo \"FOO is ${env.FOO}\"\n" +
+                "      }\n" +
+                "    }\n" +
+                "  }\n" +
+                "}\n"
+        );
+
+        missingToolVersionJob.sandbox.check();
+        missingToolVersionJob.save();
+
+        Build missingToolVersionBuild = missingToolVersionJob.startBuild().shouldFail();
+        String missingToolVersionConsole = missingToolVersionBuild.getConsole();
+        assertThat(missingToolVersionConsole, containsRegexp("Tool type 'maven' does not have an install of 'some-other-version' configured"));
+        assertThat(missingToolVersionConsole, not(containsRegexp("FOO is BAR")));
     }
 }
