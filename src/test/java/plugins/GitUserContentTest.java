5d8f54a16f42f928e90f9c7c838660552fd4c207
Evaristo Gutiérrez <egutierrez@cloudbees.com>
2017-02-07 15:33:19 +0100

    [JENKINS-41796] Include checking /userContent to verify the git repo uses that.

--- src/test/java/plugins/GitUserContentTest.java
+++ src/test/java/plugins/GitUserContentTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.jenkinsci.test.acceptance.Matchers.hasContent;
 
 /**
  * Verifies the Git repository setup by git-userContent plugin is working fine for some basic operations.
@@ -17,8 +18,11 @@ import static org.hamcrest.Matchers.containsString;
 @WithPlugins({"git-userContent", "git", "workflow-job", "workflow-cps", "workflow-basic-steps", "workflow-durable-task-step"})
 public class GitUserContentTest extends AbstractJUnitTest {
 
-    private static final String FILE_NAME = "text.txt";
-    private static final String FILE_CONTENT = "anytextwhatever";
+    private static final String NEW_FILE_NAME = "text.txt";
+    private static final String NEW_FILE_CONTENT = "anytextwhatever";
+
+    private static final String GIT_FOLDER = ".git";
+    private static final String README_FILE = "readme.txt";
 
     @Before
     public void setUp() throws Exception {
@@ -30,21 +34,27 @@ public class GitUserContentTest extends AbstractJUnitTest {
 
     @Test
     public void userContentGitTest() throws Exception {
+        this.checkUserContent(README_FILE);
+
         this.createAndBuildWorkflowJob(String.format("node {\n" +
                 "    git url: '%suserContent.git/'\n" +
                 "    writeFile encoding: 'UTF-8', file: '%s', text: '%s'\n" +
                 "    sh 'git add %s'\n" +
                 "    sh 'git commit -m \"Including new file\"'\n" +
                 "    sh 'git push --set-upstream origin master'\n" +
-                "}", jenkins.url, FILE_NAME, FILE_CONTENT, FILE_NAME));
+                "}", jenkins.url, NEW_FILE_NAME, NEW_FILE_CONTENT, NEW_FILE_NAME));
+
+        this.checkUserContent(README_FILE, GIT_FOLDER, NEW_FILE_NAME);
 
         final String pullConsole = this.createAndBuildWorkflowJob(String.format("node {\n" +
                 "    git url: '%suserContent.git/'\n" +
                 "    sh 'cat %s'\n" +
-                "}", jenkins.url, FILE_NAME)).getConsole();
+                "}", jenkins.url, NEW_FILE_NAME)).getConsole();
 
         assertThat(pullConsole, containsString("Cloning the remote Git repository"));
-        assertThat(pullConsole, containsString(FILE_CONTENT));
+        assertThat(pullConsole, containsString(NEW_FILE_CONTENT));
+
+        this.checkUserContent(README_FILE, GIT_FOLDER, NEW_FILE_NAME);
     }
 
     private Build createAndBuildWorkflowJob(final String script) {
@@ -52,6 +62,14 @@ public class GitUserContentTest extends AbstractJUnitTest {
         job.script.set(script);
         job.save();
         return job.startBuild().shouldSucceed();
+    }
+
+    private void checkUserContent(String... expectedFiles) {
+        jenkins.visit("userContent");
+
+        for (final String file : expectedFiles) {
+            assertThat(driver, hasContent(file));
+        }
     }
 
 }
