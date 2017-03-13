84fd7d1440c6a35a99b008f640da96b31169e052
Ulli Hafner <ullrich.hafner@gmail.com>
2017-03-13 11:19:35 +0100

    Revert "Make sh script work on older bash versions."
    This reverts commit b5588c804c0119c275a96043e68e06da2e1a2f14.

--- jut-server.sh
+++ jut-server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-CMD="target/appassembler/bin/jut-server"
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+CMD="$DIR/target/appassembler/bin/jut-server"
 if [ ! -s $CMD ]
 then
-  mvn package -DskipTests
+  mvn package -DskipTests -f "$DIR/pom.xml"
 fi
 sh "$CMD" "$@"
