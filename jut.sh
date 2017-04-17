5c62c8e2ce5fd7ba31527d9053d435cbc5a60bbd
Lukasz Koceluch <koceluch@hm.edu>
2017-04-17 14:00:33 +0200

    refactored jut.sh

--- jut.sh
+++ jut.sh
@@ -1,6 +1,2 @@
-JENKINS_WAR=/usr/lib/jenkins/jenkins.war 
-PLUGINS_DIR=/var/lib/jenkins/plugins/ 
-WORSPACE=/tmp 
-BROWSER=firefox 
-./jut-server.sh -n 2
+env JENKINS_WAR=/usr/lib/jenkins/jenkins.war PLUGINS_DIR=/var/lib/jenkins/plugins/ WORSPACE=/tmp BROWSER=chrome ./jut-server.sh -n 2
 
