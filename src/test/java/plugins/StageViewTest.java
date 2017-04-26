836de8f3b77be02599caed1c9abcf878ac54ed1b
Boris Dippolter <b.dippolter@testbirds.de>
2017-04-26 18:52:06 +0200

    some refactoring to remove duplicate code. Needs still soem work

--- src/test/java/plugins/StageViewTest.java
+++ src/test/java/plugins/StageViewTest.java
@@ -9,6 +9,12 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsString;
@@ -16,31 +22,23 @@ import static org.hamcrest.Matchers.containsString;
 /**
  * Created by boris on 17.04.17.
  * Base Implementation of the stageview test.
- * TODO: Remove inline editor Tests and store in files/improve with builder pattern
- * TODO: Build po for
+ * TODO: Build po for / in progress
  */
+@WithPlugins("workflow-aggregator")
 public class StageViewTest extends AbstractJUnitTest{
+
 
     /**
      * This tests create a simple stage. It checks if after the first build the stage view is now part of the job page.
      * @throws Exception
      */
-    @WithPlugins("workflow-aggregator@1.1")
+
     @Test
     public void jobShouldContainStageview() throws Exception {
-        WorkflowJob job = jenkins.jobs.create(WorkflowJob.class);
-        job.script.set("node {\n" +
-                "    stage ('Clone sources'){\n" +
-                "    echo 'cloned'\n" +
-                "    } " +
-                "}");
-
-        job.sandbox.check();
-        job.save();
+        WorkflowJob job = this.saveWorkflowJobWithFile("stageview_plugin/single_job.txt");
         Build build = job.startBuild().shouldSucceed();
         job.open();
-        WebElement webElement = this.driver.findElement(By.id("pipeline-box"));
-        assertThat(webElement.getText(),containsString("Stage View"));
+        assertThat(driver.findElement(By.id("pipeline-box")).getText(),containsString("Stage View"));
     }
 
     /**
@@ -83,7 +81,6 @@ public class StageViewTest extends AbstractJUnitTest{
                 "    echo 'build'\n" +
                 "    }\n " +
                 "}");
-
         job.sandbox.check();
         job.save();
         Build build = job.startBuild().shouldSucceed();
@@ -91,6 +88,63 @@ public class StageViewTest extends AbstractJUnitTest{
         WebElement webElement = this.driver.findElement(By.xpath("//*[@id=\"pipeline-box\"]/div/div/table/thead/tr"));
         assertThat(webElement.getText(),containsString("Clone sources"));
         assertThat(webElement.getText(),containsString("build"));
-
+        //assertThat(webElement.getText(),containsString("build"));
     }
+
+    /**
+     * Does check multiple jobs in the stage view.
+     * @throws Exception
+     */
+    @WithPlugins("workflow-aggregator@1.1")
+    @Test
+    public void stageViewContainsMultipleStagesWithFail() throws Exception {
+        WorkflowJob job = jenkins.jobs.create(WorkflowJob.class);
+        job.script.set("node {\n" +
+                "    stage ('Clone sources'){\n" +
+                "    echo 'cloned'\n" +
+                "    }\n " +
+                "    stage ('build'){\n" +
+                "    echo 'Meant to fail'\n" +
+                "    sh \"exit 1\"\n" +
+                "    }\n " +
+                "}");
+        job.sandbox.check();
+        job.save();
+        Build build = job.startBuild().shouldFail();
+        job.open();
+        job.getNavigationLinks();
+        WebElement webElement = this.driver.findElement(By.xpath("//*[@id=\"pipeline-box\"]/div/div/table/thead/tr"));
+        assertThat(driver.getPageSource(),containsString("FAILED"));
+        assertThat(driver.getPageSource(),containsString("SUCCESS"));
+        //order check
+        //gesamtergebnis
+        //refactor
+    }
+
+    /**
+     * Helper method to convenient located a file int he ressource folder
+     * @param fileName the naame of the file including path
+     * @return return the file content as a String
+     */
+    private String readFromRessourceFolder(String fileName) {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        return  new BufferedReader(new InputStreamReader(classloader.getResourceAsStream(fileName)))
+                .lines().collect(Collectors.joining("\n"));
+    }
+
+
+    /**
+     * Helper Method for Workflow job egneration. The filename represents
+     * the File to be reat as the pipeline definition file
+     * @param fileName the naame of the file including path
+     * @return return the newly generated workflow job with a defined pipeline
+     */
+    private WorkflowJob saveWorkflowJobWithFile(String fileName) {
+        WorkflowJob job = jenkins.jobs.create(WorkflowJob.class);
+        job.script.set(readFromRessourceFolder(fileName));
+        job.sandbox.check();
+        job.save();
+        return job;
+    }
+
 }
