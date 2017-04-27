d3d89630cc444aca25330016b393b10ec4f55923
Boris Dippolter <b.dippolter@testbirds.de>
2017-04-27 12:08:13 +0200

    Refactor other testcases with new methods

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,7 +1,19 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 /**
  * Created by boris on 26.04.17.
  */
 public class ExampleTest {
+
+    @Test
+    public void testSth() {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        String sth =  new BufferedReader(new InputStreamReader(classloader.getResourceAsStream("stageview_plugin/single_job.txt")))
+                .lines().collect(Collectors.joining("\n"));
+        System.out.println(sth);
+    }
 }
