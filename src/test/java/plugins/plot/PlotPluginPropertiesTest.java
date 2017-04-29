6a58cded2a4ee273bef785b1ce74a458850620c4
stefanSchuhbaeck <stefan@schuhbaeck.eu>
2017-04-29 17:33:16 +0200

    initial Test for properties-file as data source

--- src/test/java/plugins/plot/PlotPluginPropertiesTest.java
+++ src/test/java/plugins/plot/PlotPluginPropertiesTest.java
@@ -20,10 +20,34 @@ import static org.junit.Assert.assertFalse;
 public class PlotPluginPropertiesTest extends AbstractJUnitTest {
 
     private FreeStyleJob job;
+    private PlotPublisher pub;
 
     @Before
     public void setUp() {
         job = jenkins.jobs.create();
+        pub = job.addPublisher(PlotPublisher.class);
     }
 
+    @Test
+    public void generate_simple_plot_properties(){
+        job.configure();
+        job.copyResource("/plot_plugin/plot.properties");
+
+        Plot plot = pub.getPlot(1);
+        plot.setGroup("Group_1");
+        plot.setTitle("PropertiesPlot1");
+
+        pub.source("properties", "plot.properties");
+        job.save();
+        job.startBuild().shouldSucceed();
+
+        assertThatBuildHasPlot("PropertiesPlot1", "Group_1");
+
+    }
+
+    private void assertThatBuildHasPlot(String title, String group) {
+        job.visit("plot");
+        find(by.xpath("//h1[contains(text(), '%s')]", group));
+        find(by.xpath("//select[@name='choice']/option[contains(text(), '%s')]",title));
+    }
 }
