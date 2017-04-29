ba99ffc7ec9e5762aa65bd268f732ac978f6d059
Lukasz Koceluch <koceluch@hm.edu>
2017-04-29 19:49:27 +0200

    Implemented generateSimpleXmlTest.

--- src/test/java/plugins/plot/PlotPluginXmlTest.java
+++ src/test/java/plugins/plot/PlotPluginXmlTest.java
@@ -26,5 +26,36 @@ public class PlotPluginXmlTest extends AbstractJUnitTest {
         job = jenkins.jobs.create();
     }
 
+    @Test
+    public void generateSimpleXmlTest() {
+        job.configure();
+        job.copyResource(resource("/plot_plugin/plot.xml"));
+        PlotPublisher pub = job.addPublisher(PlotPublisher.class);
+
+        Plot p1 = pub.getPlot(1);
+        p1.setGroup("G1");
+        p1.setTitle("XML plot");
+        p1.setStyle("Line");
+
+        XmlDataSeries xmlDataSeries1 =  p1.addDataSeries(XmlDataSeries.class);
+        xmlDataSeries1.setFile("plot.xml");
+        xmlDataSeries1.setXpath("count(/books/book[price>35.00])");
+        xmlDataSeries1.setUrl("http://foo.bar");
+        xmlDataSeries1.selectResultTypNumber();
+
+        // this will override first csv line inside plot
+        //XmlDataSeries xmlDataSeries2 = p1.addDataSeries(XmlDataSeries.class);
+        //xmlDataSeries2.setFile("plot.xml");
+        //xmlDataSeries2.setUrl("http://foo.bar2");
+        //xmlDataSeries2.setXpath("count(/books/book[author='Max, Mustermann'])");
+        //xmlDataSeries2.selectResultTypNumber();
+
+        job.save();
+        job.startBuild().shouldSucceed();
+        job.visit("plot");
+
+        find(by.xpath("//map/area[contains(@href, '%s')]", "http://foo.bar"));
+
+    }
 
 }
