485849c3dd89625b018a82eaf79c262a96bf362a
Boris Dippolter <b.dippolter@testbirds.de>
2017-05-03 18:00:27 +0200

    Extend po, add new test for build number

--- src/main/java/org/jenkinsci/test/acceptance/po/StageView.java
+++ src/main/java/org/jenkinsci/test/acceptance/po/StageView.java
@@ -1,12 +1,32 @@
 package org.jenkinsci.test.acceptance.po;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 import java.util.ArrayList;
 import java.util.Collection;
 
 /**
  * Created by boris on 26.04.17.
  */
-public class StageView {
+public class StageView extends  PageAreaImpl {
+
+    /**
+     * Main bocx for all content
+     */
+    public static final String ID_WRAPPER = "pipeline-box";
+
+    /**
+     * Main job wrapper. Alle children in this are "tr".
+     */
+    public static final String XPATH_JOBS = "//*[@id=\"pipeline-box\"]/div/div/table/tbody[2]";
+
+    //public static final String XPATh_Exmaple_for stages in job
+    // = //*[@id="pipeline-box"]/div/div/table/tbody[2]/tr[1]/td[2]
+
+    public StageView(PageObject context, String path) {
+        super(context, path);
+    }
 
     public Collection getAllStageViewJobs() {
         //TODO implement
@@ -31,6 +51,10 @@ public class StageView {
     public Collection getAllStageViewHeadLines() {
         //TODO implement
         return null;
+    }
+
+    public WebElement getRootElementName() {
+        return super.find(By.id("pipeline-box"));
     }
 
 }
--- src/test/java/plugins/StageViewTest.java
+++ src/test/java/plugins/StageViewTest.java
@@ -1,10 +1,10 @@
 package plugins;
 
+import javafx.scene.paint.Color;
 import org.jenkinsci.test.acceptance.junit.AbstractJUnitTest;
 import org.jenkinsci.test.acceptance.junit.WithPlugins;
-import org.jenkinsci.test.acceptance.po.Build;
-import org.jenkinsci.test.acceptance.po.BuildHistory;
-import org.jenkinsci.test.acceptance.po.WorkflowJob;
+import org.jenkinsci.test.acceptance.po.*;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -21,7 +21,9 @@ import static org.hamcrest.Matchers.containsString;
 
 /**
  * Created by boris on 17.04.17.
- * Base Implementation of the stageview test.
+ * Base Implementation of the stageview test as a component. Important aspect of this testclass is the correct
+ * visualisation depending of stages and builds (matrix).
+ *
  * TODO: Build po for / in progress
  */
 @WithPlugins("workflow-aggregator")
@@ -31,18 +33,38 @@ public class StageViewTest extends AbstractJUnitTest{
     public static final String SINGLE_JOB = "stageview_plugin/single_job.txt";
     public static final String MULTI_JOB = "stageview_plugin/multi_job.txt";
     public static final String MUTLI_JOB_FAIL = "stageview_plugin/multi_job_fail.txt";
+
+    private PageObject context;
+    private String path;
+    private StageView stageView;
+
+    @Before
+    public void before() {
+        stageView = new StageView(context, path);
+    }
     
     /**
      * This tests create a simple stage. It checks if after the first build the stage view is now part of the job page.
      * @throws Exception
      */
-
     @Test
     public void jobShouldContainStageview() throws Exception {
         WorkflowJob job = this.saveWorkflowJobWithFile(SINGLE_JOB);
         Build build = job.startBuild().shouldSucceed();
         job.open();
-        assertThat(driver.findElement(By.id("pipeline-box")).getText(),containsString("Stage View"));
+        assertThat(stageView.getRootElementName().getText(),containsString("Stage View"));
+    }
+
+    /**
+     * Test validates against the current build number. Every row(aka build) contains the correct build number.
+     */
+    public void jobNumberShouldbeCorrect() {
+            WorkflowJob job = this.saveWorkflowJobWithFile(SINGLE_JOB);
+            Build build = job.startBuild().shouldFail();
+            job.open();
+            job.getNavigationLinks();
+            WebElement webElement = this.driver.findElement(By.xpath("//*[@id=\"pipeline-box\"]/div/div/table/tbody[2]/tr[1]/td[1]/div/div/div[1]/span"));
+            assertThat(webElement.getText(),containsString(String.valueOf(build.getNumber());
     }
 
     /**
@@ -76,7 +98,8 @@ public class StageViewTest extends AbstractJUnitTest{
     }
 
     /**
-     * Does check multiple jobs in the stage view.
+     * Does check multiple jobs in the stage view. One with a failed, and one with a success.
+     * //TODO check the color of these jobs
      * @throws Exception
      */
     @Test
@@ -106,8 +129,8 @@ public class StageViewTest extends AbstractJUnitTest{
 
 
     /**
-     * Helper Method for Workflow job egneration. The filename represents
-     * the File to be reat as the pipeline definition file
+     * Helper Method for Workflow job generation. The filename represents
+     * the File to be read as the pipeline definition file
      * @param fileName the naame of the file including path
      * @return return the newly generated workflow job with a defined pipeline
      */
