8a664dfb331512ada9e0d8d69f30f8581d224e0b
Kurt Fastner <k.fastner@live.de>
2017-07-11 10:52:06 +0200

    Updating PR: Additional gradle tests #339

--- src/main/java/org/jenkinsci/test/acceptance/plugins/gradle/GradleTask.java
+++ src/main/java/org/jenkinsci/test/acceptance/plugins/gradle/GradleTask.java
@@ -1,19 +1,20 @@
 package org.jenkinsci.test.acceptance.plugins.gradle;
 
-/**
- * Created by wlaschinger on 29.06.17.
- */
 public enum  GradleTask {
 
     HELLO("hello", "Hello world!"),
     FIRST("firstTask", "First!"),
     SECOND("secondTask", "Second!"),
-    ENVIRONMENT_VARIABLES("environmentVariables", null),
-    JOB_PARAM_AS_PROJECT_PROPERTIES("jobParametersAsProjectProperties", null),
-    JOB_PARAM_AS_SYSTEM_PROPERTIES("jobParametersAsSystemProperties", null);
+    ENVIRONMENT_VARIABLES("environmentVariables"),
+    JOB_PARAM_AS_PROJECT_PROPERTIES("jobParametersAsProjectProperties"),
+    JOB_PARAM_AS_SYSTEM_PROPERTIES("jobParametersAsSystemProperties");
 
     private String name;
     private String println;
+
+    GradleTask(final String name){
+        this.name = name;
+    }
 
     GradleTask(final String name, final String println){
         this.name = name;
