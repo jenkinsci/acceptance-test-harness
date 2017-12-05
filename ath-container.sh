be8037bfb22c528cf1a5926e9eee76c905c50e40
Oliver Gondža <ogondza@gmail.com>
2017-12-05 14:02:55 +0100

    Introduce ath-container.sh helper

---
+++ ath-container.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# https://disconnected.systems/blog/another-bash-strict-mode/
+set -euo pipefail
+trap 's=$?; echo "$0: Error $s on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+
+uid=$(id -u)
+gid=$(id -g)
+tag="jenkins/ath"
+
+docker build --build-arg=uid="$uid" --build-arg=gid="$gid" src/main/resources/ath-container -t "$tag"
+
+run_opts="--interactive --tty --rm --publish-all --user ath-user --workdir /home/ath-user"
+run_drive_mapping="-v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/home/ath-user -v ${HOME}/.m2/repository:/home/ath-user/.m2/repository"
+docker run $run_opts $run_drive_mapping $tag /bin/bash
