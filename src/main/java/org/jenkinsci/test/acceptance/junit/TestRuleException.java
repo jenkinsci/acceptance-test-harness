35e16d8c7643bf46a15fc0a0228a3b9a6ace6848
BurnMyDread <zach@valvatorez>
2018-02-05 11:37:52 -0500

    docs

--- src/main/java/org/jenkinsci/test/acceptance/junit/TestRuleException.java
+++ src/main/java/org/jenkinsci/test/acceptance/junit/TestRuleException.java
@@ -2,15 +2,18 @@ package org.jenkinsci.test.acceptance.junit;
 
 import org.junit.rules.TestRule;
 
+/**
+ * This is a service exception that wraps the TestRule failure to all traceability back to failing test rules.
+ */
 public class TestRuleException extends RuntimeException {
-
+    //This is the test rule that failed.
     public final TestRule testRule;
-
+    //We take the exception, as a throwable, and the test rule
     public TestRuleException(Throwable throwable, TestRule failingRule) {
         super(throwable);
         testRule = failingRule;
     }
-
+    //This displays what the test rule that failed.
     @Override
     public String getMessage() {
         return "TestRule " + testRule.getClass() + " failed with stacktrace.\n" + super.getMessage();
