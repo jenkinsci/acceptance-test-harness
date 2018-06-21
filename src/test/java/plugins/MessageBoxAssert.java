13dc31fcb474fa47032a57c93018a46f3ce3d186
AlexanderPraegla <alexander.praegla@gmail.com>
2018-06-21 15:29:48 +0200

    Added custom assert object for message box page object

---
+++ src/test/java/plugins/MessageBoxAssert.java
@@ -0,0 +1,61 @@
+package plugins;
+
+import org.assertj.core.api.AbstractAssert;
+import org.jenkinsci.test.acceptance.po.MessageBox;
+
+public class MessageBoxAssert extends AbstractAssert<MessageBoxAssert, MessageBox> {
+
+	public MessageBoxAssert(MessageBox actual) {
+		super(actual, actual.getClass());
+	}
+
+	public static MessageBoxAssert assertThat(MessageBox actual) {
+		return new MessageBoxAssert(actual);
+	}
+
+	public MessageBoxAssert hasErrorMessagesSize(int size) {
+		isNotNull();
+
+		if (actual.getErrorMsgContent().size() != size) {
+			failWithMessage("Expected size of error message box to be <%d> but was <%d>", size, actual.getErrorMsgContent().size());
+		}
+
+		return this;
+	}
+
+	public MessageBoxAssert hasInfoMessagesSize(int size) {
+		isNotNull();
+
+		if (actual.getInfoMsgContent().size() != size) {
+			failWithMessage("Expected size of info message box to be <%d> but was <%d>", size, actual.getErrorMsgContent().size());
+		}
+
+		return this;
+	}
+
+	public MessageBoxAssert containsErrorMessage(String errorMessage) {
+		isNotNull();
+
+		for (String message : actual.getErrorMsgContent()) {
+			if (message.contains(errorMessage)) {
+				return this;
+			}
+		}
+
+		failWithMessage("Expected message box to have error message <%s>", errorMessage);
+		return this;
+	}
+
+	public MessageBoxAssert containsInfoMessage(String infoMessage) {
+		isNotNull();
+
+		for (String message : actual.getInfoMsgContent()) {
+			if (message.contains(infoMessage)) {
+				return this;
+			}
+		}
+
+		failWithMessage("Expected message box to have info message <%s>", infoMessage);
+		return this;
+	}
+}
