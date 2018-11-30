46fc246317e07edc3345aec1a8dc87daec70f316
Oliver Gondža <ogondza@gmail.com>
2018-11-30 13:05:39 +0100

    Adjust --shm-size for ath-contianer.sh too

--- ath-container.sh
+++ ath-container.sh
@@ -11,4 +11,4 @@ docker build --build-arg=uid="$uid" --build-arg=gid="$gid" src/main/resources/at
 
 run_opts="--interactive --tty --rm --publish-all --user ath-user --workdir /home/ath-user/ath-sources"
 run_drive_mapping="-v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/home/ath-user/ath-sources -v ${HOME}/.m2/repository:/home/ath-user/.m2/repository"
-docker run $run_opts $run_drive_mapping $tag /bin/bash
+docker run --shm-size 2g $run_opts $run_drive_mapping $tag /bin/bash
