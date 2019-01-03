e150194da6c710a5915212019f3d1c963dd8bf4b
Ramon Leon <manuelramonleonjimenez@gmail.com>
2019-01-03 13:45:17 +0100

    [INFRA-1953] Restore docker run sentence as it has no sense to check the set-java.sh script here

--- ath-container.sh
+++ ath-container.sh
@@ -13,7 +13,4 @@ docker build --build-arg=uid="$uid" --build-arg=gid="$gid" src/main/resources/at
 run_opts="--interactive --tty --rm --publish-all --user ath-user --workdir /home/ath-user/ath-sources"
 run_drive_mapping="-v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/home/ath-user/ath-sources -v ${HOME}/.m2/repository:/home/ath-user/.m2/repository"
 
-#Allow to run this script with old images without the set-java.sh script. A message is showed if it doesn't exist to
-#alert that you are using an old ATH image (... type: ./set-java.sh: not found). Reason: it's common to change the $tag
-#variable to use existing images.
-docker run --shm-size 2g $run_opts $run_drive_mapping $tag /bin/bash -c "type ./set-java.sh >/dev/null && ./set-java.sh $java_version; bash"
+docker run --shm-size 2g $run_opts $run_drive_mapping $tag /bin/bash -c "./set-java.sh $java_version; bash"
