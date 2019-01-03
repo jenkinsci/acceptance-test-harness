04ad9a53a21e56adcd8a813cfee56e35bc93b4a0
Ramon Leon <manuelramonleonjimenez@gmail.com>
2019-01-03 13:45:56 +0100

    [INFRA-1953] Remove extraline

--- ath-container.sh
+++ ath-container.sh
@@ -12,5 +12,4 @@ docker build --build-arg=uid="$uid" --build-arg=gid="$gid" src/main/resources/at
 
 run_opts="--interactive --tty --rm --publish-all --user ath-user --workdir /home/ath-user/ath-sources"
 run_drive_mapping="-v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/home/ath-user/ath-sources -v ${HOME}/.m2/repository:/home/ath-user/.m2/repository"
-
 docker run --shm-size 2g $run_opts $run_drive_mapping $tag /bin/bash -c "./set-java.sh $java_version; bash"
