c18d6412f8af4916f3e5754068a6a7088e52a9a3
Ulli Hafner <ullrich.hafner@gmail.com>
2019-02-13 22:16:02 +0100

    Added default test case.

--- test.sh
+++ test.sh
@@ -4,10 +4,17 @@ GECKO_DRIVER_PATH=/usr/local/bin/geckodriver
 CHROME_DRIVER_PATH=/usr/local/bin/chromedriver
 
 BROWSER=chrome
-RETRY=1
+RETRY=2
 TEST_CASE=$1
 ELASTIC=5 # increase if your machine is slow
 
+if [ -z "$TEST_CASE" ];
+then
+    TEST_CASE=WarningsNextGenerationPluginTest
+fi
+
 mvnOptions="-Dsurefire.rerunFailingTestsCount=${RETRY} -Dwebdriver.gecko.driver=${GECKO_DRIVER_PATH} -DElasticTime.factor=${ELASTIC} -Dwebdriver.chrome.driver=${CHROME_DRIVER_PATH}"
+
+echo Running: env LC_NUMERIC="en_US.UTF-8" BROWSER=${BROWSER} mvn test -o -Dtest=${TEST_CASE} ${mvnOptions}
 
 env LC_NUMERIC="en_US.UTF-8" BROWSER=${BROWSER} mvn test -o -Dtest=${TEST_CASE} ${mvnOptions}
