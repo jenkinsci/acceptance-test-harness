8f2088a1f11e3ca710cc098d6235e49abf62db66
Oliver Gondža <ogondza@gmail.com>
2019-05-13 12:31:54 +0200

    Make sure test works for older JIRA as well

--- src/test/java/plugins/JiraPluginTest.java
+++ src/test/java/plugins/JiraPluginTest.java
@@ -25,7 +25,7 @@ import org.junit.experimental.categories.Category;
 
 import static org.junit.Assert.fail;
 
-@WithPlugins({"jira@3.0.7", "git"})
+@WithPlugins({"jira", "git"})
 @Category(DockerTest.class)
 @WithDocker
 @Since( "2.73.3" )
