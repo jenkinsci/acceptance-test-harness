5d2d04de9591e7b6d06dd491d6dffde92e75f8cc
Oliver Gondža <ogondza@gmail.com>
2020-01-23 15:17:06 +0100

    Prototype nested ATH invocation from Jenkins plugin

---
+++ src/main/java/org/jenkinsci/test/acceptance/utils/ContainerizedRunner.java
@@ -0,0 +1,69 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) Red Hat, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jenkinsci.test.acceptance.utils;
+
+import org.jenkinsci.test.acceptance.docker.Docker;
+import org.jenkinsci.utils.process.CommandBuilder;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+/**
+ * Main class to run ATH test maven module inside ATH container.
+ *
+ * @author ogondza.
+ */
+public final class ContainerizedRunner {
+    private static final Logger LOGGER = Logger.getLogger(ContainerizedRunner.class.getName());
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Docker d = new Docker(); // static fields initialized in constructor :-/
+
+        // TODO either accept from the target plugin or bake in the ATH version when released
+        String tag = "jenkins/ath:acceptance-test-harness-1.69";
+        LOGGER.info("Pulling image " + tag);
+        assert d.cmd("pull", tag).system() == 0;
+
+        //String athModuleSources = System.getProperty("user.dir");
+
+        CommandBuilder cmd = d.cmd(
+                "run", "--rm",
+                "--user=ath-user",
+                "--workdir=/home/ath-user/sources",
+                "-v=/var/run/docker.sock:/var/run/docker.sock",
+                "-v=" + System.getenv("MAVEN_PROJECTBASEDIR") + ":/home/ath-user/sources",
+                "-v=" + System.getenv("HOME") + "/.m2/repository:/home/ath-user/.m2/repository",
+                "--shm-size=2g",
+                "-e", "JENKINS_VERSION=" + System.getenv("JENKINS_VERSION"),
+                "-e", "LOCAL_JARS=" + System.getenv("LOCAL_JARS"),
+                "-e", "FORM_ELEMENT_PATH_VERSION=1.6",
+                tag,
+                "bash", "-c", "eval $(vnc.sh); mvn package -pl=ui-tests -Dmaven.test.skip=false" // TODO set java version
+        );
+        LOGGER.info("Running tests in container: " + cmd.toList());
+        int exit = cmd.system();
+        LOGGER.info("Execution terminated with exit code " + exit);
+    }
+}
