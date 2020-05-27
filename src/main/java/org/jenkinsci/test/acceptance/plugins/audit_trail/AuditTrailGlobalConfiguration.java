fdfb966500725b4c0c180779e60baac43c310d71
Esther Alvarez Feijoo <ealvarez@cloudbees.com>
2020-05-27 14:26:48 +0200

    [JENKINS-62481] Fix AuditTrailPluginTest

--- src/main/java/org/jenkinsci/test/acceptance/plugins/audit_trail/AuditTrailGlobalConfiguration.java
+++ src/main/java/org/jenkinsci/test/acceptance/plugins/audit_trail/AuditTrailGlobalConfiguration.java
@@ -1,20 +1,20 @@
 package org.jenkinsci.test.acceptance.plugins.audit_trail;
 
 import org.jenkinsci.test.acceptance.po.Control;
-import org.jenkinsci.test.acceptance.po.GlobalPluginConfiguration;
 import org.jenkinsci.test.acceptance.po.JenkinsConfig;
+import org.jenkinsci.test.acceptance.po.PageAreaImpl;
 
 /**
  * Global configuration section of the audit-trail plugin.
  *
  * @author Kohsuke Kawaguchi
  */
-public class AuditTrailGlobalConfiguration extends GlobalPluginConfiguration {
+public class AuditTrailGlobalConfiguration extends PageAreaImpl {
 
     public final Control addLogger = control("hetero-list-add[loggers]");
 
     public AuditTrailGlobalConfiguration(JenkinsConfig context) {
-        super(context, "audit-trail");
+        super(context, "/hudson-plugins-audit_trail-AuditTrailPlugin");
     }
 
     // TODO
