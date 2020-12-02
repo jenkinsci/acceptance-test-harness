e14254f7c4e9720fa5614830313dd1643c4743ab
A. Jard <angelique.jard@gmail.com>
2020-12-02 21:08:07 +0100

    Move LDAP default configuration to Docker fixture. (#626)
    Co-authored-by: aHenryJard <ajard@cloudbees.com>

--- src/main/java/org/jenkinsci/test/acceptance/docker/fixtures/LdapContainer.java
+++ src/main/java/org/jenkinsci/test/acceptance/docker/fixtures/LdapContainer.java
@@ -2,6 +2,7 @@ package org.jenkinsci.test.acceptance.docker.fixtures;
 
 import org.jenkinsci.test.acceptance.docker.DockerContainer;
 import org.jenkinsci.test.acceptance.docker.DockerFixture;
+import org.jenkinsci.test.acceptance.plugins.ldap.LdapDetails;
 
 /**
  * openLDAP (slapd) Container with a small user directory.
@@ -31,6 +32,13 @@ public class LdapContainer extends DockerContainer {
 
     public String getManagerPassword() {
         return "jenkins";
+    }
+
+    /**
+     * @return default ldap connection details from current running docker LdapContainer.
+     */
+    public LdapDetails createDefault() {
+        return new LdapDetails(getHost(), getPort(), getManagerDn(), getManagerPassword(), getRootDn());
     }
 
 }
