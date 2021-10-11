86d2ae11aebb94079072d1a314715244ec99f82f
Kenneth Rogers <kerogers@cloudbees.com>
2021-10-11 15:33:38 -0400

    Make httpPort protected so derived classes don't have to re-implement it.

--- src/main/java/org/jenkinsci/test/acceptance/controller/WinstoneController.java
+++ src/main/java/org/jenkinsci/test/acceptance/controller/WinstoneController.java
@@ -43,7 +43,7 @@ public class WinstoneController extends LocalController {
         }
     }
 
-    private final int httpPort;
+    protected final int httpPort;
 
     @Inject
     public WinstoneController(Injector i) {
