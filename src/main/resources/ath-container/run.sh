cba09996232a8570aff8037c36d1a106fb965ba4
Tim Jacomb <timjacomb1+github@gmail.com>
2021-12-23 19:24:22 +0000

    Bump version

--- src/main/resources/ath-container/run.sh
+++ src/main/resources/ath-container/run.sh
@@ -46,7 +46,7 @@ if [ ! -f $war ]; then
         "latest")
             war=jenkins-latest.war
             # TODO https://github.com/jenkinsci/jenkins/pull/5923
-            url=https://ci.jenkins.io/job/Core/job/jenkins/job/PR-5923/62/artifact/org/jenkins-ci/main/jenkins-war/2.326-rc31999.a86620d1b7d7/jenkins-war-2.326-rc31999.a86620d1b7d7.war
+            url=https://repo.jenkins-ci.org/snapshots/org/jenkins-ci/main/jenkins-war/2.327-SNAPSHOT/jenkins-war-2.327-20211223.192222-1.war
         ;;
         "latest-rc")
             war=jenkins-latest-rc.war
