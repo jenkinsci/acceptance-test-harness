3f931a4852cd36957c93ecae7392634cb9709eb3
Tim Jacomb <21194782+timja@users.noreply.github.com>
2021-12-24 07:37:23 +0000

    Test jruby change

--- src/main/resources/ath-container/run.sh
+++ src/main/resources/ath-container/run.sh
@@ -45,7 +45,8 @@ if [ ! -f $war ]; then
     case "$war" in
         "latest")
             war=jenkins-latest.war
-            url=$mirrors/war/latest/jenkins.war
+	    # https://github.com/jenkinsci/jenkins/pull/6103
+            url=https://repo.jenkins-ci.org/incrementals/org/jenkins-ci/main/jenkins-war/2.327-rc31894.13111dbdc40b/jenkins-war-2.327-rc31894.13111dbdc40b.war
         ;;
         "latest-rc")
             war=jenkins-latest-rc.war
