71d96d8ff9c247b89c0c2842a3b6e2e4972d0e30
Tim Jacomb <21194782+timja@users.noreply.github.com>
2021-12-27 09:10:57 +0000

    Update run.sh

--- src/main/resources/ath-container/run.sh
+++ src/main/resources/ath-container/run.sh
@@ -46,7 +46,7 @@ if [ ! -f $war ]; then
         "latest")
             war=jenkins-latest.war
             # TODO https://github.com/jenkinsci/jenkins/pull/5923
-            url=https://repo.jenkins-ci.org/incrementals/org/jenkins-ci/main/jenkins-war/2.327-rc32062.cc50203b9c3e/jenkins-war-2.327-rc32062.cc50203b9c3e.war
+            url=https://ci.jenkins.io/job/Core/job/jenkins/job/PR-5923/81/artifact/org/jenkins-ci/main/jenkins-war/2.327-rc32073.65e10d88f34a/jenkins-war-2.327-rc32073.65e10d88f34a.war
         ;;
         "latest-rc")
             war=jenkins-latest-rc.war
