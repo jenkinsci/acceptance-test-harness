bd7828082771e2a2dffe69834c1715ad1b88e13e
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2022-02-03 10:31:45 +0100

    Trying to un-favorite a project
    This is not working.

--- src/test/java/plugins/FavoriteTest.java
+++ src/test/java/plugins/FavoriteTest.java
@@ -3,9 +3,16 @@ package plugins;
 import org.jenkinsci.test.acceptance.junit.AbstractJUnitTest;
 import org.jenkinsci.test.acceptance.junit.WithPlugins;
 import org.jenkinsci.test.acceptance.plugins.mock_security_realm.MockSecurityRealm;
+import org.jenkinsci.test.acceptance.po.Control;
 import org.jenkinsci.test.acceptance.po.FreeStyleJob;
 import org.jenkinsci.test.acceptance.po.GlobalSecurityConfig;
+import org.jenkinsci.test.acceptance.po.User;
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
+
+import java.time.Duration;
+
+import static org.junit.Assert.assertTrue;
 
 @WithPlugins({"favorite", "mock-security-realm"})
 public class FavoriteTest extends AbstractJUnitTest {
@@ -24,8 +31,18 @@ public class FavoriteTest extends AbstractJUnitTest {
         jenkins.login().doLogin(USER);
 
         jenkins.open();
-        waitFor(by.id("fav_my-project")).click();
-        jenkins.visit("/user/"+USER+"/configure");
-        waitFor(by.id("fav_my-project")).click();
+        WebElement project_fav = waitFor(by.id("fav_my-project"));
+        project_fav.click();
+        waitFor(project_fav).withTimeout(Duration.ofSeconds(5)).until(Control::isStale);
+
+        final User user = new User(jenkins, USER);
+        jenkins.visit(user.getConfigUrl().toString());
+        assertTrue(waitFor(by.id("favorites")).isDisplayed());
+        WebElement project_unfav = waitFor(by.id("fav_my-project"));
+        project_unfav.click();
+        waitFor(project_unfav).withTimeout(Duration.ofSeconds(5)).until(Control::isStale);
+
+        jenkins.visit(user.getConfigUrl().toString());
+        assertTrue(waitFor(by.id("favorites")).getText().isEmpty());
     }
 }
