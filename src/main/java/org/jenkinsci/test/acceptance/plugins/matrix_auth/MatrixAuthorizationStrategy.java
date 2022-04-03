65d2b700744a06a630d5f0e778f1c80a7732ffea
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-04-03 22:47:31 +0100

    Potential fix

--- src/main/java/org/jenkinsci/test/acceptance/plugins/matrix_auth/MatrixAuthorizationStrategy.java
+++ src/main/java/org/jenkinsci/test/acceptance/plugins/matrix_auth/MatrixAuthorizationStrategy.java
@@ -20,7 +20,7 @@ public class MatrixAuthorizationStrategy extends AuthorizationStrategy {
      * Adds a new user to this matrix.
      */
     public MatrixRow addUser(String name) {
-        runThenHandleAlert(() -> this.name.resolve().findElement(by.xpath("../span/span/button | button")).click(),
+        runThenHandleAlert(() -> this.name.resolve().findElement(by.xpath("../span/span/button | button | input[@type='button']")).click(),
                 a -> {
             a.sendKeys(name);
             a.accept();
