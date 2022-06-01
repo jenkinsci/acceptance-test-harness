bbc60642eb23c6af39737fc56aa2fea99f8c7b88
James Nord <jtnord@users.noreply.github.com>
2022-06-01 18:24:32 +0100

    improve security some more by not exposing ports to the world!
    we only need the webdriver and vnc ports exposed on localhost not to the
    world, so limit them a bit more.

--- vars.cmd
+++ vars.cmd
@@ -4,7 +4,7 @@
 set DISPLAY=:0
 set INTERACTIVE=false
 set BROWSER=remote-webdriver-firefox
-set REMOTE_WEBDRIVER_URL=http://0.0.0.0:4444/wd/hub
+set REMOTE_WEBDRIVER_URL=http://127.0.0.1:4444/wd/hub
 set JENKINS_JAVA_OPTS=-Xmx1280m
 
 @REM Jenkins binds to 0.0.0.0 (OMG) so we can use any network but the docker network.
@@ -18,4 +18,4 @@ set SELENIUM_PROXY_HOSTNAME=%IP%
 set JENKINS_LOCAL_HOSTNAME=%IP%
 @echo.
 @echo To start the remote firefox container run the following command:
-@echo docker run --shm-size=256m -d -p 4444:4444 -p 5900:5900 -e no_proxy=localhost -e SCREEN_WIDTH=1680 -e SCREEN_HEIGHT=1090 selenium/standalone-firefox-debug:3.141.59
+@echo docker run --shm-size=256m -d -p 127.0.0.1:4444:4444 -p 127.0.0.1:5900:5900 -e no_proxy=localhost -e SCREEN_WIDTH=1680 -e SCREEN_HEIGHT=1090 selenium/standalone-firefox-debug:3.141.59
