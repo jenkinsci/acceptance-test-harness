9bdfada785a8fcb42c39a0e3bbbb710951825444
Tim Jacomb <21194782+timja@users.noreply.github.com>
2022-06-22 08:59:15 +0100

    Update src/main/resources/ath-container/run.sh

--- src/main/resources/ath-container/run.sh
+++ src/main/resources/ath-container/run.sh
@@ -45,7 +45,7 @@ if [ ! -f $war ]; then
     case "$war" in
         "latest")
             #war=jenkins-latest.war
-            url=https://repo.jenkins-ci.org/snapshots/org/jenkins-ci/main/jenkins-war/2.346-SNAPSHOT/jenkins-war-2.346-20220502.145901-3.war
+            url=https://repo.jenkins-ci.org/snapshots/org/jenkins-ci/main/jenkins-war/2.356-SNAPSHOT/jenkins-war-2.356-20220622.075827-1.war
         ;;
         "latest-rc")
             war=jenkins-latest-rc.war
