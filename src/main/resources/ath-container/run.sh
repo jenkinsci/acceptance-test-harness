adf4f71c939b7658e8bdbe50cd21d612d6d63735
Tim Jacomb <timjacomb1+github@gmail.com>
2022-07-08 17:03:29 +0100

    Test Rework "Updates" table checkbox selection controls
    see https://github.com/jenkinsci/jenkins/pull/6806

--- src/main/resources/ath-container/run.sh
+++ src/main/resources/ath-container/run.sh
@@ -44,7 +44,7 @@ if [ ! -f $war ]; then
     case "$war" in
         "latest")
             war=jenkins-latest.war
-            url=https://updates.jenkins.io/latest/jenkins.war
+            url=https://repo.jenkins-ci.org/snapshots/org/jenkins-ci/main/jenkins-war/2.359-SNAPSHOT/jenkins-war-2.359-20220708.155905-1.war
         ;;
         "lts")
             war=jenkins-lts.war
