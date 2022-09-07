cf72035f7920c813736e64bdf805dc92a74a699d
Dmitry_Platonov <platonovd32@gmail.com>
2022-09-07 16:22:12 +0300

    click 'Check Now' before tick plugin to install

--- src/main/java/org/jenkinsci/test/acceptance/po/PluginManager.java
+++ src/main/java/org/jenkinsci/test/acceptance/po/PluginManager.java
@@ -95,34 +95,36 @@ public class PluginManager extends ContainerPageObject {
      * Force update the plugin update center metadata.
      */
     public void checkForUpdates() {
-        boolean isRunning = mockUpdateCenter.ensureRunning(jenkins);
-        if (isRunning) {
+        if (mockUpdateCenter.ensureRunning(jenkins)) {
             visit("index");
-            final String current = getCurrentUrl();
-            // The check now button is a form submit (POST) with a redirect to the same page only if the check is successful.
-            // We use the button itself to detect when the page has changed, which happens after the refresh has been done
-            // And we check for the presence of the button again
-            WebElement checkButton = find(by.link("Check now"));
-            checkButton.click();
-            // The wait criteria is: we have left the current page and returned to the same one
-            waitFor(checkButton).withTimeout(java.time.Duration.of(time.seconds(30), ChronoUnit.MILLIS)).until(webElement -> {
-                try {
-                    // We interact with the element just to detect if it is stale
-                    webElement.findElement(by.id("it does not matter"));
-                } catch(StaleElementReferenceException e) {
-                    // with this exception we know we've left the original page
-                    // we look for an element in the page to check for success
-                    if (current.equals(getCurrentUrl())) {
-                        return true;
-                    }
-                } catch(NoSuchElementException e) {
-                    return false;
-                }
-                return false;
-            });
-            updated = true;
-
+            clickCheckNow();
         }
+    }
+
+    private void clickCheckNow() {
+        final String current = getCurrentUrl();
+        // The check now button is a form submit (POST) with a redirect to the same page only if the check is successful.
+        // We use the button itself to detect when the page has changed, which happens after the refresh has been done
+        // And we check for the presence of the button again
+        WebElement checkButton = find(by.link("Check now"));
+        checkButton.click();
+        // The wait criteria is: we have left the current page and returned to the same one
+        waitFor(checkButton).withTimeout(java.time.Duration.of(time.seconds(30), ChronoUnit.MILLIS)).until(webElement -> {
+            try {
+                // We interact with the element just to detect if it is stale
+                webElement.findElement(by.id("it does not matter"));
+            } catch(StaleElementReferenceException e) {
+                // with this exception we know we've left the original page
+                // we look for an element in the page to check for success
+                if (current.equals(getCurrentUrl())) {
+                    return true;
+                }
+            } catch(NoSuchElementException e) {
+                return false;
+            }
+            return false;
+        });
+        updated = true;
     }
 
     public enum InstallationStatus {NOT_INSTALLED, OUTDATED, UP_TO_DATE}
@@ -193,7 +195,7 @@ public class PluginManager extends ContainerPageObject {
         final Map<String, String> candidates = getMapShortNamesVersion(specs);
 
         if (!updated) {
-            checkForUpdates();
+            mockUpdateCenter.ensureRunning(jenkins);
         }
 
         if (uploadPlugins) {
@@ -252,6 +254,12 @@ public class PluginManager extends ContainerPageObject {
             for (final PluginSpec n : specs) {
                 switch (installationStatus(n)) {
                     case NOT_INSTALLED:
+                        List<WebElement> allPlugins = jenkins.getPluginManager().all(by.xpath("//table[@id='plugins']//tr[@data-plugin-id]"));
+                        if (allPlugins.isEmpty()) {
+                            // list of available plugins wasn't refreshed, let's click 'Check Now' to update
+                            clickCheckNow();
+                        }
+
                         tickPluginToInstall(n);
                     break;
                     case OUTDATED:
