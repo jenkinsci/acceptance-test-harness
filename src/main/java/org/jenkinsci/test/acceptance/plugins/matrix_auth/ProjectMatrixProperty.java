f29373a7fb68e7f7e63bce52f2dad435b04610eb
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-10-02 18:21:17 +0100

    Add initial fixes from previous PR

--- src/main/java/org/jenkinsci/test/acceptance/plugins/matrix_auth/ProjectMatrixProperty.java
+++ src/main/java/org/jenkinsci/test/acceptance/plugins/matrix_auth/ProjectMatrixProperty.java
@@ -20,7 +20,7 @@ public class ProjectMatrixProperty extends PageAreaImpl {
      * Adds a new user/group to this matrix.
      */
     public MatrixRow addUser(String name) {
-        runThenHandleAlert(() -> this.name.resolve().findElement(by.xpath("../div/span/span/button")).click(),
+        runThenHandleAlert(() -> this.name.resolve().findElement(by.xpath("../div/span/span/button | button")).click(),
                 a -> {
             a.sendKeys(name);
             a.accept();
