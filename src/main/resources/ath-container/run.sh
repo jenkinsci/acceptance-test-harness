4bf16cae4f498d7268bd75f4b220fb9068eeac4c
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-10-02 20:11:32 +0100

    Update run.sh

--- src/main/resources/ath-container/run.sh
+++ src/main/resources/ath-container/run.sh
@@ -44,7 +44,7 @@ if [ ! -f $war ]; then
     case "$war" in
         "latest")
             war=jenkins-latest.war
-            url=https://repo.jenkins-ci.org/incrementals/org/jenkins-ci/main/jenkins-war/2.372-rc32915.3f56c040c65d/jenkins-war-2.372-rc32915.3f56c040c65d.war
+            url=https://ci.jenkins.io/job/Core/job/jenkins/job/PR-7203/1/artifact/org/jenkins-ci/main/jenkins-war/2.372-rc32933.36da_fb_248642/jenkins-war-2.372-rc32933.36da_fb_248642.war
         ;;
         "lts")
             war=jenkins-lts.war
