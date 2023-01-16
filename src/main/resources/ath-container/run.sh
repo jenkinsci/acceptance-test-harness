b1df8facd68f22b9118aaefb6952878860566c1c
Alexander Brandes <mc.cache@web.de>
2023-01-16 13:17:32 +0100

    Test core/7577

--- src/main/resources/ath-container/run.sh
+++ src/main/resources/ath-container/run.sh
@@ -43,8 +43,8 @@ war=$2
 if [ ! -f $war ]; then
     case "$war" in
         "latest")
-            war=jenkins-latest.war
-            url=https://updates.jenkins.io/latest/jenkins.war
+            #war=jenkins-latest.war
+            url=https://repo.jenkins-ci.org/incrementals/org/jenkins-ci/main/jenkins-war/2.387-rc33299.55a_62055dea_b/jenkins-war-2.387-rc33299.55a_62055dea_b.war
         ;;
         "lts")
             war=jenkins-lts.war
