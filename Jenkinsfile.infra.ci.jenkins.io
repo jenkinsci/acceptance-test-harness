02cb9aa6863154eb3d8045b705deaf2b0493b6e4
Tim Jacomb <timjacomb1@gmail.com>
2023-03-05 15:30:32 +0000

    Test clouds rework

--- Jenkinsfile.infra.ci.jenkins.io
+++ Jenkinsfile.infra.ci.jenkins.io
@@ -1,8 +1,1 @@
-// This pipeline runs on infra.ci.jenkins.io (private) to build, test and deploy the Docker image
-buildDockerAndPublishImage('ath', [
-  automaticSemanticVersioning: false,
-  gitCredentials: 'jenkinsci-ath-ghapp', // Only available in infra.ci.jenkins.io
-  dockerfile: 'src/main/resources/ath-container/Dockerfile',
-  imageDir: 'src/main/resources/ath-container/',
-  registryNamespace: 'jenkins',
-])
+echo "Skipping"
--- src/main/resources/ath-container/run.sh
+++ src/main/resources/ath-container/run.sh
@@ -43,8 +43,8 @@ war=$2
 if [ ! -f $war ]; then
     case "$war" in
         "latest")
-            war=jenkins-latest.war
-            url=https://updates.jenkins.io/latest/jenkins.war
+            #war=jenkins-latest.war
+            url=https://repo.jenkins-ci.org/incrementals/org/jenkins-ci/main/jenkins-war/2.394-rc33399.a_9b_e4d03d1fb_/jenkins-war-2.394-rc33399.a_9b_e4d03d1fb_.war
         ;;
         "lts")
             war=jenkins-lts.war
