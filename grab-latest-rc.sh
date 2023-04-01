637baf9da533ba12b4db3f1eab66b0657a784d18
Basil Crow <me@basilcrow.com>
2023-03-31 18:20:52 -0700

    Clean up shell scripts

--- grab-latest-rc.sh
+++ grab-latest-rc.sh
@@ -1,3 +1,3 @@
-#!/bin/sh -xe
-
-curl -L "https://get.jenkins.io/war-rc/latest/jenkins.war" > jenkins.war
+#!/usr/bin/env bash
+set -eux
+curl -L https://get.jenkins.io/war-rc/latest/jenkins.war >jenkins.war
