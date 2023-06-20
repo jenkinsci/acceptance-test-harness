3dd61f83323add7995cde77f9e810c40240a3f5b
Alexander Brandes <mc.cache@web.de>
2023-06-20 11:14:08 +0200

    Forward compatibility with Java 17 in GroovyPluginTest (#1235)
    * Forward compatibility with Java 17 in GroovyPluginTest
    
    * Update Groovy to 4.0.12
    
    * Bump groovy in GroovyPluginTest

--- src/test/java/plugins/GroovyPluginTest.java
+++ src/test/java/plugins/GroovyPluginTest.java
@@ -99,17 +99,17 @@ public class GroovyPluginTest extends AbstractJUnitTest {
 
     @Test
     public void use_custom_groovy_version() {
-        GroovyInstallation.installGroovy(jenkins, "groovy-2.2.1", "Groovy 2.2.1");
+        GroovyInstallation.installGroovy(jenkins, "groovy-4.0.12", "Groovy 4.0.12");
 
         configureJob();
 
         final GroovyStep step = job.addBuildStep(GroovyStep.class);
-        step.version.select("groovy-2.2.1");
+        step.version.select("groovy-4.0.12");
         step.script(
                 "println 'version: ' + groovy.lang.GroovySystem.getVersion()"
         );
         job.save();
-        shouldReport("version: 2.2.1");
+        shouldReport("version: 4.0.12");
     }
 
     @Test @Native("groovy")
