e089964fa7d79100e3bfd3a710ac84833a58b9d3
Raul Arabaolaza <rarabaolaza@cloudbees.com>
2023-09-04 16:48:09 +0200

    Try to wait for jobs to exist before checking build number

--- src/test/java/plugins/GitLabPluginTest.java
+++ src/test/java/plugins/GitLabPluginTest.java
@@ -1,8 +1,8 @@
 package plugins;
 
 import jakarta.inject.Inject;
-import org.apache.commons.io.IOUtils;
 import org.gitlab4j.api.GitLabApiException;
+import org.jenkinsci.test.acceptance.Matchers;
 import org.jenkinsci.test.acceptance.docker.DockerContainerHolder;
 import org.jenkinsci.test.acceptance.docker.fixtures.GitLabContainer;
 import org.jenkinsci.test.acceptance.junit.*;
@@ -12,17 +12,13 @@ import org.jenkinsci.test.acceptance.plugins.gitlab_plugin.*;
 import org.jenkinsci.test.acceptance.po.Build;
 import org.jenkinsci.test.acceptance.po.WorkflowJob;
 import org.jenkinsci.test.acceptance.po.WorkflowMultiBranchJob;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.openqa.selenium.WebElement;
 
 import java.net.http.HttpResponse;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
@@ -116,18 +112,19 @@ public class GitLabPluginTest extends AbstractJUnitTest {
         final WorkflowMultiBranchJob multibranchJob = jenkins.jobs.create(WorkflowMultiBranchJob.class);
         this.configureJobWithGitLabBranchSource(multibranchJob, adminUserName, repoName);
         multibranchJob.save();
-        multibranchJob.waitForBranchIndexingFinished(20);
 
+        multibranchJob.waitForBranchIndexingFinished((int)time.seconds(20));
         this.assertBranchIndexing(multibranchJob);
 
         final WorkflowJob successJob1 = multibranchJob.getJob("main");
         final WorkflowJob successJob2 = multibranchJob.getJob("firstbranch");
         final WorkflowJob successJob3 = multibranchJob.getJob("MR-1");
         final WorkflowJob failureJob = multibranchJob.getJob("failedjob");
-        this.assertExistAndRun(successJob1, true);
-        this.assertExistAndRun(successJob2, true);
-        this.assertExistAndRun(successJob3, true);
-        this.assertExistAndRun(failureJob, false);
+
+        this.assertExistAndResult(successJob1, true);
+        this.assertExistAndResult(successJob2, true);
+        this.assertExistAndResult(successJob3, true);
+        this.assertExistAndResult(failureJob, false);
     }
 
     private void assertBranchIndexing(final WorkflowMultiBranchJob job) {
@@ -147,16 +144,10 @@ public class GitLabPluginTest extends AbstractJUnitTest {
         ghBranchSource.setProject(adminUserName, project);
     }
 
-    private void assertExistAndRun(final WorkflowJob job, final boolean withSuccess) {
-        try {
-            IOUtils.toString(job.url("").openStream(), StandardCharsets.UTF_8);
-        } catch (final IOException ex) {
-            Assert.fail("Job has not been created");
-        }
-
+    private void assertExistAndResult(final WorkflowJob job, final boolean withSuccess) {
         final Build.Result expectedResult = (withSuccess) ? Build.Result.SUCCESS : Build.Result.FAILURE;
-        System.out.println("care e numarul urmator " + job.getNextBuildNumber());
-        assertEquals(job.getNextBuildNumber(), 2);
-        assertEquals(job.build(1).getResult(), expectedResult.name());
+        waitFor(job, Matchers.pageObjectExists(), (int)time.seconds(3));
+        assertEquals(job.getLastBuild().getResult(), expectedResult.name());
     }
+
 }
