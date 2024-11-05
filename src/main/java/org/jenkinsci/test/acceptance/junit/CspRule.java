d615ab36d31d76a1eb9fc0f1630ac19edef67d83
haresahani <111163929+haresahani@users.noreply.github.com>
2024-11-06 05:19:12 +0530

    Remove workaround for JENKINS-73900 in `CspRule` (#1818)

--- src/main/java/org/jenkinsci/test/acceptance/junit/CspRule.java
+++ src/main/java/org/jenkinsci/test/acceptance/junit/CspRule.java
@@ -37,11 +37,9 @@ public final class CspRule implements TestRule {
                     base.evaluate();
                 } finally {
                     // TODO enable for ArtifactoryPluginTest when JENKINS-74047 is resolved
-                    // TODO enable for SubversionPluginTest when JENKINS-73900 is resolved
                     if (isEnabled()
                             && !isSkipped()
-                            && !d.getTestClass().getName().equals("plugins.ArtifactoryPluginTest")
-                            && !d.getTestClass().getName().equals("plugins.SubversionPluginTest")) {
+                            && !d.getTestClass().getName().equals("plugins.ArtifactoryPluginTest")) {
                         ContentSecurityPolicyReport csp = new ContentSecurityPolicyReport(jenkins);
                         csp.open();
                         List<String> lines = csp.getReport();
