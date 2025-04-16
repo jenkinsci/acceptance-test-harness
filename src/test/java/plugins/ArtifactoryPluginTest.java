8258e51075d4bc30e28ea5348af488ff1fbd2a20
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-04-16 11:01:25 +0100

    Update ArtifactoryPluginTest.java

--- src/test/java/plugins/ArtifactoryPluginTest.java
+++ src/test/java/plugins/ArtifactoryPluginTest.java
@@ -71,7 +71,7 @@ public class ArtifactoryPluginTest extends AbstractJUnitTest {
          * Navigate back to the dashboard first to dismiss the alert so that CspRule can check for violations (see
          * FormValidationTest).
          */
-        jenkins.runThenConfirmAlert(() -> driver.findElement(By.xpath("//ol[@id=\"breadcrumbs\"]/li[1]/a"))
+        jenkins.runThenConfirmAlert(() -> driver.findElement(By.id("jenkins-head-icon"))
                 .click());
         sleep(1000);
     }
