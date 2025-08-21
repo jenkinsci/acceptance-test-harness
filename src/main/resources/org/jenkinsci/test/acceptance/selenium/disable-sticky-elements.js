c754dfc6e16df09e23cac321e6784c446248ee92
James Nord <jtnord@users.noreply.github.com>
2025-08-21 11:35:15 +0100

    breadcrumbs are not more, we need to make the header non sticky
    The header can obscure some elements which can get in the way of
    selenium
    This also makes the header not sticky

--- src/main/resources/org/jenkinsci/test/acceptance/selenium/disable-sticky-elements.js
+++ src/main/resources/org/jenkinsci/test/acceptance/selenium/disable-sticky-elements.js
@@ -2,13 +2,13 @@
 document.querySelector("html").style.scrollBehavior = "initial";
 
 // Disable common sticky elements
-const breadcrumbBar = document.getElementById("breadcrumbBar");
+const header = document.getElementById("page-header");
 const bottomAppBar = document.getElementById("bottom-sticker");
 // https://github.com/jenkinsci/jenkins/commit/6481e78d20a0c689859058da5a029489e8b5072c introduced a shadow on a different div!?
 const bottomShadow = document.querySelector(".jenkins-bottom-app-bar__shadow")
 
-if (breadcrumbBar) {
-    breadcrumbBar.style.position = "relative";
+if (header) {
+    header.style.position = "relative";
 }
 
 if (bottomAppBar) {
