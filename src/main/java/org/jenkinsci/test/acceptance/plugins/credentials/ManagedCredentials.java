94529cdea9cbda1e2c36972fb4f8b7e5ca5382c8
Tim Jacomb <timjacomb1@gmail.com>
2026-02-25 21:44:51 +0000

    Return the card

--- src/main/java/org/jenkinsci/test/acceptance/plugins/credentials/ManagedCredentials.java
+++ src/main/java/org/jenkinsci/test/acceptance/plugins/credentials/ManagedCredentials.java
@@ -41,7 +41,7 @@ public class ManagedCredentials extends ContainerPageObject {
      * Check if the given credential is part of the domain.
      */
     public Control checkIfCredentialsExist(String name) {
-        By xpath = by.xpath("//span[contains(text(), '" + name + "')]");
+        By xpath = by.xpath("//span[contains(text(), '" + name + "')]/parent::div/parent::div");
         return control(xpath);
     }
 
