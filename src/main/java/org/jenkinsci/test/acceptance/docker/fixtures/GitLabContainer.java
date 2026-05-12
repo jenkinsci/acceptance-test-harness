9446c27b24b286438a82d11aad884c83dad11d20
James Nord <jtnord@users.noreply.github.com>
2026-05-12 12:33:51 +0100

    Apply suggestion from @jtnord

--- src/main/java/org/jenkinsci/test/acceptance/docker/fixtures/GitLabContainer.java
+++ src/main/java/org/jenkinsci/test/acceptance/docker/fixtures/GitLabContainer.java
@@ -61,7 +61,6 @@ public class GitLabContainer extends GenericContainer<GitLabContainer> {
     @Override
     public String getHost() {
         return hostname;
-        // return super.getHost();
     }
 
     public String host() {
