d42da64ee043a4e744f0b188466256c5be2d750e
James Nord <jtnord@users.noreply.github.com>
2026-05-12 12:39:24 +0100

    Apply suggestion from @jtnord

--- src/main/java/org/jenkinsci/test/acceptance/docker/fixtures/GitLabContainer.java
+++ src/main/java/org/jenkinsci/test/acceptance/docker/fixtures/GitLabContainer.java
@@ -119,13 +119,15 @@ public class GitLabContainer extends GenericContainer<GitLabContainer> {
         // To avoid this we set the wait strategy here using which polls using webdriver
         // thus keeping the selenium session alive
         waitingFor(new WaitStrategy() {
+            private Duration timeout = READINESS_TIMEOUT;
+
             @Override
             public void waitUntilReady(WaitStrategyTarget waitStrategyTarget) {
                 try {
                     GitLabPage gitLabPage = new GitLabPage(p.injector, new URL(getHttpUrl()));
                     p.waitFor(this)
                             .withMessage("Waiting for GitLab to come up")
-                            .withTimeout(READINESS_TIMEOUT)
+                            .withTimeout(timeout)
                             .pollingEvery(READINESS_POLL_INTERVAL)
                             .ignoring(WebDriverException.class)
                             .until(() -> {
