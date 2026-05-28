ea0f23f25dbe20a2a66066904dbc733aecd780df
James Nord <jtnord@users.noreply.github.com>
2026-05-28 21:12:50 +0100

    Update src/test/java/org/jenkinsci/test/acceptance/po/SnippetGeneratorTest.java
    Co-authored-by: Jesse Glick <jglick@cloudbees.com>

--- src/test/java/org/jenkinsci/test/acceptance/po/SnippetGeneratorTest.java
+++ src/test/java/org/jenkinsci/test/acceptance/po/SnippetGeneratorTest.java
@@ -17,7 +17,7 @@ public class SnippetGeneratorTest extends AbstractJUnitTest {
         SnippetGenerator snippetGenerator = new SnippetGenerator(job);
         snippetGenerator.open();
         ArtifactArchiver aa = snippetGenerator.selectStep(ArtifactArchiver.class);
-        aa.includes(null);
-        assertThat(snippetGenerator.generateScript(), is("archiveArtifacts artifacts: '', followSymlinks: false"));
+        aa.includes("*.bin");
+assertThat(snippetGenerator.generateScript(), is("archiveArtifacts artifacts: '*.bin', followSymlinks: false"));
     }
 }
